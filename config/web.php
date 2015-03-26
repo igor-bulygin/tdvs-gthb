6d0aace3c4a197d92cc135f6d1e5dd62bbdd3008
Alexander Nestorov <alexandernst@gmail.com>
2015-03-26 12:18:33 +0100

    Add i18n messages

--- config/web.php
+++ config/web.php
@@ -1,21 +1,30 @@
 <?php
 
-$params = require(__DIR__ . '/params.php');
-
 $config = [
 	'id' => 'basic',
+
 	'basePath' => dirname(__DIR__),
-	'bootstrap' => ['log'],
+
+	'bootstrap' => ['log', 'languagepicker'],
+
 	'components' => [
+
+		//Requests and cookies
 		'request' => [
 			'cookieValidationKey' => 'Yq$66191i>#VPkmnDgW<L@Ol<Sw4R+0A1?*9r49%.<02`Q:7_8^0)Pe#tp87',
 		],
+
+		//Cache
 		'cache' => [
 			'class' => 'yii\caching\FileCache',
 		],
+
+		//Assets
 		'assetManager' => [
 			'appendTimestamp' => true,
 		],
+
+		//Log
 		'log' => [
 			'traceLevel' => YII_DEBUG ? 3 : 0,
 			'targets' => [
@@ -25,24 +34,55 @@ $config = [
 				],
 			],
 		],
+
+		//Errors
 		'errorHandler' => [
 			'errorAction' => 'site/error',
 		],
+
+		//Database
 		'mongodb' => require(__DIR__ . '/db.php'),
+
+		//Sessions
 		'session' => [
 			'class' => 'yii\mongodb\Session',
 			'sessionCollection' => 'tmp_sessions'
 		],
+
+		//URLs
 		'urlManager' => [
 			'enablePrettyUrl' => true,
 			'showScriptName' => false,
 			'rules' => [
 			]
 		],
+
+		'devicedetect' => [
+			'class' => 'alexandernst\devicedetect\DeviceDetect'
+		],
+
+		//Available languages
+		'languagepicker' => [
+			'class' => 'lajax\languagepicker\Component',
+			'languages' => require(__DIR__ . '/langs.php'),
+			'cookieName' => 'lng',
+			'expireDays' => 64,
+			'callback' => function() {
+				/*
+				if (!\Yii::$app->user->isGuest) {
+					$user = \Yii::$app->user->identity;
+					$user->language = \Yii::$app->language;
+					$user->save();
+				}
+				*/
+			}
+		],
+
 		'user' => [
 			'identityClass' => 'app\models\User',
 			'enableAutoLogin' => true,
 		],
+
 		'mailer' => [
 			'class' => 'yii\swiftmailer\Mailer',
 			// send all mails to a file by default. You have to set
@@ -51,9 +91,12 @@ $config = [
 			'useFileTransport' => true,
 		],
 	],
+
 	'defaultRoute' => 'public',
-	'language' => 'en',
-	'params' => $params,
+
+	//'language' => 'en',
+
+	'params' => require(__DIR__ . '/params.php')
 ];
 
 if (YII_ENV_DEV) {
