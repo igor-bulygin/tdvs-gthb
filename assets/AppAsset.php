71796f53c19ff3a9e6bd499161d86230e06f9630
Alexander Nestorov <alexandernst@gmail.com>
2015-03-31 12:08:56 +0200

    Restructuring CSS files

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -1,29 +1,33 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
 
 namespace app\assets;
 
 use yii\web\AssetBundle;
 
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
-    public $css = [
-        'css/site.css',
-    ];
-    public $js = [
-    ];
-    public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
-    ];
+	public function init()
+	{
+		$dd = \Yii::$app->devicedetect;
+		if ($dd->isMobile()) {
+			$this->css[] = "css/mobile/global.css";
+		} else if($dd->isTablet()) {
+			$this->css[] = "css/tablet/global.css";
+		} else {
+			$this->css[] = "css/desktop/global.css";
+		}
+	}
+
+	public $basePath = '@webroot';
+	public $baseUrl = '@web';
+	public $css = [
+		'css/global.css',
+		'css/bootstrap-patches.css',
+	];
+	public $js = [
+	];
+	public $depends = [
+		'yii\web\YiiAsset',
+		'yii\bootstrap\BootstrapAsset',
+	];
 }
