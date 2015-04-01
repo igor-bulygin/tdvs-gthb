034cae309b6bd67b46fa3d4b824ec5a1f7bdf86e
Alexander Nestorov <alexandernst@gmail.com>
2015-04-01 19:41:39 +0200

    Further organize views

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -8,22 +8,36 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\base\ViewContextInterface;
 
-class AdminController extends Controller
+class AdminController extends Controller implements ViewContextInterface
 {
 	public $defaultAction = "index";
+	public $viewPath = "";
 
 	public function init()
 	{
 		if (\Yii::$app->params['devicedetect']['isMobile']) {
-			$this->layout = "/mobile/admin";
+			$this->layout = '/mobile/admin';
+			$this->viewPath = '@app/views/admin/mobile';
 		} else if(\Yii::$app->params['devicedetect']['isTablet']) {
-			$this->layout = "/tablet/admin";
+			$this->layout = '/tablet/admin';
+			$this->viewPath = '@app/views/admin/tablet';
 		} else {
-			$this->layout = "/desktop/admin";
+			$this->layout = '/desktop/admin';
+			$this->viewPath = '@app/views/admin/desktop';
 		}
 	}
 
+	public function getViewPath()
+	{
+		return Yii::getAlias($this->viewPath);
+	}
+
 	public function actionIndex()
+	{
+		return $this->render("index");
+	}
+
+	public function actionCategories()
 	{
 		return $this->render("index");
 	}
