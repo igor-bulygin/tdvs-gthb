f489d2638afc0d36ab8f0fcdbebdaf7331bc2840
Alexander Nestorov <alexandernst@gmail.com>
2015-04-01 20:23:52 +0200

    Use CController

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -8,29 +8,11 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\base\ViewContextInterface;
 
-class AdminController extends Controller implements ViewContextInterface
+use yii\helpers\CController;
+
+class AdminController extends CController
 {
 	public $defaultAction = "index";
-	public $viewPath = "";
-
-	public function init()
-	{
-		if (\Yii::$app->params['devicedetect']['isMobile']) {
-			$this->layout = '/mobile/admin';
-			$this->viewPath = '@app/views/admin/mobile';
-		} else if(\Yii::$app->params['devicedetect']['isTablet']) {
-			$this->layout = '/tablet/admin';
-			$this->viewPath = '@app/views/admin/tablet';
-		} else {
-			$this->layout = '/desktop/admin';
-			$this->viewPath = '@app/views/admin/desktop';
-		}
-	}
-
-	public function getViewPath()
-	{
-		return Yii::getAlias($this->viewPath);
-	}
 
 	public function actionIndex()
 	{
