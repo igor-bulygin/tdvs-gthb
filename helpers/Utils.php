27a51220fb93b85b72c6b9a3eb670da7936cd98b
Alexander Nestorov <alexandernst@gmail.com>
2015-04-01 18:29:15 +0200

    Add a helper function to decide which sidenav's el should be active

--- helpers/Utils.php
+++ helpers/Utils.php
@@ -1,9 +1,14 @@
 <?php
 namespace yii\helpers;
+
 use Yii;
+use yii\helpers\Url;
 
 class Utils
 {
+	/**
+	 * Generate a random ID (YouTube-like) useful for slugs.
+	 */
 	public static function shortID($length)
 	{
 		$l = floor($length / 2);
@@ -13,5 +18,18 @@ class Utils
 			$random .= rand(0, 1) ? rand(0, 9) : chr(rand(ord('a'), ord('z')));
 		}
 		return $random;
+	}
+
+	/**
+	 * Returns true if the current controller/action is equal to the passed URL
+	 */
+	public static function compareURL($url)
+	{
+		$queryParams = Yii::$app->request->queryParams;
+		array_walk($queryParams, function(&$v, $k){
+			$v = null;
+		});
+
+		return Url::current($queryParams) === Url::toRoute($url);
 	}
 }
