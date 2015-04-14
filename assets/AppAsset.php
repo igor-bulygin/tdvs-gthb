51650ee565d121ebb660fd2fdc5f770baf754689
Alexander Nestorov <alexandernst@gmail.com>
2015-04-14 20:46:04 +0200

    Add global JS files to AppAsset

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -9,10 +9,13 @@ class AppAsset extends AssetBundle
 	public function init()
 	{
 		if (\Yii::$app->params['devicedetect']['isMobile']) {
+			$this->js[] = "js/mobile/global.js";
 			$this->css[] = "css/mobile/global.css";
 		} else if(\Yii::$app->params['devicedetect']['isTablet']) {
+			$this->js[] = "js/tablet/global.js";
 			$this->css[] = "css/tablet/global.css";
 		} else {
+			$this->js[] = "js/desktop/global.js";
 			$this->css[] = "css/desktop/global.css";
 		}
 	}
@@ -24,6 +27,7 @@ class AppAsset extends AssetBundle
 		'css/bootstrap-patches.css',
 	];
 	public $js = [
+		'js/global.js'
 	];
 	public $depends = [
 		'yii\web\YiiAsset',
