c119d16b1cd97c0a1eafc5fb384d0f5d33b9ebec
Alexander Nestorov <alexandernst@gmail.com>
2015-04-15 13:18:42 +0200

    Improve CActiveRecord

--- helpers/CActiveRecord.php
+++ helpers/CActiveRecord.php
@@ -2,26 +2,29 @@
 namespace app\helpers;
 
 use Yii;
-use yii\base\Model;
 use yii\db\Query;
+use yii\mongodb\ActiveRecord;
 
-class CModel extends Model
+class CActiveRecord extends ActiveRecord
 {
 
-	public function genValidID($collection)
+	public function genValidID()
 	{
+		//TODO: hacer algun tipo de bloqueo a nivel de base de datos del ID devuelto
 		$_found = false;
 		$_id = 0;
 		while ($_found  === false) {
 			$_id = Utils::shortID(6);
 
 			$query = new Query;
-			$query->select(['shortID'])->from($collection)->where(['shortID' => $_id]);
+			$query->
+				select(['shortID'])->
+				from($this->collectionName())->
+				where(['shortID' => $_id]);
 			$_found = empty($query->all());
 		}
 
 		return $_id;
 	}
-
 
 }
