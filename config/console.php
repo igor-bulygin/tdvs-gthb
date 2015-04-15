b2817427f017cf0de9820756e740625c138675da
Alexander Nestorov <alexandernst@gmail.com>
2015-04-15 13:43:54 +0200

    TABs

--- config/console.php
+++ config/console.php
@@ -3,30 +3,30 @@
 Yii::setAlias('@tests', dirname(__DIR__) . '/tests');
 
 return [
-    'id' => 'basic-console',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log', 'gii'],
-    'controllerNamespace' => 'app\commands',
-    'modules' => [
-        'gii' => 'yii\gii\Module',
-    ],
-    'controllerMap' => [
-        'mongodb-migrate' => 'yii\mongodb\console\controllers\MigrateController'
-    ],
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'log' => [
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        //Database
-        'mongodb' => require(__DIR__ . '/db.php'),
-    ],
-    'params' => require(__DIR__ . '/params.php'),
+	'id' => 'basic-console',
+	'basePath' => dirname(__DIR__),
+	'bootstrap' => ['log', 'gii'],
+	'controllerNamespace' => 'app\commands',
+	'modules' => [
+		'gii' => 'yii\gii\Module',
+	],
+	'controllerMap' => [
+		'mongodb-migrate' => 'yii\mongodb\console\controllers\MigrateController'
+	],
+	'components' => [
+		'cache' => [
+			'class' => 'yii\caching\FileCache',
+		],
+		'log' => [
+			'targets' => [
+				[
+					'class' => 'yii\log\FileTarget',
+					'levels' => ['error', 'warning'],
+				],
+			],
+		],
+		//Database
+		'mongodb' => require(__DIR__ . '/db.php'),
+	],
+	'params' => require(__DIR__ . '/params.php'),
 ];
