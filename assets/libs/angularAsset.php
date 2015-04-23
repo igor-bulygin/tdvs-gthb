c487c0adac4745831c01ee2ec5dc03dc17442111
Alexander Nestorov <alexandernst@gmail.com>
2015-04-23 20:39:19 +0200

    Ignore web assets (cache)

--- assets/libs/angularAsset.php
+++ assets/libs/angularAsset.php
@@ -1,16 +1,15 @@
 <?php
 
-namespace app\assets\desktop;
+namespace app\assets\libs;
 
 use yii\web\AssetBundle;
 
-class AngularAsset extends AssetBundle
-{
+class AngularAsset extends AssetBundle {
 	public $sourcePath = '@npm/angular';
 	public $css = [
 	];
 	public $js = [
-		YII_ENV_DEV ? 'angular.min.js' : 'angular.js'
+		YII_ENV_DEV ? 'angular.js' : 'angular.min.js'
 	];
 	public $depends = [
 		'yii\web\JqueryAsset',
