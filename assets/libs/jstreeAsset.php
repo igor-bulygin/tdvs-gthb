03c4c7573a37f29eeb0b9170953e9aaa63cd9f43
Alexander Nestorov <alexandernst@gmail.com>
2015-04-23 20:43:45 +0200

    Use min version of production, normal on dev

--- assets/libs/jstreeAsset.php
+++ assets/libs/jstreeAsset.php
@@ -8,12 +8,12 @@ class jstreeAsset extends AssetBundle
 {
 	public $sourcePath = '@npm/jstree/dist';
 	public $css = [
-		YII_ENV_DEV ? 'themes/default-dark/style.min.css' : 'themes/default-dark/style.css'
+		YII_ENV_DEV ? 'themes/default-dark/style.css' : 'themes/default-dark/style.min.css'
 	];
 	public $js = [
-		YII_ENV_DEV ? 'jstree.min.js' : 'jstree.js'
+		YII_ENV_DEV ? 'jstree.js' : 'jstree.min.js'
 	];
 	public $depends = [
-		'yii\web\JqueryAsset',
+		'yii\web\JqueryAsset'
 	];
 }
