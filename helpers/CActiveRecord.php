d7ae1a4399049f35514ad08d0987205686ea3a35
Alexander Nestorov <alexandernst@gmail.com>
2015-04-23 20:45:07 +0200

    Make the 'genValidID()' method rocksolid

--- helpers/CActiveRecord.php
+++ helpers/CActiveRecord.php
@@ -2,26 +2,21 @@
 namespace app\helpers;
 
 use Yii;
+use Exception;
 use yii\db\Query;
 use yii\mongodb\ActiveRecord;
 
-class CActiveRecord extends ActiveRecord
-{
+class CActiveRecord extends ActiveRecord {
 
-	public function genValidID()
-	{
-		//TODO: hacer algun tipo de bloqueo a nivel de base de datos del ID devuelto
+	public function genValidID() {
 		$_found = false;
-		$_id = 0;
-		while ($_found  === false) {
+		$_id = null;
+		while ($_found === false) {
 			$_id = Utils::shortID(6);
-
-			$query = new Query;
-			$query->
-				select(['shortID'])->
-				from($this->collectionName())->
-				where(['shortID' => $_id]);
-			$_found = empty($query->all());
+			try {
+				$this->getCollection()->insert(['short_id' => $_id]);
+				$_found = true;
+			} catch (Exception $e) {}
 		}
 
 		return $_id;
