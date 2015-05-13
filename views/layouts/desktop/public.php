4953ca4052176d471c3ccc12e52f8f0f1e9c5ff8
Alexander Nestorov <alexandernst@gmail.com>
2015-05-13 12:07:39 +0200

    Fix indent in public layout

--- views/layouts/desktop/public.php
+++ views/layouts/desktop/public.php
@@ -11,6 +11,7 @@ use lajax\languagepicker\widgets\LanguagePicker;
 
 AppAsset::register($this);
 ?>
+
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
@@ -24,59 +25,59 @@ AppAsset::register($this);
 <body>
 
 <?php $this->beginBody() ?>
-	<div class="wrap">
-		<?php
+<div class="wrap">
+	<?php
 
-			NavBar::begin([
-				'brandLabel' => 'Todevise',
-				'brandUrl' => Yii::$app->homeUrl,
-				'options' => [
-					'class' => 'navbar-inverse navbar-fixed-top',
-				],
-			]);
+	NavBar::begin([
+		'brandLabel' => 'Todevise',
+		'brandUrl' => Yii::$app->homeUrl,
+		'options' => [
+			'class' => 'navbar-inverse navbar-fixed-top',
+		],
+	]);
 
-			echo LanguagePicker::widget([
-				'skin' => LanguagePicker::SKIN_DROPDOWN,
-				'size' => LanguagePicker::SIZE_LARGE,
-				'parentTemplate' => '<div class="language-picker dropdown-list {size}"><div>{activeItem}<ul>{items}</ul></div></div>',
-				'itemTemplate' => '<li><a href="{link}" title="{name}">{name}</a></li>',
-				'activeItemTemplate' => '<a href="{link}" title="{name}">{name}</a>',
-				'languageAsset' => 'lajax\languagepicker\bundles\LanguageLargeIconsAsset',
-				'languagePluginAsset' => 'lajax\languagepicker\bundles\LanguagePluginAsset',
-			]);
+	echo LanguagePicker::widget([
+		'skin' => LanguagePicker::SKIN_DROPDOWN,
+		'size' => LanguagePicker::SIZE_LARGE,
+		//'parentTemplate' => '<div class="language-picker dropdown-list {size}"><div>{activeItem}<ul>{items}</ul></div></div>',
+		'itemTemplate' => '<li><a href="{link}" title="{name}">{name}</a></li>',
+		'activeItemTemplate' => '<a href="" title="{name}">{name}</a>',
+		'languageAsset' => 'lajax\languagepicker\bundles\LanguageLargeIconsAsset',
+		'languagePluginAsset' => 'lajax\languagepicker\bundles\LanguagePluginAsset',
+	]);
 
-			echo Nav::widget([
-				'options' => ['class' => 'navbar-nav navbar-right'],
-				'items' => [
-					['label' => 'Home', 'url' => ['/site/index']],
-					['label' => 'About', 'url' => ['/site/about']],
-					['label' => 'Contact', 'url' => ['/site/contact']],
-					Yii::$app->user->isGuest ?
-						['label' => 'Login', 'url' => ['/site/login']] :
-						['label' => 'Logout (' . Yii::$app->user->identity->personal_infoModel->name . ')',
-							'url' => ['/site/logout'],
-							'linkOptions' => ['data-method' => 'post']],
-				],
-			]);
-			NavBar::end();
-		?>
+	echo Nav::widget([
+		'options' => ['class' => 'navbar-nav navbar-right'],
+		'items' => [
+			['label' => 'Home', 'url' => ['/site/index']],
+			['label' => 'About', 'url' => ['/site/about']],
+			['label' => 'Contact', 'url' => ['/site/contact']],
+			Yii::$app->user->isGuest ?
+				['label' => 'Login', 'url' => ['/site/login']] :
+				['label' => 'Logout (' . Yii::$app->user->identity->personal_infoModel->name . ')',
+					'url' => ['/site/logout'],
+					'linkOptions' => ['data-method' => 'post']],
+		],
+	]);
+	NavBar::end();
+	?>
 
-		<div class="container">
-			<?= Breadcrumbs::widget([
-				'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-			]) ?>
-			<?= $content ?>
-		</div>
+	<div class="container">
+		<?= Breadcrumbs::widget([
+			'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+		]) ?>
+		<?= $content ?>
 	</div>
+</div>
 
-	<footer class="footer">
-		<div class="container">
-			<p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-			<p class="pull-right"><?= Yii::powered() ?></p>
-		</div>
-	</footer>
-
+<footer class="footer">
+	<div class="container">
+		<p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+		<p class="pull-right"><?= Yii::powered() ?></p>
+	</div>
+</footer>
 <?php $this->endBody() ?>
+
 </body>
 </html>
 <?php $this->endPage() ?>
