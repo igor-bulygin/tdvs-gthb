42396f5c65e0f847fc6c361533aa3a1add23d7da
Alexander Nestorov <alexandernst@gmail.com>
2015-05-14 20:55:57 +0200

    Stop requiring manually AppAsset

--- views/layouts/desktop/public.php
+++ views/layouts/desktop/public.php
@@ -3,13 +3,11 @@ use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
-use app\assets\AppAsset;
 use lajax\languagepicker\widgets\LanguagePicker;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-AppAsset::register($this);
 ?>
 
 <?php $this->beginPage() ?>
