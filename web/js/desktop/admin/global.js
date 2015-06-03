f327e76582fb36590bf61ec95c8d97ea2e9cadd1
Alexander Nestorov <alexandernst@gmail.com>
2015-06-03 23:21:48 +0200

    Rename the global angular module to global-admin, as we'll have more than 1 global module (one per level)

--- web/js/desktop/admin/global.js
+++ web/js/desktop/admin/global.js
@@ -1,14 +1,14 @@
 console.log("Global desktop admin");
-var global = angular.module('global', ['toastr', 'template/modal/confirm.html']);
+var global_admin = angular.module('global-admin', ['toastr', 'template/modal/confirm.html']);
 
-global.constant("CONSTS", (function(){
+global_admin.constant("CONSTS", (function(){
 	var lang = $('html').attr('lang') || "en-US";
 	return {
 		current_lang: lang
 	};
 })());
 
-global.controller("confirmCtrl", function($scope, $modalInstance, data) {
+global_admin.controller("confirmCtrl", function($scope, $modalInstance, data) {
 	$scope.data = angular.extend({}, {
 		title: "Please confirm",
 		text: "",
@@ -27,15 +27,15 @@ global.controller("confirmCtrl", function($scope, $modalInstance, data) {
 
 angular.module("template/modal/confirm.html", []).run(["$templateCache", function($templateCache) {
 	$templateCache.put("template/modal/confirm.html",
-		"<div class=\"modal-header\">" +
-		"	<h3 class=\"modal-title\">{{ data.title }}</h3>" +
+		"<div class='modal-header'>" +
+		"	<h3 class='modal-title'>{{ data.title }}</h3>" +
 		"</div>" +
-		"<div class=\"modal-body\">" +
+		"<div class='modal-body'>" +
 		"	<div>{{ data.text }}</div>" +
 		"</div>" +
-		"<div class=\"modal-footer\">" +
-		"	<button class=\"btn btn-danger\" ng-click=\"ok()\">{{ data.ok }}</button>" +
-		"	<button class=\"btn btn-primary\" ng-click=\"cancel()\">{{ data.cancel }}</button>" +
-		"</div>" +
-		"");
+		"<div class='modal-footer'>" +
+		"	<button class='btn btn-danger' ng-click='ok()'>{{ data.ok }}</button>" +
+		"	<button class='btn btn-primary' ng-click='cancel()'>{{ data.cancel }}</button>" +
+		"</div>"
+	);
 }]);
