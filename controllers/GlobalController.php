6c72f18e362f49652b7a3a3d0facf1331edd17c3
Alexander Nestorov <alexandernst@gmail.com>
2015-06-05 02:44:05 +0200

    Add a global controller for helpful functions

---
+++ controllers/GlobalController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\helpers\Json;
+use yii\web\Response;
+use app\helpers\CController;
+use yii\filters\ContentNegotiator;
+
+class GlobalController extends CController {
+
+	public function behaviors() {
+		return [
+			'format' => [
+				'class' => ContentNegotiator::className(),
+				'formats' => [
+					'application/json' => Response::FORMAT_JSON
+				]
+			]
+		];
+	}
+
+	public function actionSetFlash($key, $message) {
+		Yii::$app->session->setFlash($key, $message);
+		return true;
+	}
+
+	public function actionGetFlashes() {
+		return Json::encode(Yii::$app->session->getAllFlashes());
+	}
+
+}
