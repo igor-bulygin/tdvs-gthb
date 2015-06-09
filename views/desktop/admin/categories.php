ab519a217afa88100e76f22bac22ee11587f1c0f
Alexander Nestorov <alexandernst@gmail.com>
2015-06-09 17:54:52 +0200

    Improve styling a little bit

--- views/desktop/admin/categories.php
+++ views/desktop/admin/categories.php
@@ -16,14 +16,14 @@ $this->title = 'Todevise / Admin / Categories';
 <div class="row" ng-controller="categoriesCtrl">
 	<div class="col-sm-12 col-md-12 col-lg-12">
 		<h2>Categories</h2>
-		<div>
-			Search:
-			<input ng-model="searchModel" ng-change="search()" class="" placeholder="Category name..."></input>
+		<div class="input-group">
+			<span class="input-group-addon" id="basic-addon1">Search</span>
+			<input type="text" class="form-control" placeholder="Category name..." aria-describedby="basic-addon1" ng-model="searchModel" ng-change="search()">
 		</div>
 
-		<button ng-click="open_all()">Open all</button>
-		<button ng-click="close_all()">Close all</button>
-		<button ng-click="create()">Create root category</button>
+		<button class="btn btn-default" ng-click="open_all()">Open all</button>
+		<button class="btn btn-default" ng-click="close_all()">Close all</button>
+		<button class="btn btn-default" ng-click="create()">Create root category</button>
 
 		<div js-tree="treeConfig" ng-model="treeData" should-apply="ignoreModelChanges" tree="treeInstance"
 			tree-events="ready:readyCB;create_node:restoreState;move_node:move">
