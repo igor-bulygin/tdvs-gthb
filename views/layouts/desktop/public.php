289436546249438facaf36ed0d9ed75c1ef73fce
Alexander Nestorov <alexandernst@gmail.com>
2015-06-19 21:21:06 +0200

    WIP main

--- views/layouts/desktop/public.php
+++ views/layouts/desktop/public.php
@@ -2,7 +2,6 @@
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
 use lajax\languagepicker\widgets\LanguagePicker;
 
 /* @var $this \yii\web\View */
@@ -23,57 +22,62 @@ use lajax\languagepicker\widgets\LanguagePicker;
 	<body>
 
 	<?php $this->beginBody() ?>
-	<div class="wrap">
-		<?php
 
-		NavBar::begin([
-			'brandLabel' => 'Todevise',
-			'brandUrl' => Yii::$app->homeUrl,
-			'options' => [
-				'class' => 'navbar-inverse navbar-fixed-top',
-			],
-		]);
+		<div class="container-fluid no-horizontal-padding max-height">
 
-		echo LanguagePicker::widget([
-			'skin' => LanguagePicker::SKIN_DROPDOWN,
-			'size' => LanguagePicker::SIZE_LARGE,
-			//'parentTemplate' => '<div class="language-picker dropdown-list {size}"><div>{activeItem}<ul>{items}</ul></div></div>',
-			'itemTemplate' => '<li><a href="{link}" title="{name}">{name}</a></li>',
-			'activeItemTemplate' => '<a href="" title="{name}">{name}</a>',
-			'languageAsset' => 'lajax\languagepicker\bundles\LanguageLargeIconsAsset',
-			'languagePluginAsset' => 'lajax\languagepicker\bundles\LanguagePluginAsset',
-		]);
+			<div class="row no-gutter max-height">
 
-		echo Nav::widget([
-			'options' => ['class' => 'navbar-nav navbar-right'],
-			'items' => [
-				['label' => 'Home', 'url' => ['/site/index']],
-				['label' => 'About', 'url' => ['/site/about']],
-				['label' => 'Contact', 'url' => ['/site/contact']],
-				Yii::$app->user->isGuest ?
-					['label' => 'Login', 'url' => ['/site/login']] :
-					['label' => 'Logout (' . Yii::$app->user->identity->personal_info["name"] . ')',
-						'url' => ['/site/logout'],
-						'linkOptions' => ['data-method' => 'post']],
-			],
-		]);
-		NavBar::end();
-		?>
+				<div class="col-lg-1-5 max-height">
+					LEFT COLUMNN
+				</div>
 
-		<div class="container">
-			<?= Breadcrumbs::widget([
-				'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-			]) ?>
-			<?= $content ?>
+				<div class="col-lg-10-5 main-content">
+
+					<?php
+
+					NavBar::begin([
+						'brandLabel' => 'Todevise',
+						'brandUrl' => Yii::$app->homeUrl,
+						'options' => [
+							'class' => 'navbar-inverse navbar-fixed-top',
+						],
+					]);
+
+					echo Nav::widget([
+						'options' => ['class' => 'navbar-nav navbar-right'],
+						'items' => [
+							['label' => 'Home', 'url' => ['/site/index']],
+							['label' => 'About', 'url' => ['/site/about']],
+							['label' => 'Contact', 'url' => ['/site/contact']],
+							Yii::$app->user->isGuest ?
+								['label' => 'Login', 'url' => ['/site/login']] :
+								['label' => 'Logout (' . Yii::$app->user->identity->personal_info["name"] . ')',
+									'url' => ['/site/logout'],
+									'linkOptions' => ['data-method' => 'post']],
+						],
+					]);
+					NavBar::end();
+					?>
+
+					<div class="container-fluid">
+						<div class="site-index">
+							<div class="body-content">
+								<?= $content ?>
+							</div>
+						</div>
+					</div>
+
+					<div class="footer">
+						<div class="container">
+							<p class="pull-left">&copy; Todevise <?= date('Y') ?></p>
+							<p class="pull-right"><?= Yii::powered() ?></p>
+						</div>
+					</div>
+
+				</div>
+			</div>
 		</div>
-	</div>
 
-	<footer class="footer">
-		<div class="container">
-			<p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-			<p class="pull-right"><?= Yii::powered() ?></p>
-		</div>
-	</footer>
 	<?php $this->endBody() ?>
 
 	</body>
