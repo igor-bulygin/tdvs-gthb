830a796978b9634203ef2a6fd49f2214538c8ddb
Alexander Nestorov <alexandernst@gmail.com>
2015-06-26 21:28:03 +0200

    Add migration for a Deviser person

--- migrations/m150405_184924_create_person.php
+++ migrations/m150405_184924_create_person.php
@@ -1,19 +1,36 @@
 <?php
 
+use app\models\Lang;
+use app\models\Person;
+use app\helpers\Currency;
 use yii\mongodb\Migration;
 
 class m150405_184924_create_person extends Migration {
 	public function up() {
+		$en = array_keys(Lang::EN_US)[0];
+
 		$this->createCollection('person');
 		$this->createIndex('person', 'short_id', [
 			'unique' => true
 		]);
+		$this->createIndex('person', 'type');
+		$this->createIndex('person', 'credentials.email');
 
 		$this->insert('person', [
 			"short_id" => "10000",
-			"path" => '/',
-			"name" => ["en-US" => "Category 1 (10000)"],
-			"slug" => ["en-US" => "category-1"]
+			"type" => [Person::DEVISER],
+			"personal_info" => [
+				"name" => "Deviser",
+				"surnames" => ["Foo", "Bar"],
+				"bday" => new MongoDate(strtotime("1990-01-06 00:00:00"))
+			],
+			"credentials" => [
+				"email" => "deviser@test.com"
+			],
+			"preferences" => [
+				"language" => $en,
+				"currency" => Currency::_EUR
+			]
 		]);
 	}
 
