c4c1d430c1eaafc9a006a41caeb71b0c4e3e94f9
Alexander Nestorov <alexandernst@gmail.com>
2015-06-26 18:36:28 +0200

    More styling fixes in /admin/tags

--- views/desktop/admin/tags.php
+++ views/desktop/admin/tags.php
@@ -26,32 +26,34 @@ $this->title = 'Todevise / Admin / Tags';
 
 		<?php $this->registerJs("var _categories = " . Json::encode($categories) . ";", View::POS_HEAD) ?>
 
-		<div class="row no-gutter">
-			<div class="col-xs-2">
-				<h2 class="funiv_bold fs-upper fc-fff fs1-071"><?= Yii::t("app/admin", "Tag list"); ?></h2>
-			</div>
-			<div class="col-xs-6 flex-inline flex-align-center">
-				<div class="funiv fs-upper fc-9b fs0-786"><?php echo Yii::t("app/admin", "FILTER BY CATEGORY"); ?></div>
-				<div
-					angular-multi-select
-					api="api"
-					id-property="short_id"
-					input-model="categories"
-					output-model="selectedCategories"
+		<div class="row no-gutter page-title-row">
+			<div class="row-same-height">
+				<div class="col-xs-2 col-height col-middle">
+					<h2 class="page-title funiv_bold fs-upper fc-fff fs1-071"><?= Yii::t("app/admin", "Tag list"); ?></h2>
+				</div>
+				<div class="col-xs-6 col-height col-middle flex flex-align-center">
+					<div class="funiv fs-upper fc-9b fs0-786 flex-inline"><?php echo Yii::t("app/admin", "FILTER BY CATEGORY"); ?></div>
+					<div
+						angular-multi-select
+						api="api"
+						id-property="short_id"
+						input-model="categories"
+						output-model="selectedCategories"
 
-					group-property="sub"
-					tick-property="check"
+						group-property="sub"
+						tick-property="check"
 
-					item-label="{{ name[lang] }}"
-					selection-mode="single"
-					search-property="name"
-					min-search-length="3"
-					hidden-property="hidden"
-					helper-elements="noall nonone noreset nofilter"
-				></div>
-			</div>
-			<div class="col-xs-4">
-				<button class="btn btn-default btn-purple fc-fff funiv fs0-786 fs-upper" ng-click="create_new()">Create new</button>
+						item-label="{{ name[lang] }}"
+						selection-mode="single"
+						search-property="name"
+						min-search-length="3"
+						hidden-property="hidden"
+						helper-elements="noall nonone noreset nofilter"
+					></div>
+				</div>
+				<div class="col-xs-4 col-height col-middle">
+					<button class="btn btn-default btn-purple fc-fff funiv fs0-786 fs-upper pull-right" ng-click="create_new()"><?php echo Yii::t("app/admin", "Create new"); ?></button>
+				</div>
 			</div>
 		</div>
 
