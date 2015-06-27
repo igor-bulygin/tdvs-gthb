d5a8837f364020fbe52d85bc727ae4cb72a80c2f
Alexander Nestorov <alexandernst@gmail.com>
2015-06-27 20:34:59 +0200

    Add edit-info action to the Devisers controller

--- controllers/DeviserController.php
+++ controllers/DeviserController.php
@@ -9,12 +9,14 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\base\ViewContextInterface;
 
-class DeviserController extends CController
-{
+class DeviserController extends CController {
 	public $defaultAction = "index";
 
-	public function actionIndex()
-	{
+	public function actionIndex() {
 		return $this->render("index");
+	}
+
+	public function actionEditInfo($slug) {
+		return $slug;
 	}
 }
