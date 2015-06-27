9d227bae64357875995277a16b35a2ef9866da84
Alexander Nestorov <alexandernst@gmail.com>
2015-06-27 09:31:37 +0200

    Create one person of each type

--- migrations/m150405_184924_create_person.php
+++ migrations/m150405_184924_create_person.php
@@ -18,14 +18,65 @@ class m150405_184924_create_person extends Migration {
 
 		$this->insert('person', [
 			"short_id" => "10000",
+			"type" => [Person::ADMIN],
+			"personal_info" => [
+				"name" => "Admin",
+				"surnames" => ["Foo", "Bar"],
+				"bday" => new MongoDate(strtotime("1990-01-06 03:42:15"))
+			],
+			"credentials" => [
+				"email" => "admin@test.com"
+			],
+			"preferences" => [
+				"language" => $en,
+				"currency" => Currency::_EUR
+			]
+		]);
+
+		$this->insert('person', [
+			"short_id" => "20000",
+			"type" => [Person::CLIENT],
+			"personal_info" => [
+				"name" => "Client",
+				"surnames" => ["Foo", "Bar"],
+				"bday" => new MongoDate(strtotime("1990-01-06 03:42:15"))
+			],
+			"credentials" => [
+				"email" => "client@test.com"
+			],
+			"preferences" => [
+				"language" => $en,
+				"currency" => Currency::_EUR
+			]
+		]);
+
+		$this->insert('person', [
+			"short_id" => "30000",
 			"type" => [Person::DEVISER],
 			"personal_info" => [
 				"name" => "Deviser",
 				"surnames" => ["Foo", "Bar"],
-				"bday" => new MongoDate(strtotime("1990-01-06 00:00:00"))
+				"bday" => new MongoDate(strtotime("1990-01-06 03:42:15"))
 			],
 			"credentials" => [
 				"email" => "deviser@test.com"
+			],
+			"preferences" => [
+				"language" => $en,
+				"currency" => Currency::_EUR
+			]
+		]);
+
+		$this->insert('person', [
+			"short_id" => "40000",
+			"type" => [Person::COLLABORATOR],
+			"personal_info" => [
+				"name" => "Collaborator",
+				"surnames" => ["Foo", "Bar"],
+				"bday" => new MongoDate(strtotime("1990-01-06 03:42:15"))
+			],
+			"credentials" => [
+				"email" => "collaborator@test.com"
 			],
 			"preferences" => [
 				"language" => $en,
