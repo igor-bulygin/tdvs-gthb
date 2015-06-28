489f4eb32680f9c984e11e344671c3d7da157bf5
Alexander Nestorov <alexandernst@gmail.com>
2015-06-28 20:40:24 +0200

    Change the index view to match previous commit changes

--- views/desktop/public/index.php
+++ views/desktop/public/index.php
@@ -12,16 +12,9 @@ IndexAsset::register($this);
 
 $this->title = 'Todevise / Public / Index';
 ?>
-<div class="site-index">
 
-	<div class="body-content">
-
-		<div class="row">
-			<div class="col-lg-12">
-				<?= Yii::t("app", "This is a test from {0} controller!", $this->context->id); ?>
-			</div>
-		</div>
-
+<div class="row no-gutter">
+	<div class="col-xs-12">
+		<?= Yii::t("app", "This is a test from {0} controller!", $this->context->id); ?>
 	</div>
-
 </div>
