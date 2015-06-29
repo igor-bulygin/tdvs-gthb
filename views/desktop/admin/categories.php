bfcdf044ae843f6473cf68a2f2af41348077ea17
NVNAT <natalia.vallinas@gmail.com>
2015-06-30 00:39:46 +0200

    text with yii | text color edit --> black | text color input --> black

--- views/desktop/admin/categories.php
+++ views/desktop/admin/categories.php
@@ -19,20 +19,20 @@ $this->title = 'Todevise / Admin / Categories';
 		<div class="row no-gutter page-title-row bgcolor-3d">
 			<div class="row-same-height">
 				<div class="col-xs-3 col-height col-middle">
-					<h2 class="page-title funiv_bold fs-upper fc-fff fs1-071">Categories</h2>
+					<h2 class="page-title funiv_bold fs-upper fc-fff fs1-071"><?= Yii::t("app/admin", "Categories"); ?></h2>
 				</div>
 
 				<div class="col-xs-5 col-height col-middle">
 					<div class="input-group">
 						<span class="input-group-addon" id="basic-addon1">Search</span>
-						<input type="text" class="form-control" placeholder="Category name..." aria-describedby="basic-addon1" ng-model="searchModel" ng-change="search()">
+						<input type="text" class="form-control" placeholder="<?php echo Yii::t("app/admin", "Category name..."); ?>" aria-describedby="basic-addon1" ng-model="searchModel" ng-change="search()">
 					</div>
 				</div>
 
 				<div class="col-xs-4 col-height col-middle text-center">
-					<button class="btn btn-light-green fc-fff funiv fs0-786 fs-upper margin-l0-r1" ng-click="open_all()">Open all</button>
-					<button class="btn btn-coral fc-fff funiv fs0-786 fs-upper margin-l0-r1" ng-click="close_all()">Close all</button>
-					<button class="btn btn-purple fc-fff funiv fs0-786 fs-upper" ng-click="create()">Create root category</button>
+					<button class="btn btn-light-green fc-fff funiv fs0-786 fs-upper margin-l0-r1" ng-click="open_all()"><?php echo Yii::t("app/admin", "Open all"); ?></button>
+					<button class="btn btn-coral fc-fff funiv fs0-786 fs-upper margin-l0-r1" ng-click="close_all()"><?php echo Yii::t("app/admin", "Close all"); ?></button>
+					<button class="btn btn-purple fc-fff funiv fs0-786 fs-upper" ng-click="create()"><?php echo Yii::t("app/admin", "Create root category"); ?></button>
 				</div>
 			</div>
 		</div>
