a5e3aad1d13fdb98f64e80a587d96b4b8078c575
NVNAT <natalia.vallinas@gmail.com>
2015-06-30 00:01:17 +0200

    catefories in progress

--- views/desktop/admin/categories.php
+++ views/desktop/admin/categories.php
@@ -14,19 +14,32 @@ $this->title = 'Todevise / Admin / Categories';
 ?>
 
 <div class="row no-gutter" ng-controller="categoriesCtrl">
-	<div class="col-sm-12 col-md-12 col-lg-12">
-		<h2>Categories</h2>
-		<div class="input-group">
-			<span class="input-group-addon" id="basic-addon1">Search</span>
-			<input type="text" class="form-control" placeholder="Category name..." aria-describedby="basic-addon1" ng-model="searchModel" ng-change="search()">
-		</div>
+	<div class="col-xs-12 no-horizontal-padding">
 
-		<button class="btn btn-default" ng-click="open_all()">Open all</button>
-		<button class="btn btn-default" ng-click="close_all()">Close all</button>
-		<button class="btn btn-default" ng-click="create()">Create root category</button>
+		<div class="row no-gutter page-title-row bgcolor-3d">
+			<div class="row-same-height">
+				<div class="col-xs-3 col-height col-middle">
+					<h2 class="page-title funiv_bold fs-upper fc-fff fs1-071">Categories</h2>
+				</div>
+
+				<div class="col-xs-5 col-height col-middle">
+					<div class="input-group">
+						<span class="input-group-addon" id="basic-addon1">Search</span>
+						<input type="text" class="form-control" placeholder="Category name..." aria-describedby="basic-addon1" ng-model="searchModel" ng-change="search()">
+					</div>
+				</div>
+
+				<div class="col-xs-4 col-height col-middle text-center">
+					<button class="btn btn-light-green fc-fff funiv fs0-786 fs-upper margin-l0-r1" ng-click="open_all()">Open all</button>
+					<button class="btn btn-coral fc-fff funiv fs0-786 fs-upper margin-l0-r1" ng-click="close_all()">Close all</button>
+					<button class="btn btn-purple fc-fff funiv fs0-786 fs-upper" ng-click="create()">Create root category</button>
+				</div>
+			</div>
+		</div>
 
 		<div js-tree="treeConfig" ng-model="treeData" should-apply="ignoreModelChanges" tree="treeInstance"
 			tree-events="ready:readyCB;create_node:restoreState;move_node:move">
 		</div>
+
 	</div>
 </div>
