b8f60cccb9edf466159abc22a59231e9a1da7976
Alexander Nestorov <alexandernst@gmail.com>
2015-06-30 20:09:44 +0200

    Add deviser global utils (waiting for https://github.com/danialfarid/ng-file-upload/issues/849)

--- web/js/desktop/deviser/global.js
+++ web/js/desktop/deviser/global.js
@@ -1,2 +1,62 @@
 console.log("Global desktop deviser");
 var global_deviser = angular.module('global-deviser', ['toastr']);
+
+global_deviser.dataURLtoBlob = function(dataURI) {
+	var byteString;
+	if (dataURI.split(',')[0].indexOf('base64') >= 0)
+		byteString = atob(dataURI.split(',')[1]);
+	else
+		byteString = unescape(dataURI.split(',')[1]);
+
+	// separate out the mime component
+	var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+	// write the bytes of the string to a typed array
+	var ia = new Uint8Array(byteString.length);
+	for (var i = 0; i < byteString.length; i++) {
+		ia[i] = byteString.charCodeAt(i);
+	}
+
+	return new Blob([ia], {type:mimeString});
+};
+
+global_deviser.directive('ngfBgSrc', ['$parse', '$timeout', function ($parse, $timeout) {
+	return {
+		restrict: 'AE',
+		link: function (scope, elem, attr) {
+			if (window.FileReader) {
+				scope.$watch(attr.ngfBgSrc, function (file) {
+					if (file &&
+						ngFileUpload.validate(scope, $parse, attr, file, null) &&
+						(!window.FileAPI || navigator.userAgent.indexOf('MSIE 8') === -1 || file.size < 20000) &&
+						(!window.FileAPI || navigator.userAgent.indexOf('MSIE 9') === -1 || file.size < 4000000)) {
+						$timeout(function () {
+							//prefer URL.createObjectURL for handling refrences to files of all sizes
+							//since it doesn´t build a large string in memory
+							var URL = window.URL || window.webkitURL;
+							if (URL && URL.createObjectURL) {
+								elem.css({
+									'background-image': 'url(' + URL.createObjectURL(file) + ')'
+								});
+							} else {
+								var fileReader = new FileReader();
+								fileReader.readAsDataURL(file);
+								fileReader.onload = function (e) {
+									$timeout(function () {
+										elem.css({
+											'background-image': 'url(' + e.target.result + ')'
+										});
+									});
+								};
+							}
+						});
+					} else {
+						elem.css({
+							'background-image': attr.ngfDefaultSrc || ''
+						});
+					}
+				});
+			}
+		}
+	};
+}]);
