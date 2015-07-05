94bc68599229bb946117cab5ea33b648be97e169
NVNAT <natalia.vallinas@gmail.com>
2015-07-05 23:37:19 +0200

    edit profile styles

--- views/desktop/deviser/edit-info.php
+++ views/desktop/deviser/edit-info.php
@@ -40,7 +40,7 @@ $profile_photo_base64 = isset($deviser["media"]["profile"]) ? Utils::fileToBase6
 		<div class="row no-gutter">
 			<div class="col-xs-12 header-photo flex flex-justify-center">
 				<div class="header-photo-holder flex-justify-center">
-					<img class="img-circle header-photo-img" ngf-bg-src="headerphoto[0]" ngf-default-src="<?= $header_photo_base64 ?>">
+					<img class="header-photo-img" ngf-bg-src="headerphoto[0]" ngf-default-src="<?= $header_photo_base64 ?>">
 
 					<div class="flex flex-justify-center flex-align-center">
 						<div class="controls">
@@ -48,40 +48,45 @@ $profile_photo_base64 = isset($deviser["media"]["profile"]) ? Utils::fileToBase6
 							<span class="glyphicon glyphicon-resize-small pointer" aria-hidden="true" ng-click="crop_header()" ng-cloak ng-show="headerphoto[0] !== undefined"></span>
 						</div>
 
-						<div class="pointer drop-box flex flex-justify-center flex-align-center" ngf-drop ngf-select ng-model="headerphoto" class="drop-box"
-						     ngf-drag-over-class="dragover" ngf-multiple="false" ngf-allow-dir="false"
-						     ngf-accept="'image/*'" ngf-drop-available="dropAvailable">
-							<div ng-cloak ng-hide="dropAvailable">File Drop not available</div>
-							<div ng-cloak ng-show="dropAvailable && (!headerphoto || headerphoto.length === 0)" class="funiv fs1 fc-3d">Upload header photo</div>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="col-xs-12 profile-photo flex flex-justify-center">
-				<div class="profile-photo-holder flex-justify-center">
-					<img class="img-circle profile-photo-img" ngf-bg-src="profilephoto[0]" ngf-default-src="<?= $profile_photo_base64 ?>">
-
-					<div class="flex flex-justify-center flex-align-center">
-						<div class="controls">
-							<span class="glyphicon glyphicon-refresh pointer" aria-hidden="true" ngf-select ng-model="profilephoto" ng-cloak ng-show="profilephoto[0] !== undefined"></span>
-							<span class="glyphicon glyphicon-resize-small pointer" aria-hidden="true" ng-click="crop_profile()" ng-cloak ng-show="profilephoto[0] !== undefined"></span>
-						</div>
-
-						<div class="pointer drop-box flex flex-justify-center flex-align-center" ngf-drop ngf-select ng-model="profilephoto" class="drop-box"
+						<div class="pointer drop-box flex flex-justify-center" ngf-drop ngf-select ng-model="headerphoto" class="drop-box"
 							ngf-drag-over-class="dragover" ngf-multiple="false" ngf-allow-dir="false"
 							ngf-accept="'image/*'" ngf-drop-available="dropAvailable">
 							<div ng-cloak ng-hide="dropAvailable">File Drop not available</div>
-							<div ng-cloak ng-show="dropAvailable && (!profilephoto || profilephoto.length === 0)" class="funiv fs1 fc-3d">Upload profile photo</div>
+							<div ng-cloak ng-show="dropAvailable && (!headerphoto || headerphoto.length === 0)" class="funiv_ultra fs1 fc-3d fs-upper header-photo-txt">
+								<span class="glyphicon glyphicons-up-arrow"></span> Upload header photo
+							</div>
 						</div>
 					</div>
+
+					<div class="col-xs-12 profile-photo flex flex-justify-center">
+						<div class="profile-photo-holder flex-justify-center">
+							<img class="img-circle profile-photo-img" ngf-bg-src="profilephoto[0]" ngf-default-src="<?= $profile_photo_base64 ?>">
+
+							<div class="flex flex-justify-center flex-align-center">
+								<div class="controls">
+									<span class="glyphicon glyphicon-refresh pointer" aria-hidden="true" ngf-select ng-model="profilephoto" ng-cloak ng-show="profilephoto[0] !== undefined"></span>
+									<span class="glyphicon glyphicon-resize-small pointer" aria-hidden="true" ng-click="crop_profile()" ng-cloak ng-show="profilephoto[0] !== undefined"></span>
+								</div>
+
+								<div class="pointer drop-box flex flex-justify-center flex-align-center" ngf-drop ngf-select ng-model="profilephoto" class="drop-box"
+									ngf-drag-over-class="dragover" ngf-multiple="false" ngf-allow-dir="false"
+									ngf-accept="'image/*'" ngf-drop-available="dropAvailable">
+									<div ng-cloak ng-hide="dropAvailable">File Drop not available</div>
+									<div ng-cloak ng-show="dropAvailable && (!profilephoto || profilephoto.length === 0)" class="funiv_ultra fs1 fc-3d fs-upper">
+										<span class="glyphicon glyphicons-up-arrow"></span> <br/> Upload <br/>profile<br/> photo
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
 				</div>
 			</div>
 		</div>
 
 		<br />
 
-		<div class="row no-gutter">
+		<div class="row no-gutter deviser">
 			<div class="col-xs-4 flex flex-justify-center"></div>
 			<div class="col-xs-4 flex flex-justify-center">
 				<input type="text" placeholder="<?= Yii::t("app/deviser", "Your name"); ?>" ng-model="deviser.personal_info.name" class="input-name funiv_thin fs2-857 fc-9b fs-upper">
