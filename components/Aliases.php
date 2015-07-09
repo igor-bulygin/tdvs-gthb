1bbe823357ed9c6948b912ac67bccf47ca1f1cb7
Alexander Nestorov <alexandernst@gmail.com>
2015-07-09 14:52:41 +0200

    Add more aliases

--- components/Aliases.php
+++ components/Aliases.php
@@ -9,5 +9,9 @@ class Aliases extends Component {
 		Yii::setAlias('uploads', '@webroot/uploads');
 		Yii::setAlias('deviser', '@uploads/deviser');
 		Yii::setAlias('product', '@uploads/product');
+
+		Yii::setAlias('uploads_url', '/uploads');
+		Yii::setAlias('deviser_url', '@uploads_url/deviser');
+		Yii::setAlias('product_url', '@uploads_url/product');
 	}
 }
