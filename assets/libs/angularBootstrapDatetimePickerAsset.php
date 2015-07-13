72f591ccb18ac95f72fb03e29a098d5982898f23
Alexander Nestorov <alexandernst@gmail.com>
2015-07-13 15:47:31 +0200

    Add angular boostrap datetime picker asset

---
+++ assets/libs/angularBootstrapDatetimePickerAsset.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\assets\libs;
+
+use yii\web\AssetBundle;
+
+class angularBootstrapDatetimePickerAsset extends AssetBundle {
+	public $sourcePath = '@bower/angular-bootstrap-datetimepicker';
+	public $css = [
+		YII_ENV_DEV ? 'src/css/datetimepicker.css' : 'src/css/datetimepicker.css'
+	];
+	public $js = [
+		YII_ENV_DEV ? 'src/js/datetimepicker.js' : 'src/js/datetimepicker.js'
+	];
+	public $depends = [
+		'app\assets\libs\momentAsset',
+		'app\assets\libs\angularAsset'
+	];
+}
