383bccd40ae1ff90e1f5ff7643c29a57a4e6b22d
Alexander Nestorov <alexandernst@gmail.com>
2015-07-17 00:26:57 +0200

    Make sure the Crop widget can do both circle and square crops

--- components/views/Crop.php
+++ components/views/Crop.php
@@ -13,7 +13,26 @@ cropAsset::register($this);
 		<div class='modal-body'>
 
 			<div class="cropArea">
-				<img-crop image="photo" result-image="croppedphoto"></img-crop>
+				<img-crop area-type="square" image="photo" result-image="croppedphoto"></img-crop>
+			</div>
+
+		</div>
+		<div class='modal-footer'>
+			<button class='btn btn-success' ng-click='form.$submitted = true; form.$valid && ok()'><?php echo Yii::t("app/deviser", "Confirm"); ?></button>
+			<button class='btn btn-primary' ng-click='cancel()' type="submit"><?php echo Yii::t("app/deviser", "Cancel"); ?></button>
+		</div>
+	</form>
+</script>
+
+<script type="text/ng-template" id="template/modal/deviser/crop_circle.html">
+	<form novalidate name="form">
+		<div class='modal-header'>
+			<h3 class='modal-title'><?php echo Yii::t("app/deviser", "Crop image"); ?></h3>
+		</div>
+		<div class='modal-body'>
+
+			<div class="cropArea">
+				<img-crop area-type="circle" image="photo" result-image="croppedphoto"></img-crop>
 			</div>
 
 		</div>
