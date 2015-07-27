cc87fd26193bbc04bd220a00610505f82c9818a8
Alexander Nestorov <alexandernst@gmail.com>
2015-07-27 02:38:04 +0200

    We do use jsonpath in some remaining sections in /admin

--- assets/desktop/admin/TagAsset.php
+++ assets/desktop/admin/TagAsset.php
@@ -14,6 +14,7 @@ class TagAsset extends AssetBundle {
 	];
 	public $depends = [
 		'app\assets\api\ApiAsset',
+		'app\assets\libs\jsonpathAsset',
 		'app\assets\libs\angularUiUtilsAsset',
 		'app\assets\libs\angularMultiSelectAsset',
 		'app\assets\libs\angularBootstrapAsset',
--- assets/desktop/admin/TagsAsset.php
+++ assets/desktop/admin/TagsAsset.php
@@ -14,6 +14,7 @@ class TagsAsset extends AssetBundle {
 	];
 	public $depends = [
 		'app\assets\api\ApiAsset',
+		'app\assets\libs\jsonpathAsset',
 		'app\assets\libs\angularMultiSelectAsset',
 		'app\assets\libs\angularBootstrapAsset',
 		'app\assets\desktop\admin\GlobalAsset'
