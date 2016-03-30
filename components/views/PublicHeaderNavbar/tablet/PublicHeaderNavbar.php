4ccbdf63925ba411deb56b0a228e2297d909ea2a
Alexander Nestorov <alexandernst@gmail.com>
2016-03-30 21:30:45 +0200

    More oops

--- components/views/PublicHeaderNavbar/tablet/PublicHeaderNavbar.php
+++ components/views/PublicHeaderNavbar/tablet/PublicHeaderNavbar.php
@@ -2,4 +2,4 @@
 
 use app\helpers\Utils;
 
-require Utils::join_paths(Yii::getAlias("@app"), "components", "views", "PublicHeader", "desktop", basename(__FILE__));
+require Utils::join_paths(Yii::getAlias("@app"), "components", "views", "PublicHeaderNavbar", "desktop", basename(__FILE__));
