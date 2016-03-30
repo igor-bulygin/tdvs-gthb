94241849c17136018599de217e71253ec68eeaf0
Alexander Nestorov <alexandernst@gmail.com>
2016-03-30 14:26:29 +0200

    WIP header

--- components/views/PublicHeaderNavbar.php
+++ components/views/PublicHeaderNavbar.php
@@ -7,24 +7,47 @@ publicHeaderNavbarAsset::register($this);
 ?>
 
 <nav class="navbar-inverse navbar" role="navigation">
-	<div class="container-fluid container-menutop">
-		<div class="navbar-collapse">
-			<ul id="w1" class="navbar-nav flex menutop funiv_ultra fs1 nav">
-				<li class="item-menutop fs-upper flex-prop-1-0">
-					<?= Html::a(Yii::t("app/public", 'Find the perfect gift'), "site/index"); ?>
+	<div class="container-fluid container-menutop no-horizontal-padding">
+		<div class="navbar-collapse no-horizontal-padding">
+			<ul id="w1" class="flex menutop funiv_ultra fs0-857 no-padding no-margin">
+				<li class="item-menutop border fs-upper flex-prop-1-0">
+					<?= Html::a(Yii::t("app/public", 'Explore boxes'), "site/index", [
+						'class' => "hdr_btn funiv_bold fc-4a flex flex-align-center flex-justify-center"
+					]); ?>
+				</li>
+				<li class="item-menutop border fs-upper flex-prop-1-0">
+					<?= Html::a(Yii::t("app/public", 'Discover stories'), "site/about", [
+						'class' => "hdr_btn funiv_bold fc-4a flex flex-align-center flex-justify-center"
+					]); ?>
+				</li>
+				<li class="item-menutop border fs-upper flex-prop-1-0">
+					<?= Html::a(Yii::t("app/public", 'Our devisers'), "site/about", [
+						'class' => "hdr_btn funiv_bold fc-4a flex flex-align-center flex-justify-center"
+					]); ?>
 				</li>
 				<li class="item-menutop fs-upper flex-prop-1-0">
-					<?= Html::a(Yii::t("app/public", 'Discover trends'), "site/about"); ?>
+					<div class="search_box flex flex-justify-start white">
+						<input type="text" class="funiv_bold fs1-143 fc-4e flex-prop-1-0" name="name" value="" placeholder="<?= Yii::t("app/public", "Search...") ?>">
+						<!--
+						<select class="white" name="">
+							<option>Foo</option>
+							<option>Bar</option>
+							<option>Foo Bar</option>
+						</select>
+						-->
+						<span class="helper_btns pointer flex flex-align-center fs1-071 glyphicon glyphicon-search"></span>
+					</div>
 				</li>
-				<li class="item-menutop fs-upper flex-prop-1-0 flex-grow-3">
-					<?= Html::a(Yii::t("app/public", 'Caja de busqueda'), "site/about"); ?>
+				<li class="item-menutop">
+					<div class="hdr_btn funiv_bold flex flex-align-center flex-justify-center">
+						<span class="helper_btns pointer flex flex-align-center fs1-071 glyphicon glyphicon-shopping-cart"></span>
+					</div>
 				</li>
-				<li class="item-menutop flex-prop-1-0"><a href="#">IMG</a></li>
-				<li class="item-menutop fs-upper flex-prop-1-0 fpf_bold fs0-786">
-					<a href="#">TODEVISE</a>
-				</li>
-				<li class="item-menutop flex-prop-1-0">
-					<?= Html::a(Yii::t("app/public", 'Login'), "site/login"); ?>
+				<li class="item-menutop fs-upper funiv_bold fc-fff pointer">
+					<div class="my_account black flex flex-justify-center flex-align-center">
+						<span class="glyphicon glyphicon-user"></span>
+						<span class="access"><?= Yii::t("app/public", "My todevise") ?></span>
+					</div>
 				</li>
 			</ul>
 		</div>
