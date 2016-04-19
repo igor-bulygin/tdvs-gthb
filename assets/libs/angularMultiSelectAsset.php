9e4140a2968337536201d02013d174ff6faeed9d
Alexander Nestorov <alexandernst@gmail.com>
2016-04-19 04:43:44 +0200

    Upgrade to new AMS library

--- assets/libs/angularMultiSelectAsset.php
+++ assets/libs/angularMultiSelectAsset.php
@@ -5,14 +5,27 @@ namespace app\assets\libs;
 use yii\web\AssetBundle;
 
 class angularMultiSelectAsset extends AssetBundle {
-	public $sourcePath = '@npm/angular-multi-select/dist';
+	public $sourcePath = '@npm/angular-multi-select';
 	public $css = [
-		YII_ENV_DEV ? 'angular-multi-select.css' : 'angular-multi-select.min.css'
+		YII_ENV_DEV ? 'dist/dev/angular-multi-select.css' : 'dist/prod/angular-multi-select.min.css'
 	];
-	public $js = [
-		YII_ENV_DEV ? 'angular-multi-select.js' : 'angular-multi-select.min.js'
+	public $cssOptions = [
+		"crossorigin" => "anonymous"
+	];
+	public $js = YII_ENV_DEV ? [
+		'dist/dev/angular-multi-select-constants.js',
+		'dist/dev/angular-multi-select-data-converter.js',
+		'dist/dev/angular-multi-select-engine.js',
+		'dist/dev/angular-multi-select-styles-helper.js',
+		'dist/dev/angular-multi-select-utils.js',
+		'dist/dev/angular-multi-select.js',
+		'dist/dev/angular-multi-select.tpl.js',
+		'dist/dev/angular-multi-select-filters.js',
+		'dist/dev/angular-multi-select-i18n.js'
+	] : [
+		'dist/prod/angular-multi-select.min.js'
 	];
 	public $depends = [
-		'app\assets\libs\angularFilterAsset'
+		'app\assets\libs\lokijsAsset'
 	];
 }
