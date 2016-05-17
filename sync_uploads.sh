62e11d8d301b12a919d8b91c6ff6287ccab3dc37
Alexander Nestorov <alexandernst@gmail.com>
2016-05-17 03:47:58 +0200

    Fix sync script

--- sync_uploads.sh
+++ sync_uploads.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -razhe ssh --progress dev2@dev.todevise.com:/var/www/dev2.todevise.com/web/web/uploads/ .
+rsync -razhe ssh --progress dev2@dev.todevise.com:/var/www/dev2.todevise.com/web/web/uploads/ ./web/uploads/
