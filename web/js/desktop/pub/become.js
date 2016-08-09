4f827070695c7506cd415e19fb463364be27bc7e
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-08-09 10:44:30 +0200

    Change indentation on desktop/public/become.

--- web/js/desktop/pub/become.js
+++ web/js/desktop/pub/become.js
@@ -3,39 +3,39 @@
  */
 
 (function () {
-    "use strict";
+	"use strict";
 
-    function becomeCtrl($cacheFactory) {
-        var vm = this;
+	function becomeCtrl($cacheFactory) {
+		var vm = this;
 
-        function newItem() {
-            return {
-                link: ''
-            };
-        }
+		function newItem() {
+			return {
+				link: ''
+			};
+		}
 
-        function init() {
-            vm.portfolio_links = [{
-                link: ''
+		function init() {
+			vm.portfolio_links = [{
+				link: ''
             }];
 
-            vm.video_links = [{
-                link: ''
+			vm.video_links = [{
+				link: ''
             }]
-        }
+		}
 
-        init();
+		init();
 
-        vm.newVideoLink = function () {
-            vm.video_links.push(newItem());
-        }
+		vm.newVideoLink = function () {
+			vm.video_links.push(newItem());
+		}
 
-        vm.newPortFolioLink = function () {
-            vm.portfolio_links.push(newItem());
-        }
-    }
+		vm.newPortFolioLink = function () {
+			vm.portfolio_links.push(newItem());
+		}
+	}
 
-    angular.module('todevise', [])
-        .controller('becomeCtrl', becomeCtrl);
+	angular.module('todevise', [])
+		.controller('becomeCtrl', becomeCtrl);
 
 }());
