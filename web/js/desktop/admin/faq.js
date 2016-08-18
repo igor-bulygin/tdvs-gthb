ea323f7a8e6744acab9a29c16a459e59a0a8f587
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-08-18 09:43:07 +0200

    Controllers & view models in desktop/admin/faq.

--- web/js/desktop/admin/faq.js
+++ web/js/desktop/admin/faq.js
@@ -1,115 +1,107 @@
-var todevise = angular.module('todevise', ['ngAnimate', 'ui.bootstrap', 'ngJsTree', 'global-admin', 'global-desktop', 'api']);
+(function () {
+	"use strict";
 
-todevise.controller('faqCtrl', ["$scope", "$faq", "$category_util", "$location", "toastr", "$uibModal", function($scope, $faq, $category_util, $location, toastr, $uibModal) {
+	function controller($faq, $category_util, $location, toastr, $uibModal) {
+		var vm = this;
 
-	$scope.treeData = [];
-	$scope.langs = _langs;
-	$scope.oneAtATime = false;
-	$scope.faq_id = _faq_id;
-	$scope.faq_subid = _faq_subid;
-	$scope.faq={};
+		vm.treeData = [];
+		vm.langs = _langs;
+		vm.oneAtATime = false;
+		vm.faq_id = _faq_id;
+		vm.faq_subid = _faq_subid;
+		vm.faq = {};
 
-	console.log(_faq_id);
-	console.log(_faq_subid);
+		console.log(_faq_id);
+		console.log(_faq_subid);
 
+		vm.load = load;
+		vm.save = save;
+		vm.create_sub = create_sub;
 
-	$scope.load = function () {
-
-		$faq.get({
-			"short_id": $scope.faq_id
-		}).then(function(_faq) {
-			if(_faq.length !== 1) {
-				toastr.error("Unexpected category details!");
-				return;
-			} else {
-				_faq = _faq.pop();
-				console.log(_faq);
-
-				$scope.faq = _faq;
-				if($scope.faq_subid.length <= 0){
-					$scope.faq_subid = $scope.faq.faqs.push({answer: {}, question: {}});
-					$scope.faq_subid = $scope.faq_subid - 1;
-				}
-				$scope.subfaq = $scope.faq.faqs[$scope.faq_subid];
-
-				console.log($scope.subfaq);
-			}
-
-		}, function(err) {
-			toastr.error("Couldn't get faqs details!", err);
-		});
-	};
-
-
-	$scope.load();
-
-
-	$scope.save = function(){
-
-		$scope.faq.faqs[$scope.faq_subid] = $scope.subfaq;
-
-		$faq.modify("POST", $scope.faq).then(function() {
-			toastr.success("Faq modified!");
-
-			//var _node = $category_util.categoryToNode(data.category);
-			//var _current = _.findWhere($scope.treeData, {id: data.category.short_id});
-			//angular.merge(_current, _node);
-
-		}, function(err) {
-			toastr.error("Couldn't modify category!", err);
-		});
-
-
-	};
-
-
-	$scope.groups = [
-		{
-			title: 'Dynamic Group Header - 1',
-			content: 'Dynamic Group Body - 1'
+		vm.groups = [
+			{
+				title: 'Dynamic Group Header - 1',
+				content: 'Dynamic Group Body - 1'
 		},
-		{
-			title: 'Dynamic Group Header - 2',
-			content: 'Dynamic Group Body - 2'
+			{
+				title: 'Dynamic Group Header - 2',
+				content: 'Dynamic Group Body - 2'
 		}
 	];
 
-	$scope.status = {
-		isCustomHeaderOpen: false,
-		isFirstOpen: true,
-		isFirstDisabled: false
-	};
+		vm.status = {
+			isCustomHeaderOpen: false,
+			isFirstOpen: true,
+			isFirstDisabled: false
+		};
 
+		vm.load();
 
-	$scope.create_sub = function(node_id, node, action_id, action_el) {
-		var path = "/";
-		if (node !== undefined) {
-			path += $scope.treeInstance.jstree(true).get_path(node, "/", true) + "/";
+		function load() {
+			$faq.get({
+				"short_id": vm.faq_id
+			}).then(function (_faq) {
+				if (_faq.length !== 1) {
+					toastr.error("Unexpected category details!");
+					return;
+				} else {
+					_faq = _faq.pop();
+					console.log(_faq);
+					vm.faq = _faq;
+					if (vm.faq_subid.length <= 0) {
+						vm.faq_subid = vm.faq.faqs.push({
+							answer: {},
+							question: {}
+						});
+						vm.faq_subid = vm.faq_subid - 1;
+					}
+					vm.subfaq = vm.faq.faqs[vm.faq_subid];
+					console.log(vm.subfaq);
+				}
+			}, function (err) {
+				toastr.error("Couldn't get faqs details!", err);
+			});
 		}
 
-		$faq.modify("POST", tmp_node).then(function(faqs_group) {
-			toastr.success("Category created!");
-			console.log("debug id:");
-			console.log(faqs_group);
-			$scope.treeData.push(
-				{
+		function save() {
+			vm.faq.faqs[vm.faq_subid] = vm.subfaq;
+
+			$faq.modify("POST", vm.faq).then(function () {
+				toastr.success("Faq modified!");
+
+				//var _node = $category_util.categoryToNode(data.category);
+				//var _current = _.findWhere(vm.treeData, {id: data.category.short_id});
+				//angular.merge(_current, _node);
+
+			}, function (err) {
+				toastr.error("Couldn't modify category!", err);
+			});
+		}
+
+		function create_sub(node_id, node, action_id, action_el) {
+			var path = "/";
+			if (node !== undefined) {
+				path += vm.treeInstance.jstree(true).get_path(node, "/", true) + "/";
+			}
+
+			$faq.modify("POST", tmp_node).then(function (faqs_group) {
+				toastr.success("Category created!");
+				console.log("debug id:");
+				console.log(faqs_group);
+				vm.treeData.push({
 					id: faqs_group.short_id,
 					parent: '#',
 					text: faqs_group.title[_lang] || "",
 					icon: "glyphicon glyphicon-menu-hamburger"
-				}
-			);
-		}, function(err) {
-			toastr.error("Couldn't create category!", err);
-		});
-	};
+				});
+			}, function (err) {
+				toastr.error("Couldn't create category!", err);
+			});
+		}
+	}
 
+	angular
+		.module('todevise', ['ngAnimate', 'ui.bootstrap', 'ngJsTree', 'global-admin', 'global-desktop', 'api'])
+		.controller('faqCtrl', controller);
 
-
-
-
-
-
-
-
-}]);
+}());
