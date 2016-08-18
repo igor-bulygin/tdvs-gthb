c02feff4b67f679b4052e14cc2aeb37adb357cab
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-08-18 10:10:24 +0200

    Controller & view model in desktop/admin/global.

--- web/js/desktop/admin/global.js
+++ web/js/desktop/admin/global.js
@@ -1,34 +1,41 @@
-console.log("Global desktop admin");
-var global_admin = angular.module('global-admin', ['toastr', 'template/modal/confirm.html']);
+(function () {
+	console.log("Global desktop admin");
 
-global_admin.controller("confirmCtrl", ["$scope", "$uibModalInstance", "data", function($scope, $uibModalInstance, data) {
-	$scope.data = angular.extend({}, {
-		title: "Please confirm",
-		text: "",
-		ok: "Ok",
-		cancel: "Cancel"
-	}, data);
+	function controller($uibModalInstance, data) {
+		var vm = this;
 
-	$scope.ok = function() {
-		$uibModalInstance.close();
+		vm.data = angular.extend({}, {
+			title: "Please confirm",
+			text: "",
+			ok: "Ok",
+			cancel: "Cancel"
+		}, data);
+
+		vm.ok = function () {
+			$uibModalInstance.close();
+		}
+
+		vm.cancel = function () {
+			$uibModalInstance.dismiss();
+		}
 	};
 
-	$scope.cancel =  function() {
-		$uibModalInstance.dismiss();
-	};
-}]);
+	angular.module('global-admin', ['toastr', 'template/modal/confirm.html'])
+		.controller('confirmCtrl', controller);
 
-angular.module("template/modal/confirm.html", []).run(["$templateCache", function($templateCache) {
-	$templateCache.put("template/modal/confirm.html",
-		"<div class='modal-header'>" +
-		"	<h3 class='modal-title'>{{ data.title }}</h3>" +
-		"</div>" +
-		"<div class='modal-body'>" +
-		"	<div>{{ data.text }}</div>" +
-		"</div>" +
-		"<div class='modal-footer'>" +
-		"	<button class='btn btn-danger' ng-click='ok()'>{{ data.ok }}</button>" +
-		"	<button class='btn btn-primary' ng-click='cancel()'>{{ data.cancel }}</button>" +
-		"</div>"
-	);
-}]);
+	angular.module("template/modal/confirm.html", [])
+		.run(["$templateCache", function ($templateCache) {
+			$templateCache.put("template/modal/confirm.html",
+				"<div class='modal-header'>" +
+				"	<h3 class='modal-title'>{{ data.title }}</h3>" +
+				"</div>" +
+				"<div class='modal-body'>" +
+				"	<div>{{ data.text }}</div>" +
+				"</div>" +
+				"<div class='modal-footer'>" +
+				"	<button class='btn btn-danger' ng-click='ok()'>{{ data.ok }}</button>" +
+				"	<button class='btn btn-primary' ng-click='cancel()'>{{ data.cancel }}</button>" +
+				"</div>"
+			);
+			}]);
+}());
