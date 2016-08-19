884f59931f324c1606da24946cb4b6b70224f8db
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-08-19 14:43:39 +0200

    Controller & view model in desktop/admin/terms.

--- views/desktop/admin/terms.php
+++ views/desktop/admin/terms.php
@@ -13,7 +13,7 @@ TermsAsset::register($this);
 $this->title = 'Todevise / Admin / Terms';
 ?>
 
-<div class="row no-gutter" ng-controller="termsCtrl">
+<div class="row no-gutter" ng-controller="termsCtrl as termsCtrl">
 	<div class="col-xs-12 no-horizontal-padding">
 		<div class="row no-gutter page-title-row bgcolor-3d">
 			<div class="row-same-height">
@@ -24,67 +24,67 @@ $this->title = 'Todevise / Admin / Terms';
 				<div class="col-xs-5 col-height col-middle">
 					<div class="input-group">
 						<span class="input-group-addon" id="basic-addon1">Search</span>
-						<input type="text" class="form-control" placeholder="<?= Yii::t("app/admin", "Category name..."); ?>" aria-describedby="basic-addon1" ng-model="searchModel" ng-change="search()">
+						<input type="text" class="form-control" placeholder="<?= Yii::t("app/admin", "Category name..."); ?>" aria-describedby="basic-addon1" ng-model="termsCtrl.searchModel" ng-change="termsCtrl.search()">
 					</div>
 				</div>
 
 				<div class="col-xs-4 col-height col-middle text-center">
-					<button class="btn btn-light-green fc-fff funiv fs0-786 fs-upper margin-l0-r1" ng-click="open_all()"><?= Yii::t("app/admin", "Open all"); ?></button>
-					<button class="btn btn-coral fc-fff funiv fs0-786 fs-upper margin-l0-r1" ng-click="close_all()"><?= Yii::t("app/admin", "Close all"); ?></button>
-					<button class="btn btn-purple fc-fff funiv fs0-786 fs-upper" ng-click="create()"><?= Yii::t("app/admin", "Create new category"); ?></button>
+					<button class="btn btn-light-green fc-fff funiv fs0-786 fs-upper margin-l0-r1" ng-click="termsCtrl.open_all()"><?= Yii::t("app/admin", "Open all"); ?></button>
+					<button class="btn btn-coral fc-fff funiv fs0-786 fs-upper margin-l0-r1" ng-click="termsCtrl.close_all()"><?= Yii::t("app/admin", "Close all"); ?></button>
+					<button class="btn btn-purple fc-fff funiv fs0-786 fs-upper" ng-click="termsCtrl.create()"><?= Yii::t("app/admin", "Create new category"); ?></button>
 				</div>
 			</div>
 		</div>
 
-		<div js-tree="treeConfig" ng-model="treeData" tree="treeInstance"
-			tree-events="ready:readyCB;create_node:restoreState;move_node:move">
+		<div js-tree="termsCtrl.treeConfig" ng-model="termsCtrl.treeData" tree="termsCtrl.treeInstance"
+			tree-events="ready:termsCtrl.readyCB;create_node:termsCtrl.restoreState;move_node:termsCtrl.move">
 		</div>
 
 	</div>
 </div>
 
 <script type="text/ng-template" id="template/modal/terms/create_new.html">
-	<form novalidate name="form" class="popup-tag">
+	<form novalidate name="create_newCtrl.form" class="popup-tag">
 		<div class='modal-header'>
 			<h3 class='modal-title funiv fs1'><?= Yii::t("app/admin", "Create Term Group"); ?></h3>
 		</div>
 		<div class='modal-body'>
 			<label class="modal-title funiv fs1 fnormal fc-18"><?= Yii::t("app/admin", "Group name"); ?></label>
-			<div class="input-group" ng-repeat="(lang_k, lang_v) in data.langs">
+			<div class="input-group" ng-repeat="(lang_k, lang_v) in create_newCtrl.data.langs">
 				<span class="input-group-addon funiv_bold fs1 fs-upper" id="basic-addon-{{ $index }}">{{ lang_v }}</span>
-				<input required="" type="text" class="form-control funiv fs1" placeholder="<?= Yii::t("app/admin", "Group name..."); ?>" aria-describedby="basic-addon-{{ $index }}" ng-model="langs[lang_k]" name="{{ lang_k }}">
-				<span class="input-group-addon alert-danger funiv fs0-929" ng-show="form.$submitted && !form.$valid && !form['{{lang_k}}'].$valid">
-					<span ng-show="form['{{lang_k}}'].$error.required"><?= Yii::t("app/admin", "Required!"); ?></span>
+				<input required="" type="text" class="form-control funiv fs1" placeholder="<?= Yii::t("app/admin", "Group name..."); ?>" aria-describedby="basic-addon-{{ $index }}" ng-model="create_newCtrl.langs[lang_k]" name="{{ lang_k }}">
+				<span class="input-group-addon alert-danger funiv fs0-929" ng-show="create_newCtrl.form.$submitted && !create_newCtrl.form.$valid && !create_newCtrl.form['{{lang_k}}'].$valid">
+					<span ng-show="create_newCtrl.form['{{lang_k}}'].$error.required"><?= Yii::t("app/admin", "Required!"); ?></span>
 				</span>
 			</div>
 
 		</div>
 		<div class='modal-footer'>
-			<button class='btn btn-light-green fc-18 funiv fs0-786 fs-upper' ng-click='form.$submitted = true; form.$valid && ok()'><?= Yii::t("app/admin", "Confirm"); ?></button>
-			<button class='btn btn-grey fc-fff funiv fs-upper fs0-786' ng-click='cancel()' type="submit"><?= Yii::t("app/admin", "Cancel"); ?></button>
+			<button class='btn btn-light-green fc-18 funiv fs0-786 fs-upper' ng-click='create_newCtrl.form.$submitted = true; create_newCtrl.form.$valid && create_newCtrl.ok()'><?= Yii::t("app/admin", "Confirm"); ?></button>
+			<button class='btn btn-grey fc-fff funiv fs-upper fs0-786' ng-click='create_newCtrl.cancel()' type="submit"><?= Yii::t("app/admin", "Cancel"); ?></button>
 		</div>
 	</form>
 </script>
 
 <script type="text/ng-template" id="template/modal/terms/edit.html">
-	<form novalidate name="form" class="popup-tag">
+	<form novalidate name="editCtrl.form" class="popup-tag">
 		<div class='modal-header'>
 			<h3 class='modal-title funiv fs1'><?= Yii::t("app/admin", "Edit category"); ?></h3>
 		</div>
 		<div class='modal-body'>
 			<label class="modal-title funiv fs1 fnormal fc-18"><?= Yii::t("app/admin", "Category name"); ?></label>
-			<div class="input-group" ng-repeat="(lang_k, lang_v) in data.langs">
+			<div class="input-group" ng-repeat="(lang_k, lang_v) in editCtrl.data.langs">
 				<span class="input-group-addon funiv_bold fs1 fs-upper" id="basic-addon-{{ $index }}">{{ lang_v }}</span>
-				<input required="" type="text" class="form-control funiv fs1" placeholder="<?= Yii::t("app/admin", "Category name..."); ?>" aria-describedby="basic-addon-{{ $index }}" ng-model="data.category.name[lang_k]" name="{{ lang_k }}">
-				<span class="input-group-addon alert-danger funiv fs0-929" ng-show="form.$submitted && !form.$valid && !form['{{lang_k}}'].$valid">
-					<span ng-show="form['{{lang_k}}'].$error.required"><?= Yii::t("app/admin", "Required!"); ?></span>
+				<input required="" type="text" class="form-control funiv fs1" placeholder="<?= Yii::t("app/admin", "Category name..."); ?>" aria-describedby="basic-addon-{{ $index }}" ng-model="editCtrl.data.category.name[lang_k]" name="{{ lang_k }}">
+				<span class="input-group-addon alert-danger funiv fs0-929" ng-show="editCtrl.form.$submitted && !editCtrl.form.$valid && !editCtrl.form['{{lang_k}}'].$valid">
+					<span ng-show="editCtrl.form['{{lang_k}}'].$error.required"><?= Yii::t("app/admin", "Required!"); ?></span>
 				</span>
 			</div>
 
 		</div>
 		<div class='modal-footer'>
-			<button class='btn btn-light-green fc-18 funiv fs0-786 fs-upper' ng-click='form.$submitted = true; form.$valid && ok()'><?= Yii::t("app/admin", "Confirm"); ?></button>
-			<button class='btn btn-grey fc-fff funiv fs-upper fs0-786' ng-click='cancel()' type="submit"><?= Yii::t("app/admin", "Cancel"); ?></button>
+			<button class='btn btn-light-green fc-18 funiv fs0-786 fs-upper' ng-click='editCtrl.form.$submitted = true; editCtrl.form.$valid && editCtrl.ok()'><?= Yii::t("app/admin", "Confirm"); ?></button>
+			<button class='btn btn-grey fc-fff funiv fs-upper fs0-786' ng-click='editCtrl.cancel()' type="submit"><?= Yii::t("app/admin", "Cancel"); ?></button>
 		</div>
 	</form>
 </script>
--- web/js/desktop/admin/terms.js
+++ web/js/desktop/admin/terms.js
@@ -1,277 +1,263 @@
-var todevise = angular.module('todevise', ['ngAnimate', 'ui.bootstrap', 'ngJsTree', 'global-admin', 'global-desktop', 'api']);
+(function () {
+	"use strict";
 
-todevise.controller('termsCtrl', ["$scope", "$terms", "$category_util", "$location", "toastr", "$uibModal", function($scope, $terms, $category_util, $location, toastr, $uibModal) {
+	function controller($terms, $category_util, $location, toastr, $uibModal, treeService) {
+		var vm = this;
+		vm.treeData = [];
+		vm.langs = _langs;
 
-	$scope.treeData = [];
-	$scope.langs = _langs;
+		vm.treeConfig = treeService.treeDefaultConfig("todevise_categories_jstree", vm);
 
-	$scope.treeConfig = {
-		core : {
-			check_callback: true,
-			themes: {
-				name: "default-dark"
-			}
-		},
-		state : {
-			key : "todevise_categories_jstree",
-			filter: function(state) {
-				$scope.tree_state = {};
-				angular.copy(state, $scope.tree_state);
-			}
-		},
-		dnd: {
-			touch: "selected"
-		},
-		search: {
-			fuzzy: false,
-			case_insensitive: true,
-			show_only_matches: true,
-			search_only_leaves: false
-		},
-		plugins: ["state", "dnd", "search", "sort", "wholerow", "actions", "types"]
-	};
+		vm.readyCB = readyCB;
+		vm.load_terms = load_terms;
+		vm.search = search;
+		vm.open_all = open_all;
+		vm.close_all = close_all;
+		vm.restoreState = _.debounce(restoreState, 100);
+		vm.create = create;
+		vm.edit_term = edit_term;
+		vm.create_sub = create_sub;
+		vm.edit_sub = edit_sub;
+		vm.rename = rename;
+		vm.move = move;
+		vm.remove = remove;
 
-	$scope.readyCB = function () {
-		$scope.treeInstance.jstree(true).add_action("all", {
-			"id": "action_rename",
-			"class": "action_rename pull-right",
-			"text": "",
-			"after": true,
-			"selector": "a",
-			"event": "click",
-			"callback": $scope.rename
-		});
+		function readyCB() {
+			vm.treeInstance.jstree(true).add_action("all", {
+				id: "action_rename",
+				class: "action_rename pull-right",
+				text: "",
+				after: true,
+				selector: "a",
+				event: "click",
+				callback: vm.rename
+			});
 
-		$scope.treeInstance.jstree(true).add_action("all", {
-			"id": "action_remove",
-			"class": "action_remove pull-right",
-			"text": "",
-			"after": true,
-			"selector": "a",
-			"event": "click",
-			"callback": $scope.remove
-		});
+			vm.treeInstance.jstree(true).add_action("all", {
+				id: "action_remove",
+				class: "action_remove pull-right",
+				text: "",
+				after: true,
+				selector: "a",
+				event: "click",
+				callback: vm.remove
+			});
 
-		$scope.load_terms();
-	};
+			vm.load_terms();
+		}
 
-	$scope.load_terms = function() {
-		$terms.get().then(function(_terms) {
+		function load_terms() {
+			$terms.get().then(function (_terms) {
 
-			//Empty the data holder (if there is anything)
-			$scope.treeData.splice(0, $scope.treeData.length);
+				//Empty the data holder (if there is anything)
+				vm.treeData.splice(0, vm.treeData.length);
 
-			console.log(_terms);
-			//Fill the new values
-			angular.forEach(_terms, function(terms_group, key){
-				//console.log(terms_group.title[_lang]);
-				short_id = terms_group.short_id;
-				$scope.treeInstance.jstree(true).add_action(short_id, {
-					"id": "action_add",
-					"class": "action_add pull-right",
-					"text": "",
-					"after": true,
-					"selector": "a",
-					"event": "click",
-					"callback": $scope.edit_term
-				});
-				//a group
-				$scope.treeData.push(
-					{
+				console.log(_terms);
+				//Fill the new values
+				angular.forEach(_terms, function (terms_group, key) {
+					//console.log(terms_group.title[_lang]);
+					var short_id = terms_group.short_id;
+					vm.treeInstance.jstree(true).add_action(short_id, {
+						id: "action_add",
+						class: "action_add pull-right",
+						text: "",
+						after: true,
+						selector: "a",
+						event: "click",
+						callback: vm.edit_term
+					});
+					//a group
+					vm.treeData.push({
 						id: short_id,
 						parent: '#',
 						text: terms_group.title[_lang] || "",
 						icon: "none"
-					}
-				);
+					});
 
-				if(terms_group.terms.length > 0){
-					//terms inside group
-					angular.forEach(terms_group.terms, function (terms, key) {
-						console.log(terms.question[_lang]);
-						$scope.treeData.push(
-							{
-								id: short_id+'_'+key,
+					if (terms_group.terms.length > 0) {
+						//terms inside group
+						angular.forEach(terms_group.terms, function (terms, key) {
+							console.log(terms.question[_lang]);
+							vm.treeData.push({
+								id: short_id + '_' + key,
 								short_id: key,
 								parent: short_id,
 								text: terms.question[_lang] || "",
 								icon: "none"
-							}
-						);
-					});
-				}
-
-
-			});
-			console.log($scope.treeData);
-			toastr.success("Categories loaded!");
-		}, function(err) {
-			toastr.error("Couldn't load categories!", err);
-		});
-	};
-
-	$scope.search = function() {
-		$scope.treeInstance.jstree(true).search($scope.searchModel);
-	};
-
-	$scope.open_all = function() {
-		$scope.treeInstance.jstree(true).open_all();
-	};
-
-	$scope.close_all = function() {
-		$scope.treeInstance.jstree(true).close_all();
-	};
-
-	$scope.restoreState = _.debounce(function() {
-		$scope.treeInstance.jstree(true).set_state(angular.copy($scope.tree_state));
-	}, 100);
-
-	$scope.create = function(node_id, node, action_id, action_el) {
-		var path = "/";
-		if (node !== undefined) {
-			path += $scope.treeInstance.jstree(true).get_path(node, "/", true) + "/";
-		}
-
-		var modalInstance = $uibModal.open({
-			templateUrl: 'template/modal/terms/create_new.html',
-			controller: 'create_newCtrl',
-			resolve: {
-				data: function () {
-					return {
-						langs: _langs
-					};
-				}
-			}
-		});
-
-		modalInstance.result.then(function(data) {
-
-			var tmp_node = {
-				short_id: "new",
-				title: data.langs
-			};
-
-			$terms.modify("POST", tmp_node).then(function(terms_group) {
-				toastr.success("Category created!");
-				console.log("debug id:");
-				console.log(terms_group);
-				$scope.treeData.push(
-					{
-						id: terms_group.short_id,
-						parent: '#',
-						text: terms_group.title[_lang] || "",
-						icon: "none"
+							});
+						});
 					}
-				 );
-			}, function(err) {
-				toastr.error("Couldn't create category!", err);
+
+
+				});
+				console.log(vm.treeData);
+				toastr.success("Categories loaded!");
+			}, function (err) {
+				toastr.error("Couldn't load categories!", err);
 			});
-		}, function () {
-			//Cancel
-		});
-	};
-
-	$scope.edit_term = function(node_id, node, action_id, action_el) {
-		//$scope.viewtoggle = ! $scope.viewtoggle;
-		//TODO: Get this url from Yii url generator
-		document.location.href = "/admin/term/"+node.id+"/";
-	};
-
-
-	$scope.create_sub = function(node_id, node, action_id, action_el) {
-		var path = "/";
-		if (node !== undefined) {
-			path += $scope.treeInstance.jstree(true).get_path(node, "/", true) + "/";
 		}
 
-		$terms.modify("POST", tmp_node).then(function(terms_group) {
-			toastr.success("Category created!");
-			console.log("debug id:");
-			console.log(terms_group);
-			$scope.treeData.push(
-				{
-					id: terms_group.short_id,
-					parent: '#',
-					text: terms_group.title[_lang] || "",
-					icon: "glyphicon glyphicon-menu-hamburger"
-				}
-			 );
-		}, function(err) {
-			toastr.error("Couldn't create category!", err);
-		});
-	};
-
-	$scope.edit_sub = function (node_id, node, action_id, action_el) {
-		var res = node.id.split("_");
-		document.location.href = "/admin/term/"+res[0]+"/"+res[1]+"/";
-
-	}
-
-	$scope.rename = function (node_id, node, action_id, action_el) {
-
-		if(node.parent != '#'){
-			return $scope.edit_sub(node_id, node, action_id, action_el);
+		function search() {
+			vm.treeInstance.jstree(true).search(vm.searchModel);
 		}
 
-		$terms.get({
-			"short_id": node.id
-		}).then(function(_term) {
-			if(_term.length !== 1) {
-				toastr.error("Unexpected category details!");
-				return;
-			} else {
-				_term = _term.pop();
+		function open_all() {
+			vm.treeInstance.jstree(true).open_all();
+		}
+
+		function close_all() {
+			vm.treeInstance.jstree(true).close_all();
+
+		}
+
+		function restoreState() {
+			vm.treeInstance.jstree(true).set_state(angular.copy(vm.tree_state));
+		}
+
+		function create(node_id, node, action_id, action_el) {
+			var path = "/";
+			if (node !== undefined) {
+				path += vm.treeInstance.jstree(true).get_path(node, "/", true) + "/";
 			}
 
 			var modalInstance = $uibModal.open({
-				templateUrl: 'template/modal/terms/edit.html',
-				controller: 'editCtrl',
+				templateUrl: 'template/modal/terms/create_new.html',
+				controller: create_newCtrl,
+				controllerAs: 'create_newCtrl',
 				resolve: {
 					data: function () {
 						return {
-							langs: _langs,
-							term: _term
+							langs: _langs
 						};
 					}
 				}
 			});
 
-			modalInstance.result.then(function(data) {
-				$terms.modify("POST", data.category).then(function() {
-					toastr.success("term modified!");
+			modalInstance.result.then(function (data) {
 
-					var _node = $category_util.categoryToNode(data.category);
-					var _current = _.findWhere($scope.treeData, {id: data.category.short_id});
-					angular.merge(_current, _node);
-				}, function(err) {
-					toastr.error("Couldn't modify category!", err);
+				var tmp_node = {
+					short_id: "new",
+					title: data.langs
+				};
+
+				$terms.modify("POST", tmp_node).then(function (terms_group) {
+					toastr.success("Category created!");
+					console.log("debug id:");
+					console.log(terms_group);
+					vm.treeData.push({
+						id: terms_group.short_id,
+						parent: '#',
+						text: terms_group.title[_lang] || "",
+						icon: "none"
+					});
+				}, function (err) {
+					toastr.error("Couldn't create category!", err);
 				});
-
 			}, function () {
 				//Cancel
 			});
-		}, function(err) {
-			toastr.error("Couldn't get terms details!", err);
-		});
-	};
 
-	$scope.move = function (e, node) {
-		node.node.original.parent = node.node.parent;
-		var tmp_node = $category_util.nodeToCategory(node.node.original, $scope);
-		$terms.modify("post", tmp_node).then(function(category) {
-			$scope.load_terms();
-			toastr.success("Category moved!");
-		}, function(err) {
-			toastr.error("Couldn't move category!", err);
-		});
-	};
+		}
 
-	$scope.remove = function (node_id, node, action_id, action_el) {
-		var category = $scope.treeInstance.jstree(true).get_node(node.id);
-		category = $category_util.nodeToCategory(category.original, $scope);
+		function edit_term(node_id, node, action_id, action_el) {
+			//vm.viewtoggle = ! vm.viewtoggle;
+			//TODO: Get this url from Yii url generator
+			document.location.href = "/admin/term/" + node.id + "/";
+		}
 
-		//Check for sub-categories that depend on this one so we can remove those too
-		// $category_util.subCategories(category).then(function(subcategories) {
+		function create_sub(node_id, node, action_id, action_el) {
+			var path = "/";
+			if (node !== undefined) {
+				path += vm.treeInstance.jstree(true).get_path(node, "/", true) + "/";
+			}
+
+			$terms.modify("POST", tmp_node).then(function (terms_group) {
+				toastr.success("Category created!");
+				console.log("debug id:");
+				console.log(terms_group);
+				vm.treeData.push({
+					id: terms_group.short_id,
+					parent: '#',
+					text: terms_group.title[_lang] || "",
+					icon: "glyphicon glyphicon-menu-hamburger"
+				});
+			}, function (err) {
+				toastr.error("Couldn't create category!", err);
+			});
+		}
+
+		function edit_sub(node_id, node, action_id, action_el) {
+			var res = node.id.split("_");
+			document.location.href = "/admin/term/" + res[0] + "/" + res[1] + "/";
+		}
+
+		function rename(node_id, node, action_id, action_el) {
+			if (node.parent != '#') {
+				return vm.edit_sub(node_id, node, action_id, action_el);
+			}
+
+			$terms.get({
+				short_id: node.id
+			}).then(function (_term) {
+				if (_term.length !== 1) {
+					toastr.error("Unexpected category details!");
+					return;
+				} else {
+					_term = _term.pop();
+				}
+
+				var modalInstance = $uibModal.open({
+					templateUrl: 'template/modal/terms/edit.html',
+					controller: editCtrl,
+					controllerAs: 'editCtrl',
+					resolve: {
+						data: function () {
+							return {
+								langs: _langs,
+								term: _term
+							};
+						}
+					}
+				});
+
+				modalInstance.result.then(function (data) {
+					$terms.modify("POST", data.category).then(function () {
+						toastr.success("term modified!");
+
+						var _node = $category_util.categoryToNode(data.category);
+						var _current = _.findWhere(vm.treeData, {
+							id: data.category.short_id
+						});
+						angular.merge(_current, _node);
+					}, function (err) {
+						toastr.error("Couldn't modify category!", err);
+					});
+
+				}, function () {
+					//Cancel
+				});
+			}, function (err) {
+				toastr.error("Couldn't get terms details!", err);
+			});
+		}
+
+		function move(e, node) {
+			node.node.original.parent = node.node.parent;
+			var tmp_node = $category_util.nodeToCategory(node.node.original, vm);
+			$terms.modify("post", tmp_node).then(function (category) {
+				load_terms();
+				toastr.success("Category moved!");
+			}, function (err) {
+				toastr.error("Couldn't move category!", err);
+			});
+		}
+
+		function remove(node_id, node, action_id, action_el) {
+			var category = vm.treeInstance.jstree(true).get_node(node.id);
+			category = $category_util.nodeToCategory(category.original, vm);
+
+			//Check for sub-categories that depend on this one so we can remove those too
+			// $category_util.subCategories(category).then(function(subcategories) {
 			var modalInstance = $uibModal.open({
 				templateUrl: 'template/modal/confirm.html',
 				controller: 'confirmCtrl',
@@ -285,57 +271,72 @@ todevise.controller('termsCtrl', ["$scope", "$terms", "$category_util", "$locati
 				}
 			});
 
-			modalInstance.result.then(function() {
+			modalInstance.result.then(function () {
 				$terms.modify("delete", category).then(function (category) {
-					$scope.load_terms();
+					load_terms();
 					toastr.success("Category deleted!");
 				}, function (err) {
 					toastr.error("Couldn't remove category!", err);
 				});
 
-			}, function() {
+			}, function () {
 				//Cancel
 			});
 
-		// }, function(err) {
-		// 	toastr.error("Couldn't check dependencies!", err);
-		// });
-	};
+			// }, function(err) {
+			// 	toastr.error("Couldn't check dependencies!", err);
+			// });
+		}
 
-}]);
+	}
 
-todevise.controller("create_newCtrl", function($scope, $uibModalInstance, data) {
-	$scope.data = data;
-	$scope.langs = {};
-	$scope.slug = "";
-	$scope.sizecharts = false;
-	$scope.prints = false;
+	function create_newCtrl($uibModalInstance, data) {
+		var vm = this;
+		vm.data = data;
+		vm.langs = {};
+		vm.slug = "";
+		vm.sizecharts = false;
+		vm.prints = false;
 
-	$scope.ok = function() {
-		$uibModalInstance.close({
-			"langs": $scope.langs,
-			"slug": $scope.slug,
-			"sizecharts": $scope.sizecharts,
-			"prints": $scope.prints
-		});
-	};
+		vm.ok = ok;
+		vm.cancel = cancel;
 
-	$scope.cancel =  function() {
-		$uibModalInstance.dismiss();
-	};
-});
+		function ok() {
+			$uibModalInstance.close({
+				langs: vm.langs,
+				slug: vm.slug,
+				sizecharts: vm.sizecharts,
+				prints: vm.prints
+			});
+		};
 
+		function cancel() {
+			$uibModalInstance.dismiss();
+		}
+	}
 
-todevise.controller("editCtrl", function($scope, $uibModalInstance, data) {
-	$scope.data = data;
+	function editCtrl($uibModalInstance, data) {
+		var vm = this;
+		vm.data = data;
+		vm.ok = ok;
+		vm.cancel = cancel;
 
-	$scope.ok = function() {
-		$uibModalInstance.close({
-			"category": $scope.data.category
-		});
-	};
+		function ok() {
+			$uibModalInstance.close({
+				category: vm.data.category
+			});
+		};
 
-	$scope.cancel =  function() {
-		$uibModalInstance.dismiss();
-	};
-});
+		function cancel() {
+			$uibModalInstance.dismiss();
+		}
+
+	}
+
+	angular
+		.module('todevise', ['ngAnimate', 'ui.bootstrap', 'ngJsTree', 'global-admin', 'global-desktop', 'api', 'util'])
+		.controller('termsCtrl', controller)
+		.controller('create_newCtrl', create_newCtrl)
+		.controller('editCtrl', editCtrl);
+
+}());
