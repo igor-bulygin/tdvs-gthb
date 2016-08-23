e012897c1dfe70d4293ca09d6b1bc258c29f6cfe
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-08-23 19:07:23 +0200

    Controllers & view-model in deviser: global & edit-info.

--- web/js/desktop/deviser/global.js
+++ web/js/desktop/deviser/global.js
@@ -1,39 +1,48 @@
-console.log("Global desktop deviser");
-var global_deviser = angular.module('global-deviser', ['toastr']);
+(function () {
+	"use strict";
 
-global_deviser.dataURItoBlob = function(dataURI) {
-	var byteString;
-	if (dataURI.split(',')[0].indexOf('base64') >= 0)
-		byteString = atob(dataURI.split(',')[1]);
-	else
-		byteString = unescape(dataURI.split(',')[1]);
+	console.log("Global desktop deviser");
 
-	// separate out the mime component
-	var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+	function cropCtrl($scope, $uibModalInstance, $timeout, data) {
+		$scope.croppedphoto = "";
+		$scope.photo = data.photo;
 
-	// write the bytes of the string to a typed array
-	var ia = new Uint8Array(byteString.length);
-	for (var i = 0; i < byteString.length; i++) {
-		ia[i] = byteString.charCodeAt(i);
+		$scope.ok = function () {
+			var _f = global_deviser.dataURItoBlob($scope.croppedphoto);
+			_f.name = $scope.photo.name;
+
+			$uibModalInstance.close({
+				"croppedphoto": _f
+			});
+		};
+
+		$scope.cancel = function () {
+			$uibModalInstance.dismiss();
+		}
 	}
 
-	return new Blob([ia], {type:mimeString});
-};
+	var global_deviser = angular.module('global-deviser', ['toastr'])
+		.controller('cropCtrl', cropCtrl);
 
-global_deviser.controller("cropCtrl", function($scope, $uibModalInstance, $timeout, data) {
-	$scope.croppedphoto = "";
-	$scope.photo = data.photo;
+	global_deviser.dataURItoBlob = function (dataURI) {
+		var byteString;
+		if (dataURI.split(',')[0].indexOf('base64') >= 0)
+			byteString = atob(dataURI.split(',')[1]);
+		else
+			byteString = unescape(dataURI.split(',')[1]);
 
-	$scope.ok = function() {
-		var _f = global_deviser.dataURItoBlob($scope.croppedphoto);
-		_f.name = $scope.photo.name;
+		// separate out the mime component
+		var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
-		$uibModalInstance.close({
-			"croppedphoto": _f
+		// write the bytes of the string to a typed array
+		var ia = new Uint8Array(byteString.length);
+		for (var i = 0; i < byteString.length; i++) {
+			ia[i] = byteString.charCodeAt(i);
+		}
+
+		return new Blob([ia], {
+			type: mimeString
 		});
 	};
 
-	$scope.cancel =  function() {
-		$uibModalInstance.dismiss();
-	};
-});
+}());
