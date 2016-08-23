349dd66f14e97ba29aae02415068105e4e570326
Javier Negreira <jnegreira@gmail.com>
2016-08-23 11:57:06 +0200

    install dependencies instead update them, on deploy

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 rsync -razhe ssh --progress --exclude '.git' --exclude 'runtime' --exclude 'vendor' --exclude 'web/assets' . root@dev.todevise.com:/var/www/dev.todevise.com/web/
 ssh root@dev.todevise.com << EOF
 su - todevise
-docker exec web_nginx_1 bash -c 'cd /var/www/html/ && composer update -v --prefer-dist && ./yii mongodb-migrate --interactive=0'
+docker exec web_nginx_1 bash -c 'cd /var/www/html/ && composer install -v --prefer-dist && ./yii mongodb-migrate --interactive=0'
 EOF
