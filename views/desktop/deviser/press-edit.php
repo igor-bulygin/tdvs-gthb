3ddb091bce582d68f8a7ef138a362cc7332e404a
Marcos Muñiz García <esechinako@gmail.com>
2016-09-02 14:42:17 +0200

    Add SASS and styles edit press photos

--- views/desktop/deviser/press-edit.php
+++ views/desktop/deviser/press-edit.php
@@ -27,23 +27,26 @@ $this->params['deviser_menu_active_option'] = 'press';
 			<div class="col-md-2">
 				<?= DeviserMenu::widget() ?>
 			</div>
-			<div class="col-md-10 about-bg">
-				<h2><?= $deviser->getBrandName() ?></h2>
-				<div style="background: grey">
-					<div ng-if="editPressCtrl.images === 0">You don't have any press images!</div>
-					<form name="editPressCtrl.form">
-						<div ng-if="editPressCtrl.isDropAvailable">
-							<div ng-model="editPressCtrl.image" ngf-drop ngf-select ngf-accept="'image/*'" ngf-drop-available="editPressCtrl.isDropAvailable" style="width:200px; height: 200px; text-align: center; margin: 10px; border: 3px dashed white; background: grey;">Add press image</div>
-						</div>
-						<div ngf-no-file-drop>
-							<input type="file" name="file" ng-model="editPressCtrl.image" ngf-select ngf-accept="'image/*'" ngf-drop-available="editPressCtrl.isDropAvailable">
-							<button ng-click="editPressCtrl.upload(editPressCtrl.image)">Guardar</button>
-						</div>
-					</form>
-					<div ng-if="editPressCtrl.images.length > 0" dnd-list="editPressCtrl.images">
-						<img ng-repeat="item in editPressCtrl.images" ng-src="{{item.url}}" dnd-draggable="item" dnd-effect-allowed="move" dnd-moved="editPressCtrl.update($index)" style="width: 200px; max-height:300px; cursor:move; padding-left:20px;">
-					</div>
-				</div>
+			<div class="col-md-10">
+			    <div ng-if="editPressCtrl.images === 0">You don't have any press images!</div>
+                        
+				<div class="mesonry-row press-3">
+                       <form name="editPressCtrl.form">
+                            <div ng-if="editPressCtrl.isDropAvailable">
+                                <div class="photo-loader" class="menu-category list-group" ng-model="editPressCtrl.image" ngf-drop ngf-select ngf-accept="'image/*'" ngf-drop-available="editPressCtrl.isDropAvailable">
+                                    <div class="plus-add">+</div>
+                                    <span>Add press image</span>
+                                </div>
+                            </div>
+                            <div ngf-no-file-drop>
+                                <input type="file" name="file" ng-model="editPressCtrl.image" ngf-select ngf-accept="'image/*'" ngf-drop-available="editPressCtrl.isDropAvailable">
+                                <button ng-click="editPressCtrl.upload(editPressCtrl.image)">Guardar</button>
+                            </div>
+                        </form>
+                       <div class="menu-category list-group draggable-list" ng-if="editPressCtrl.images.length > 0" dnd-list="editPressCtrl.images" ng-repeat="item in editPressCtrl.images">
+                            <img class="grid-image draggable-img" ng-src="{{item.url}}" dnd-draggable="item" dnd-effect-allowed="move" dnd-moved="editPressCtrl.update($index)">
+                        </div>
+                </div>
 			</div>
 		</div>
 	</div>
