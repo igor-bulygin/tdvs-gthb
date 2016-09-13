5aefa1caa2fc76a1a21470465a7b16fa61435a90
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-09-13 11:11:25 +0200

    Create deviser account.

--- web/js/desktop/public-2/create-deviser-account.js
+++ web/js/desktop/public-2/create-deviser-account.js
@@ -1,21 +1,27 @@
 (function () {
 	"use strict";
 
-	function controller(UtilService) {
+	function controller(UtilService, deviserDataService, toastr) {
 		var vm = this;
-		vm.submit = submit;
+		vm.submitForm = submitForm;
 		vm.has_error = UtilService.has_error;
 
 		function init() {
-
+			vm.deviser = new deviserDataService.Devisers;
 		}
 
 		init();
 
-		function submit(form) {
-			form.$setSubmitted();
+		function submitForm(form) {
+			if (form.password_confirm.$error.same)
+				form.$setValidity('password_confirm', false);
 			if (form.$valid) {
-
+				form.$setSubmitted();
+				vm.deviser.$save().then(function (dataSaved) {
+					//ok
+				}, function (err) {
+					toastr.error("Error saving form!");
+				})
 			} else {
 				toastr.error("Invalid form!")
 			}
@@ -25,7 +31,7 @@
 
 
 	angular
-		.module('todevise', ['api', 'util'])
+		.module('todevise', ['api', 'util', 'toastr'])
 		.controller('createDeviserCtrl', controller);
 
 }());
