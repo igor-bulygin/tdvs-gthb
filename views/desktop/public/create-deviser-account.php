a430836b6aef61a90721b70e9dfd2c18a918caa9
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-09-15 13:39:19 +0200

    Tooltip working on create deviser account.

--- views/desktop/public/create-deviser-account.php
+++ views/desktop/public/create-deviser-account.php
@@ -57,8 +57,7 @@ $this->title = 'Create a Deviser account - Todevise';
 						<label>Representative name <i tooltip-placement="top" uib-tooltip="Lorem ipsum dolor sit amet" class="ion-information-circled info"></i></label>
 						<input type="text" class="form-control grey-input" name="representative_name"
 						       ng-model="createDeviserCtrl.deviser.representative_name" required>
-						<form-errors field="createDeviserCtrl.form.representative_name"
-						             condition="createDeviserCtrl.has_error(createDeviserCtrl.form, createDeviserCtrl.form.representative_name)"></form-errors>
+						<form-errors field="createDeviserCtrl.form.representative_name" condition="createDeviserCtrl.has_error(createDeviserCtrl.form, createDeviserCtrl.form.representative_name)"></form-errors>
 					</div>
 					<div class="row">
 						<label>Set your password</label>
