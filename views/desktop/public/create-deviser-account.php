3aadf4c84c5111effd469cd3415674f85e84bcb5
Marcos Muñiz García <esechinako@gmail.com>
2016-09-15 13:30:21 +0200

    Includes tooltip on create deviser form

--- views/desktop/public/create-deviser-account.php
+++ views/desktop/public/create-deviser-account.php
@@ -54,7 +54,7 @@ $this->title = 'Create a Deviser account - Todevise';
 						             condition="createDeviserCtrl.has_error(createDeviserCtrl.form, createDeviserCtrl.form.brand_name)"></form-errors>
 					</div>
 					<div class="row">
-						<label>Representative name <i data-toggle="tooltip" data-placement="top" title="" data-original-title="+ €6,50" class="ion-information-circled info"></i></label>
+						<label>Representative name <i tooltip-placement="top" uib-tooltip="Lorem ipsum dolor sit amet" class="ion-information-circled info"></i></label>
 						<input type="text" class="form-control grey-input" name="representative_name"
 						       ng-model="createDeviserCtrl.deviser.representative_name" required>
 						<form-errors field="createDeviserCtrl.form.representative_name"
