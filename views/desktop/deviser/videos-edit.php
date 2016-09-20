dc5b3eb39d15383a5b39303b5ac8900720c4c50f
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-09-20 12:06:59 +0200

    Fix ordering (edit videos).

--- views/desktop/deviser/videos-edit.php
+++ views/desktop/deviser/videos-edit.php
@@ -39,26 +39,26 @@ $this->params['deviser_menu_active_option'] = 'videos';
 								</div>
 							</form>
 							<div ng-if="editVideosCtrl.deviser.videos.length === 0" class="text-center" ng-cloak>
-							    <img class="sad-face" src="/imgs/sad-face.svg">
+								<img class="sad-face" src="/imgs/sad-face.svg">
 								<p class="no-video-text">You have no videos</p>
 							</div>
-							<div ng-repeat="video in editVideosCtrl.deviser.videos" ng-cloak dnd-list="editVideosCtrl.deviser.videos">
-								<div class="row video-row" dnd-draggable="video" dnd-effect-allowed="move" dnd-moved="editVideosCtrl.updateDeviserVideos($index)">
+							<div ng-cloak dnd-list="editVideosCtrl.deviser.videos">
+								<div class="row video-row" dnd-draggable="video" dnd-effect-allowed="move" dnd-moved="editVideosCtrl.updateDeviserVideos($index)" ng-repeat="video in editVideosCtrl.deviser.videos">
 									<div class="col-md-6">
 										<ng-youtube-embed url="video.url" autoplay="false" color="white" disablekb="true" width="100%"></ng-youtube-embed>
 									</div>
 									<div class="col-md-6 dragg-video-wrapper">
-										 <span class="title">Which works appear in this video?</span>
-										 <div class="open-drag-icons">
-								            <span class="ion-arrow-move move"></span>                                                                 <span class="ion-android-close x-close" ng-click="editVideosCtrl.deleteVideo($index)"></span>
-										 </div>
+										<span class="title">Which works appear in this video?</span>
+										<div class="open-drag-icons">
+											<span class="ion-arrow-move move"></span> <span class="ion-android-close x-close" ng-click="editVideosCtrl.deleteVideo($index)"></span>
+										</div>
 										<form name="editVideosCtrl.searchForm" novalidate>
 											<div class="input-group edit-video-wrapper">
-                                                <span class="input-group-addon" id="basic-addon1">
+												<span class="input-group-addon" id="basic-addon1">
                                                   <span class="ion-search"></span>
-                                                </span>
-                                                <input type="text" placeholder="Type the work name" ng-model="editVideosCtrl.searchTerm[$index]" name="searchTerm" ng-change="editVideosCtrl.findProducts(editVideosCtrl.searchTerm[$index], $index)" ng-minlength="4" ng-model-options="{debounce:{'default': 500}}" class="form-control edit-video-input">
-                                            </div>
+												</span>
+												<input type="text" placeholder="Type the work name" ng-model="editVideosCtrl.searchTerm[$index]" name="searchTerm" ng-change="editVideosCtrl.findProducts(editVideosCtrl.searchTerm[$index], $index)" ng-minlength="4" ng-model-options="{debounce:{'default': 500}}" class="form-control edit-video-input">
+											</div>
 											<div ng-cloak style="height:200px; overflow-y:scroll; background:white;" ng-if="editVideosCtrl.searchTerm[$index].length > 0 && editVideosCtrl.works[$index] && editVideosCtrl.works[$index].length > 0">
 												<div ng-repeat="work in editVideosCtrl.works[$parent.$index]" style="cursor:pointer;" ng-click="editVideosCtrl.selectProduct($parent.$index, work)">
 													<div class="tag-select-row row">
@@ -80,22 +80,22 @@ $this->params['deviser_menu_active_option'] = 'videos';
 												<p class="no-tags-text">Tag works to tell other members what the video is about.</p>
 											</div>
 											<div class="produts-tags-wrapper">
-                                                <div ng-repeat="product in video.products" ng-if="video.products.length > 0">
-                                                    <div class="tag-row">
-                                                        <div class="col-md-2">
-                                                            <img ng-src="{{product.url_image_preview}}" style="width: 100%; max-height: 50px;">
-                                                        </div>
-                                                        <div class="col-md-8">
-                                                            <div ng-bind="product.name"></div>
-                                                            <div>
-                                                            <span>by </span><span ng-bind="product.deviser.name"></span>
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-md-2">
-                                                            <span class="ion-android-close" style="cursor:pointer;" ng-click="editVideosCtrl.deleteTag($parent.$parent.$index, $index)"></span>
-                                                        </div>
-                                                    </div>
-                                                </div>
+												<div ng-repeat="product in video.products" ng-if="video.products.length > 0">
+													<div class="tag-row">
+														<div class="col-md-2">
+															<img ng-src="{{product.url_image_preview}}" style="width: 100%; max-height: 50px;">
+														</div>
+														<div class="col-md-8">
+															<div ng-bind="product.name"></div>
+															<div>
+																<span>by </span><span ng-bind="product.deviser.name"></span>
+															</div>
+														</div>
+														<div class="col-md-2">
+															<span class="ion-android-close" style="cursor:pointer;" ng-click="editVideosCtrl.deleteTag($parent.$parent.$index, $index)"></span>
+														</div>
+													</div>
+												</div>
 											</div>
 										</div>
 									</div>
