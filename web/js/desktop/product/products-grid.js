62548c73efdfc3c37351cd84a1b5e93f3e5ff811
Marcos Muñiz García <esechinako@gmail.com>
2016-09-21 13:55:55 +0200

    Styles products grid

--- web/js/desktop/product/products-grid.js
+++ web/js/desktop/product/products-grid.js
@@ -3,9 +3,9 @@ Macy.init({
     trueOrder: true,
     waitForImages: false,
     margin: 2,
-    columns: 5,
+    columns: 6,
     breakAt: {
-      1200: 5,
+      1200: 6,
       940: 3,
       520: 2,
       400: 1
