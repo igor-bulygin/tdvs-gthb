382e8e5aa887ea87e04b4c53fed99c457819a48c
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-09-22 14:15:00 +0200

    Drag and drop store products.

--- web/js/api/productDataService.js
+++ web/js/api/productDataService.js
@@ -1,12 +1,20 @@
 (function () {
 	"use strict";
-	
+
 	function productDataService($resource, config) {
+		//pub
 		this.Product = $resource(config.baseUrl + 'pub/' + config.version + 'products/:idProduct');
 		this.Categories = $resource(config.baseUrl + 'pub/' + config.version + 'categories');
+
+		//priv
+		this.ProductPriv = $resource(config.baseUrl + 'priv/' + config.version + 'products/:idProduct', {}, {
+			'update': {
+				method: 'PATCH'
+			}
+		});
 	}
-	
+
 	angular.module('api')
 		.service('productDataService', productDataService);
-	
+
 }());
