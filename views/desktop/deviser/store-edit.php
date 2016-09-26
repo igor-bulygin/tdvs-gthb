78a4342bd9dde5f4954e4d5c44c35a35ef96719b
Javier Negreira <jnegreira@gmail.com>
2016-09-26 16:31:44 +0200

    Preparing Store to set products position in categories

--- views/desktop/deviser/store-edit.php
+++ views/desktop/deviser/store-edit.php
@@ -41,9 +41,40 @@ $this->params['deviser'] = $deviser;
 					</div>
 					<div class="col-md-10" ng-controller="editStoreCtrl as editStoreCtrl">
 						<div class="content-store">
+							<?php if (count($categories) > 1) { ?>
+								<div class="cathegory-wrapper">
+									<?php foreach ($categories as $i => $category) { ?>
+										<div class="col-md-3 col-sm-3 col-xs-3 pad-cathegory">
+											<a href="<?= Url::to(["deviser/store-edit", "slug" => $deviser->slug, 'deviser_id' => $deviser->short_id, 'category' => $category->short_id])?>">
+												<figure class="cathegory">
+													<img class="<?= ($selectedCategory->short_id==$category->short_id) ? 'active' : '' ?>" src="<?= Utils::url_scheme() ?><?= Utils::thumborize($category->getDeviserProduct()->getMainImage())->resize(240, 175) ?>">
+													<figcaption>
+                                        <span class="name">
+                                            <?= Utils::l($category->name) ?>
+                                        </span>
+													</figcaption>
+												</figure>
+											</a>
+										</div>
+									<?php } ?>
+								</div>
+							<?php } ?>
 							<div class="store-grid">
 								<div class="title-wrapper">
-									<span class="title">category</span>
+									<div class="title-wrapper">
+										<span class="title"><?= Utils::l($selectedCategory->name) ?></span>
+									</div>
+									<nav class="products-menu">
+										<ul>
+											<?php if (count($selectedCategory->getDeviserSubcategories()) > 1) { ?>
+												<?php foreach ($selectedCategory->getDeviserSubcategories() as $i => $subcategory) { ?>
+													<li>
+														<a href="<?= Url::to(["deviser/store-edit", "slug" => $deviser->slug, 'deviser_id' => $deviser->short_id, 'category' => $selectedCategory->short_id, 'subcategory' => $subcategory->short_id])?>" class="<?= ($selectedSubcategory->short_id==$subcategory->short_id) ? 'active' : '' ?>"><?= Utils::l($subcategory["name"]) ?></a>
+													</li>
+												<?php } ?>
+											<?php } ?>
+										</ul>
+									</nav>
 									<div class="mesonry-row" dnd-list="editStoreCtrl.products">
 										<div class="menu-category list-group" ng-repeat="product in editStoreCtrl.products" ng-if="product.main_photo" dnd-draggable="product" dnd-effect-allowed="move" dnd-moved="editStoreCtrl.update($index, product)">
 											<a href="">
