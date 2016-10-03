efb5590db06d34d8e08dbfe189dc6dabfae27054
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-10-03 17:50:32 +0200

    Fix more products (macy) height.

--- web/js/desktop/product/products-grid.js
+++ web/js/desktop/product/products-grid.js
@@ -1,13 +1,24 @@
-Macy.init({
-    container: '#macy-container',
-    trueOrder: true,
-    waitForImages: false,
-    margin: 2,
-    columns: 6,
-    breakAt: {
-      1200: 6,
-      940: 3,
-      520: 2,
-      400: 1
-    }
+$(document).ready(function() {
+
+	var options = {
+		container: '#macy-container',
+		trueOrder: true,
+		waitForImages: true,
+		margin: 2,
+		columns: 6,
+		breakAt: {
+			1200: 6,
+			940: 3,
+			520: 2,
+			400: 1
+		}
+	};
+	Macy.init(options);
+
+	$('a[data-toggle="tab"]').on('click', function(e) {
+		setTimeout(function () {
+			Macy.recalculate();
+		}, 50);
+	});
+
 });
