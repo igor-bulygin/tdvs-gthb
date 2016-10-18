e0893e80976155f3e59046e0c9fc8942fea8fc4b
Marcos Muñiz García <esechinako@gmail.com>
2016-10-18 14:51:41 +0200

    Add btw on FAQ

--- views/desktop/deviser/faq-edit.php
+++ views/desktop/deviser/faq-edit.php
@@ -42,9 +42,15 @@ $this->params['deviser_links_target'] = 'edit_view';
 								<div class="col-md-10" ng-controller="editFaqCtrl as editFaqCtrl">
 									<div class="faq-wrapper faq-edit-list">
 										<!-- <div class="section-title">FAQ</div> -->
-										<div><a href="#" ng-click="editFaqCtrl.done()">I'm done editing</a></div>
-										<div ng-if="editFaqCtrl.deviser.faq.length > 0" ng-click="editFaqCtrl.addQuestion()">+ add question</div>
+										<div>
+										    <a class="red-link-btn" href="#" ng-click="editFaqCtrl.done()">I'm done editing</a></div>
 										<div class="edit-faq-wrapper" ng-cloak ng-if="editFaqCtrl.deviser.faq.length > 0" dnd-list="editFaqCtrl.deviser.faq">
+										    <div class="plus-add-wrapper" ng-if="editFaqCtrl.deviser.faq.length > 0" ng-click="editFaqCtrl.addQuestion()">
+                                                <div class="plus-add">
+                                                    <span>+</span>
+                                                </div>
+                                                <div class="text">ADD QUESTION</div>
+                                            </div>
 											<div class="delete-options-wrapper" ng-repeat="question in editFaqCtrl.deviser.faq" style="cursor:move;" dnd-draggable="question" dnd-effect-allowed="move" dnd-moved="editFaqCtrl.update($index)">
 												<a class="delete-link pull-right" href="#" ng-click="editFaqCtrl.deleteQuestion($index)">Delete question</a>
 												<div class="edit-faq-panel">
