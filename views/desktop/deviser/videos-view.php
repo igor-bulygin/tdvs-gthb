49674fdbca73ff419a5eec29372bd77c05320ad2
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-10-20 12:06:54 +0200

    Edit link in public videos view.

--- views/desktop/deviser/videos-view.php
+++ views/desktop/deviser/videos-view.php
@@ -38,22 +38,22 @@ $this->params['deviser_links_target'] = 'public_view';
 				<?= DeviserMenu::widget() ?>
 			</div>
 			<div class="col-md-10">
+				<div><a class="red-link-btn" href="<?= Url::to(["deviser/videos-edit", "slug" => $deviser->slug, 'deviser_id' => $deviser->short_id])?>">Add / remove videos</a></div>
 				<?php if (count($videos) == 0) { ?>
 					<div class="empty-wrapper">
-					    <img class="sad-face" src="/imgs/sad-face.svg">
-				        <p class="no-video-text">You have no videos</p>
+						<img class="sad-face" src="/imgs/sad-face.svg">
+						<p class="no-video-text">You have no videos</p>
 					</div>
 				<?php } else { ?>
-			    <div class="video-container">
-				    <div><a class="red-link-btn" href="<?= Url::to(["deviser/videos-edit", "slug" => $deviser->slug, 'deviser_id' => $deviser->short_id])?>">Add / remove videos</a></div>
-				    <?php foreach ($videos as $video) { ?>
-                    <div class="col-sm-<?= (count($videos)<=3) ? '12' : '6' ?>">
-                        <div class="video-wrapper">
-                            <iframe width="560" height="315" src="<?= $video->getUrlEmbeddedYoutubePlayer() ?>" frameborder="0" allowfullscreen></iframe>
-                        </div>
-                    </div>
-				    <?php }  ?>
-                </div>
+				<div class="video-container">
+					<?php foreach ($videos as $video) { ?>
+					<div class="col-sm-<?= (count($videos)<=3) ? '12' : '6' ?>">
+						<div class="video-wrapper">
+							<iframe width="560" height="315" src="<?= $video->getUrlEmbeddedYoutubePlayer() ?>" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+					<?php }  ?>
+				</div>
 				<?php } ?>
 			</div>
 		</div>
