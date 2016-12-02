f1b52a28ca39f2c50d579e5ba7773807b4b27719
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-12-02 17:05:18 +0100

    Move zeroOrLess function to UtilService.

--- web/js/util/util.js
+++ web/js/util/util.js
@@ -11,6 +11,7 @@
 		this.parseMultiLanguageEmptyFields = parseMultiLanguageEmptyFields;
 		this.has_error = has_error;
 		this.parseImagesUrl = parseImagesUrl;
+		this.isZeroOrLess = isZeroOrLess;
 		//regex from: https://gist.github.com/dperini/729294
 		//added "?" after (?:(?:https?|ftp):\/\/) for urls like www.google.es
 		this.urlRegEx = /^(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
@@ -98,6 +99,10 @@
 					delete obj[key];
 				}
 			}
+		}
+
+		function isZeroOrLess(value) {
+			return value <= 0 ? true : false;
 		}
 
 	}
