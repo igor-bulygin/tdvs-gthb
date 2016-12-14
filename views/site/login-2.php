ad52aeba53f81504f1c21224142a3a88285ada1b
Marcos Muñiz García <esechinako@gmail.com>
2016-12-14 14:45:51 +0100

    Style login

--- views/site/login-2.php
+++ views/site/login-2.php
@@ -7,33 +7,39 @@ $this->title = 'Login';
 
 ?>
 
-Log in to your todevise account
-
 <?php if ($invalidLogin) { ?>
 	<div class="alert alert-danger">Invalid login</div>
 <?php } ?>
 
 <?php $form = ActiveForm::begin(); ?>
+<div class="create-deviser-account-wrapper pt-0">
+	<span class="login-title">Log in to your todevise account</span>
+	<div class="create-deviser-account-container black-form">
+		<div class="row">
+			<label for="email">Email</label>
+			<input type="email" id="email" name="Login[email]" class="form-control grey-input" />
+		</div>
 
-<div class="row">
-	<label for="email">Email</label>
-	<input type="email" id="email" name="Login[email]" class="form-control" />
+		<div class="row">
+			<label for="password">Password</label>
+			<input type="password" id="password" name="Login[password]" class="form-control grey-input" />
+		</div>
+
+		<div class="row">
+			<div class="checkbox checkbox-circle remember-me">
+				<input id="checkbox7" name="Login[rememberMe]" class="styled" type="checkbox">
+				<label for="checkbox7">
+					Remember me
+				</label>
+			</div>
+		</div>
+
+		<div class="row">
+			<button type="submit" class="btn-red send-btn" >
+				<i class="ion-android-navigate"></i>
+			</button>
+		</div>
+	</div>
 </div>
-
-<div class="row">
-	<label for="password">Password</label>
-	<input type="password" id="password" name="Login[password]" class="form-control" />
-</div>
-
-<div class="row">
-	<label>
-		<input type="checkbox" name="Login[rememberMe]" />Remember me
-	</label>
-</div>
-
-<div class="row">
-	<button type="submit" class="btn btn-default btn-black">Login</button>
-</div>
-
 <?php ActiveForm::end(); ?>
 
