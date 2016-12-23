56200d936e2fdb6a2a5f511ecf35f5aa9b316d3d
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-12-23 12:17:15 +0100

    Check existing options in price-stock controller.

--- web/js/desktop/product/price-stock/price-stock.js
+++ web/js/desktop/product/price-stock/price-stock.js
@@ -75,7 +75,7 @@
 			var object = {};
 			if(!UtilService.isEmpty(vm.product.options)) {
 				for (var key in vm.product.options) {
-					if(vm.product.options[key][0].length > 0)
+					if(vm.product.options[key].length > 0 && vm.product.options[key][0].length > 0)
 						object[key] = vm.product.options[key];
 				}
 				if(angular.isObject(vm.product.prints) && !UtilService.isEmpty(vm.product.prints)) {
