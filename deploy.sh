c70623785552b36e09183e6f92c19eaa83e24d84
Jose Vázquez <jose.vazquez@duacode.com>
2016-12-26 22:43:27 +0100

    Ignore backups dir on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync -razhe ssh --progress --exclude '.git' --exclude 'web/uploads' --exclude 'runtime' --exclude 'vendor' --exclude 'web/assets' --exclude 'thumbor_cache' --exclude 'thumbor_resized' . root@dev.todevise.com:/var/www/dev.todevise.com/web/
+rsync -razhe ssh --progress --exclude '.git' --exclude 'web/uploads' --exclude 'runtime' --exclude 'vendor' --exclude 'web/assets' --exclude 'thumbor_cache' --exclude 'thumbor_resized' --exclude 'db_backup'  . root@dev.todevise.com:/var/www/dev.todevise.com/web/
 ssh root@dev.todevise.com << EOF
 su - todevise
 docker exec web_nginx_1 bash -c 'cd /var/www/html/ && composer install -v --prefer-dist && ./yii mongodb-migrate --interactive=0'
