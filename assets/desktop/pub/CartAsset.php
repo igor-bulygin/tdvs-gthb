6bb7b762f6603cf5552b480376d8a97266d5b342
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2016-12-27 17:38:49 +0100

    Fix cart assets.

--- assets/desktop/pub/CartAsset.php
+++ assets/desktop/pub/CartAsset.php
@@ -7,14 +7,11 @@ class CartAsset extends AssetBundle {
 	public $basePath = '@webroot';
 	public $baseUrl = '@web';
 	public $css = [
-		'css/desktop/pub/cart.css'
 	];
 	public $js = [
-		'js/desktop/pub/cart.js'
+		'js/desktop/cart/cart.js'
 	];
 	public $depends = [
-		'app\assets\desktop\pub\GlobalAsset',
-		'app\assets\libs\socialShareKitAsset',
-		'app\assets\libs\justifiedGalleryAsset'
+		'app\assets\desktop\pub\PublicCommonAsset',
 	];
 }
