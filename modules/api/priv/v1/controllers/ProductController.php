0a347430721280af6e3c4aa956b97f1a7347f529
Jose Vázquez <jose.vazquez@duacode.com>
2017-01-04 11:01:39 +0100

    private method for retrieve products

--- modules/api/priv/v1/controllers/ProductController.php
+++ modules/api/priv/v1/controllers/ProductController.php
@@ -20,7 +20,37 @@ class ProductController extends AppPrivateController
 
     public function actionIndex()
     {
-        return ["action" => "index"];
+        // show only fields needed in this scenario
+        Product2::setSerializeScenario(Product2::SERIALIZE_SCENARIO_PUBLIC);
+
+        // set pagination values
+        $limit = Yii::$app->request->get('limit', 20);
+        $limit = ($limit < 1) ? 1 : $limit;
+        // not allow more than 100 products for request
+//	    $limit = ($limit > 100) ? 100 : $limit;
+        $page = Yii::$app->request->get('page', 1);
+        $page = ($page < 1) ? 1 : $page;
+        $offset = ($limit * ($page - 1));
+
+        $products = Product2::findSerialized([
+            "id" => Yii::$app->request->get("id"),
+            "name" => Yii::$app->request->get("name"), // search only in name attribute
+            "text" => Yii::$app->request->get("q"), // search in name, description, and more
+            "deviser_id" => Yii::$app->request->get("deviser"),
+            "categories" => Yii::$app->request->get("categories"),
+            "product_state" => Yii::$app->request->get("product_state"),
+            "limit" => $limit,
+            "offset" => $offset,
+        ]);
+
+        return [
+            "items" => $products,
+            "meta" => [
+                "total_count" => Product2::$countItemsFound,
+                "current_page" => $page,
+                "per_page" => $limit,
+            ]
+        ];
     }
 
     public function actionCreate()
