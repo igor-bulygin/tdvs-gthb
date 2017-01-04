911d489e5a09c09e6bc7f1fc70e79eec42d8d68e
Marcos Muñiz García <esechinako@gmail.com>
2017-01-04 01:01:58 +0100

    Styling cart

--- views/desktop/public/cart.php
+++ views/desktop/public/cart.php
@@ -10,14 +10,23 @@ $this->title = 'Todevise / Cart';
 ?>
 
 <div class="store" ng-controller="checkoutCtrl as checkoutCtrl">
-	<div class="container">
-		<div class="pull-right"><a href="/">Continue shopping</a></div>
-		<div class="col-md-9">
-			<shopping-cart state="checkoutCtrl.cart_state" cart="checkoutCtrl.cart" devisers="checkoutCtrl.devisers" ng-if="checkoutCtrl.cart_state.state===1"></shopping-cart>
-			<personal-info state="checkoutCtrl.cart_state" cart="checkoutCtrl.cart" ng-if="checkoutCtrl.cart_state.state===2"></personal-info>
+		<div class="cart-top-bar">
+			<div class="container">
+				<div class="pull-right">
+					<a class="continue-shopping-btn" href="/">
+						<i class="ion-arrow-left-b"></i>
+						Continue shopping
+					</a>
+				</div>
+			</div>
 		</div>
-		<div class="col-md-3">
-			<cart-summary state="checkoutCtrl.cart_state" cart="checkoutCtrl.cart" devisers="checkoutCtrl.devisers"></cart-summary>
+		<div class="container">
+			<div class="col-md-8 no-pad">
+				<shopping-cart state="checkoutCtrl.cart_state" cart="checkoutCtrl.cart" devisers="checkoutCtrl.devisers" ng-if="checkoutCtrl.cart_state.state===1"></shopping-cart>
+				<personal-info state="checkoutCtrl.cart_state" cart="checkoutCtrl.cart" ng-if="checkoutCtrl.cart_state.state===2"></personal-info>
+			</div>
+			<div class="col-md-4 no-pad">
+				<cart-summary state="checkoutCtrl.cart_state" cart="checkoutCtrl.cart" devisers="checkoutCtrl.devisers"></cart-summary>
+			</div>
 		</div>
-	</div>
 </div>
