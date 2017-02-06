e4c03a67af14428391bba24cfe7be9d6a1b78367
Marcos Muñiz García <esechinako@gmail.com>
2017-02-06 13:31:04 +0100

    Change tenner arrows styles

--- views/desktop/public/index-2.php
+++ views/desktop/public/index-2.php
@@ -35,11 +35,15 @@ $this->title = 'Todevise / Home';
 		</div>
 		<!-- Controls -->
 		  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="glyphicon glyphicon-chevron-left banner-btn" aria-hidden="true">
+				<i class="ion-ios-arrow-left"></i>
+			</span>
 			<span class="sr-only">Previous</span>
 		  </a>
 		  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="glyphicon glyphicon-chevron-right banner-btn" aria-hidden="true">
+				<i class="ion-ios-arrow-right"></i>
+			</span>
 			<span class="sr-only">Next</span>
 		  </a>
 	</div>
