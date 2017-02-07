95f8fccf15696dfa116c5b41b47cd6936fc2d16d
Jose Vázquez <jose.vazquez@duacode.com>
2017-02-07 21:08:39 +0100

    Update sync script for new dev environment

--- sync_mongo_from_dev_to_local.sh
+++ sync_mongo_from_dev_to_local.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-database="dev.todevise.com:27017/todevise"
 folder="db_backup"
 
 now=$(date +"%Y%m%d%H%M")
 folder="$folder/$now/"
 
-collections=`echo "show collections" | mongo $database --quiet`
+collections=`echo "show collections" | mongo localhost:27777/todevise --quiet`
 
 for collection in $collections;
 do
+    printf "\n"
+    printf "\n"
+
     echo "Exporting $collection"
-    mongoexport -h dev.todevise.com -p 27017 -d todevise -c $collection --out $folder/$collection.json
+    mongoexport -h localhost:27777 -d todevise -c $collection --out $folder/$collection.json
+
+    printf "\n"
 
     echo "Importing $collection"
-    mongoimport -h localhost -p 27017 -d todevise -c $collection --drop --file $folder/$collection.json
+    mongoimport -h localhost:27017 -d todevise -c $collection --drop --file $folder/$collection.json
+
+    printf "\n"
     printf "\n"
 done
