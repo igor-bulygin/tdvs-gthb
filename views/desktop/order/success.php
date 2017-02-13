6eaeca85b614fbcfcbd5775045c3890bbf192d3c
Marcos Muñiz García <esechinako@gmail.com>
2017-02-13 10:35:40 +0100

    Styles success checkout screen

--- views/desktop/order/success.php
+++ views/desktop/order/success.php
@@ -26,28 +26,38 @@ $this->title = 'Todevise / Your purchase is complete';
 	</div>
 	<div class="container">
 		<div class="row no-mar">
-			<div class="col-md-2">
-				<table class="table table-condensed">
+			<div class="col-md-3">
+				<table class="table table-condensed table-order-success">
 					<tr>
-						<td>Order id</td>
-						<td ng-bind="orderSuccessCtrl.order.id"></td>
+						<td></td>
+						<td></td>
 					</tr>
 					<tr>
-						<td>Payment method</td>
-						<td ng-bind="orderSuccessCtrl.order.payment_info.card.brand + ' **** ' + orderSuccessCtrl.order.payment_info.card.last4"></td>
+						<td class="table-label">Order id</td>
+						<td class="table-item" ng-bind="orderSuccessCtrl.order.id"></td>
 					</tr>
 					<tr>
-						<td>Phone</td>
-						<td ng-bind="orderSuccessCtrl.order.client_info.phone1.prefix + ' ' + orderSuccessCtrl.order.client_info.phone1.number"></td>
+						<td class="table-label">Payment method</td>
+						<td class="table-item" ng-bind="orderSuccessCtrl.order.payment_info.card.brand + ' **** ' + orderSuccessCtrl.order.payment_info.card.last4"></td>
 					</tr>
 					<tr>
-						<td>Email</td>
-						<td ng-bind="orderSuccessCtrl.order.client_info.email"></td>
+						<td class="table-label">Phone</td>
+						<td class="table-item" ng-bind="orderSuccessCtrl.order.client_info.phone1.prefix + ' ' + orderSuccessCtrl.order.client_info.phone1.number"></td>
+					</tr>
+					<tr>
+						<td class="table-label">Email</td>
+						<td class="table-item" ng-bind="orderSuccessCtrl.order.client_info.email"></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
 					</tr>
 				</table>
 			</div>
-			<div class="col-md-8 col-md-offset-2">
-				<cart-summary state="orderSuccessCtrl.state" cart="orderSuccessCtrl.order" devisers="orderSuccessCtrl.devisers"></cart-summary>
+			<div class="col-md-8 col-md-offset-1">
+				<div class="order-success-summary-wrapper">
+					<cart-summary state="orderSuccessCtrl.state" cart="orderSuccessCtrl.order" devisers="orderSuccessCtrl.devisers"></cart-summary>
+				</div>
 			</div>
 		</div>
 	</div>
