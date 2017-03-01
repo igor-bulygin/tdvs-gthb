87430b3b0bef237eaf8fa2df4eda12d7f5025bf7
Jose Vázquez <jose.vazquez.viader@gmail.com>
2017-03-01 11:53:11 +0100

    Refactor deviser api

--- modules/api/pub/v1/controllers/DeviserController.php
+++ modules/api/pub/v1/controllers/DeviserController.php
@@ -2,14 +2,6 @@
 
 namespace app\modules\api\pub\v1\controllers;
 
-use app\helpers\Utils;
-use app\models\Invitation;
-use app\models\Person;
-use app\models\PostmanEmail;
-use app\models\PostmanEmailTask;
-use app\modules\api\pub\v1\forms\BecomeDeviserForm;
-use MongoDate;
-use Yii;
 use yii\web\BadRequestHttpException;
 use yii\web\NotFoundHttpException;
 
@@ -23,54 +15,7 @@ class DeviserController extends AppPublicController
 	 */
 	public function actionCreate()
 	{
-
-		$deviser = new Person();
-
-		$invitation_id = Yii::$app->request->post("invitation_id");
-		/** @var Invitation $invitation */
-		$invitation = Invitation::findOneSerialized($invitation_id);
-
-		if (!$invitation) {
-			throw new NotFoundHttpException(Yii::t("app/api", "Invitation not found"));
-		}
-
-		if (!$invitation->canUse()) {
-			throw new BadRequestHttpException(Yii::t("app/api", "Invalid invitation"));
-		}
-
-		if ($invitation->email != Yii::$app->request->post('email')) {
-			throw new BadRequestHttpException(Yii::t("app/api", "The invitation is for another email account"));
-		}
-
-		$deviser->setScenario(Person::SCENARIO_DEVISER_CREATE_DRAFT);
-		$deviser->load(Yii::$app->request->post(), '');
-
-		$deviser->credentials = ["email" => $invitation->email];
-		$deviser->setPassword(Yii::$app->request->post("password"));
-		$deviser->type = [Person::DEVISER];
-
-		// Load personal info directly to subdocument
-		$deviser->personalInfoMapping->load(Yii::$app->request->post(), '');
-		// Refresh properties from the embed
-		$deviser->refreshFromEmbedded();
-
-		if ($deviser->validate()) {
-			$deviser->save();
-
-			// relate invitation and new deviser
-			$invitation->person_id = $deviser->_id;
-
-			// indicate that invitation has been used
-			$invitation->setAsUsed()->save();
-
-			// return information needed to client side
-			Person::setSerializeScenario(Person::SERIALIZE_SCENARIO_PUBLIC);
-
-			return $deviser;
-		} else {
-			Yii::$app->response->setStatusCode(400); // Bad Request
-			return ["errors" => $deviser->errors];
-		}
+		throw new NotFoundHttpException("This method was moved to POST /api/pub/v1/person");
 	}
 
 	/**
@@ -79,49 +24,7 @@ class DeviserController extends AppPublicController
 	 */
 	public function actionInvitationRequestsPost()
 	{
-		$form = new BecomeDeviserForm();
-		if ($form->load(Yii::$app->request->post(), '') && $form->validate()) {
-			// handle success
-			$email = new PostmanEmail();
-			$email->code_email_content_type = PostmanEmail::EMAIL_CONTENT_TYPE_DEVISER_REQUEST_INVITATION;
-			$email->to_email = 'info@todevise.com';
-			$email->subject = 'New deviser invitation request';
-
-			// add task only one send task (to allow retries)
-			$task = new PostmanEmailTask();
-			$task->date_send_scheduled = new MongoDate();
-			$email->addTask($task);
-
-			$email->body_html = Yii::$app->view->render(
-				'request-invitation',
-				[
-					"form" => $form,
-				],
-				$this
-			);
-			$email->save();
-
-			if ($email->send($task->id)) {
-				$email->save();
-			}
-
-			Yii::$app->response->setStatusCode(201); // Created
-//			return ["action" => "done"];
-			return null;
-		} else {
-			Yii::$app->response->setStatusCode(400); // Bad Request
-			return ["errors" => $form->errors];
-		}
+		throw new NotFoundHttpException("This method was moved to POST /api/pub/v1/invitation/request-become-deviser");
 	}
 
-	/**
-	 * Returns the directory containing view files for this controller.
-	 * The default implementation returns the directory named as controller [[id]] under the [[module]]'s
-	 * [[viewPath]] directory.
-	 * @return string the directory containing the view files for this controller.
-	 */
-	public function getViewPath()
-	{
-		return Utils::join_paths('@app', 'mail', 'deviser');
-	}
 }
