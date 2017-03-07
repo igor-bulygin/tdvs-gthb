98bd2e74b1db0df4c7a8ae54ea70e71079550523
Mario Cordovilla <mariocordovilla@gmail.com>
2017-03-07 12:12:08 +0100

    Product saved banner.

--- views/desktop/product/product-create.php
+++ views/desktop/product/product-create.php
@@ -37,13 +37,14 @@ $this->registerJs("var person = ".Json::encode($person), yii\web\View::POS_HEAD,
 				<img ng-src="{{createProductCtrl.profile}}">
 			</div>
 			<div class="text-center" ng-if="!createProductCtrl.errors"><h4 class="title">New work</h4></div>
-			<div class="text-center" ng-if="createProductCtrl.errors"><p>Please complete all the required fields before publishing your work.</p></div>
+			<div class="text-center" ng-if="createProductCtrl.errors" ng-cloak><p>Please complete all the required fields before publishing your work.</p></div>
 			<div class="btns-group">
 				<button class="btn btn-transparent" ng-click="createProductCtrl.save('product_state_draft')" ng-disabled="createProductCtrl.disable_save_buttons">Save progress</button>
 				<button class="btn btn-default btn-green" ng-click="createProductCtrl.save('product_state_active')" ng-disabled="createProductCtrl.disable_save_buttons">Publish work</button>
 			</div>
 		</div>
 	</div>
+	<div id="productSaved" style="background-color: #B8E986; min-height: 200px;" ng-if="createProductCtrl.progressSaved" ng-cloak><p class="text-center">Product saved</p></div>
 	<div class="container">
 		<product-basic-info product="createProductCtrl.product" categories="createProductCtrl.allCategories" languages="createProductCtrl.languages"></product-basic-info>
 		<product-more-details product="createProductCtrl.product" languages="createProductCtrl.languages"></product-more-details>
