c6d3f883ad828bb073733cceb4aa755ada46ab84
Mario Cordovilla <mariocordovilla@gmail.com>
2017-03-28 17:50:40 +0200

    Wrap discover influencers.

--- views/desktop/discover/influencers.php
+++ views/desktop/discover/influencers.php
@@ -7,29 +7,30 @@ $this->title = 'Discover influencers - Todevise';
 $this->registerJs("var type = 3", yii\web\View::POS_HEAD, 'person-type-script');
 
 ?>
-
-<div class="container" ng-controller="discoverCtrl as discoverCtrl">
-	<div class="our-influencers-header">
-		<h3>Our influencers</h3>
-		<h5 class="text-center">Discover talented influencers from around the globe</h5>
-		<div class="row">
-			<form name="discoverCtrl.form">
-				<div class="devisers-searcher">
-					<input type="text" class="form-control white-rounded-input" name="key" ng-model="discoverCtrl.key" on-press-enter="discoverCtrl.search(discoverCtrl.form)" placeholder="Search keyword">
-					<span class="ion-search"></span>
-				</div>
-			</form>
+<div class="our-devisers-wrapper" ng-controller="discoverCtrl as discoverCtrl">
+	<div class="container">
+		<div class="our-influencers-header">
+			<h3>Our influencers</h3>
+			<h5 class="text-center">Discover talented influencers from around the globe</h5>
+			<div class="row">
+				<form name="discoverCtrl.form">
+					<div class="devisers-searcher">
+						<input type="text" class="form-control white-rounded-input" name="key" ng-model="discoverCtrl.key" on-press-enter="discoverCtrl.search(discoverCtrl.form)" placeholder="Search keyword">
+						<span class="ion-search"></span>
+					</div>
+				</form>
+			</div>
 		</div>
-	</div>
-	<div class="our-devisers-body">
-		<div class="col-md-2">
-			<discover-filters filters="discoverCtrl.filters"></discover-filters>
+		<div class="our-devisers-body">
+			<div class="col-md-2">
+				<discover-filters filters="discoverCtrl.filters"></discover-filters>
+			</div>
 		</div>
-	</div>
-	<div class="col-md-10">
-		<div class="found-header">
-			<p ng-if="discoverCtrl.search_key">We found <span ng-bind="discoverCtrl.results.meta.total_count"></span> influencers with the keywords "<span class="key" ng-bind="discoverCtrl.search_key"></span>"</p>
+		<div class="col-md-10">
+			<div class="found-header">
+				<p ng-if="discoverCtrl.search_key">We found <span ng-bind="discoverCtrl.results.meta.total_count"></span> influencers with the keywords "<span class="key" ng-bind="discoverCtrl.search_key"></span>"</p>
+			</div>
+			<discover-results results="discoverCtrl.results" ng-if="discoverCtrl.results" ng-cloak></discover-results>
 		</div>
-		<discover-results results="discoverCtrl.results" ng-if="discoverCtrl.results" ng-cloak></discover-results>
 	</div>
 </div>
