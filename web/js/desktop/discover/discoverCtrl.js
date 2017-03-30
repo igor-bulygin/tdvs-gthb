3a6178c977e6f451ce945b910737815bdf327ffa
Mario Cordovilla <mariocordovilla@gmail.com>
2017-03-30 14:56:11 +0200

    Save init results on discover.

--- web/js/desktop/discover/discoverCtrl.js
+++ web/js/desktop/discover/discoverCtrl.js
@@ -4,15 +4,17 @@
 	function controller(personDataService, UtilService, $scope) {
 		var vm = this;
 		vm.search = search;
-		vm.filters = {}
+		vm.filters = {};
+		var original_results;
 
 		init();
 
 		function init() {
-			search(vm.form);
+			search(vm.form, true);
 		}
 
-		function search(form) {
+		/* use init_search to save all devisers on init and minimize calls to server*/
+		function search(form, init_search) {
 			delete vm.results;
 			vm.searching = true;
 			var params = {
@@ -22,7 +24,14 @@
 			function onGetPeopleSuccess(data) {
 				vm.searching = false;
 				vm.search_key = angular.copy(vm.key);
+				if(init_search)
+					original_results = angular.copy(data);
 				vm.results = angular.copy(data);
+			}
+
+			function onGetPeopleError(err) {
+				UtilService.onError(err);
+				vm.searching = false;
 			}
 
 			if(vm.key)
@@ -34,7 +43,8 @@
 					if(vm.filters[filter_type][filter])
 						new_filter.push(filter);
 				})
-				params[filter_type+'[]'] = new_filter;
+				if(new_filter.length > 0)
+					params[filter_type+'[]'] = new_filter;
 			})
 
 			personDataService.getPeople(params, onGetPeopleSuccess, UtilService.onError);
@@ -42,14 +52,24 @@
 
 		//watches
 		$scope.$watch('discoverCtrl.filters', function (newValue, oldValue) {
-			if(!angular.equals(newValue, {}))
-				search(vm.form)
+			var filters_present = false;
+			Object.keys(newValue).find(function(filter_type) {
+				Object.keys(newValue[filter_type]).find(function(filter) {
+					if(newValue[filter_type][filter])
+						filters_present = true;
+				});
+			});
+			if(filters_present)
+				search(vm.form, false);
+			else {
+				vm.results = angular.copy(original_results);
+			}
 		}, true);
 
 	}
 
 	angular
-		.module('todevise')
-		.controller('discoverCtrl', controller);
+	.module('todevise')
+	.controller('discoverCtrl', controller);
 
 }());
