a17cf8caed184c96acf9197883011ae01c07089c
Marcos Muñiz García <esechinako@gmail.com>
2017-04-03 10:29:01 +0200

    Styles red bar fixed on scroll

--- components/views/MakeProfilePublic.php
+++ components/views/MakeProfilePublic.php
@@ -4,7 +4,7 @@ use app\assets\desktop\deviser\MakeProfilePublicAsset;
 MakeProfilePublicAsset::register($this)
 
 ?>
-<div class="top-bar-red ng-class:{'top-bar-violet': makeProfilePublicCtrl.errorsRequired}" ng-controller="makeProfilePublicCtrl as makeProfilePublicCtrl">
+<div class="top-bar-red ng-class:{'top-bar-violet': makeProfilePublicCtrl.errorsRequired}" ng-controller="makeProfilePublicCtrl as makeProfilePublicCtrl" data-spy="affix" data-offset-top="120">
 	<div ng-if="makeProfilePublicCtrl.type === 2" ng-cloak>
 		<span ng-if="!makeProfilePublicCtrl.errorsRequired" ng-cloak>Your profile is not yet public. First, you need to fill in the HEADER, ABOUT SECTION, AND CREATE ONE WORK.</span>
 		<span ng-if="makeProfilePublicCtrl.errorsRequired" ng-cloak>Please fill in the HEADER, ABOUT SECTION & CREATE ONE WORK before making your profile public.</span>
