844c800cd901979708038089930746b5af44c17a
Mario Cordovilla <mariocordovilla@gmail.com>
2017-04-06 15:54:47 +0200

    Fix controllers in edit/create product.
    Commit 8c6790fe8e1e611d998591619ef8197b5072ee78.

--- views/desktop/product/product-create.php
+++ views/desktop/product/product-create.php
@@ -27,28 +27,30 @@ $this->title = $person->getBrandName() . ' - Todevise';
 $this->registerJs("var person = ".Json::encode($person), yii\web\View::POS_HEAD, 'person-var-script');
 
 ?>
-<div id="newProductHeader" class="new-product-header ng-class:{'purple': createProductCtrl.errors}" data-spy="affix" data-offset-top="120">
-		<div class="container relative">
-			<div class="pull-left">
-				<a class="back-link" ng-href="{{createProductCtrl.link_profile}}"> &lt go back to your profile</a>
-			</div>
-			<div class="avatar">
-				<img ng-src="{{createProductCtrl.profile || '/imgs/default-avatar.png'}}">
-			</div>
-			<div class="text-center" ng-if="!createProductCtrl.errors"><h4 class="title">New work</h4></div>
-			<div class="text-center" ng-if="createProductCtrl.errors" ng-cloak><p>Please complete all the required fields before publishing your work.</p></div>
-			<div class="btns-group">
-				<button class="btn btn-transparent" ng-click="createProductCtrl.save('product_state_draft')" ng-disabled="createProductCtrl.disable_save_buttons">Save progress</button>
-				<button class="btn btn-default btn-green" ng-click="createProductCtrl.save('product_state_active')" ng-disabled="createProductCtrl.disable_save_buttons">Publish work</button>
+<div ng-controller="createProductCtrl as createProductCtrl">
+	<div id="newProductHeader" class="new-product-header ng-class:{'purple': createProductCtrl.errors}" data-spy="affix" data-offset-top="120">
+			<div class="container relative">
+				<div class="pull-left">
+					<a class="back-link" ng-href="{{createProductCtrl.link_profile}}"> &lt go back to your profile</a>
+				</div>
+				<div class="avatar">
+					<img ng-src="{{createProductCtrl.profile || '/imgs/default-avatar.png'}}">
+				</div>
+				<div class="text-center" ng-if="!createProductCtrl.errors"><h4 class="title">New work</h4></div>
+				<div class="text-center" ng-if="createProductCtrl.errors" ng-cloak><p>Please complete all the required fields before publishing your work.</p></div>
+				<div class="btns-group">
+					<button class="btn btn-transparent" ng-click="createProductCtrl.save('product_state_draft')" ng-disabled="createProductCtrl.disable_save_buttons">Save progress</button>
+					<button class="btn btn-default btn-green" ng-click="createProductCtrl.save('product_state_active')" ng-disabled="createProductCtrl.disable_save_buttons">Publish work</button>
+				</div>
 			</div>
 		</div>
-	</div>
-<div ng-controller="createProductCtrl as createProductCtrl" class="create-work-wrapper">
-	<div id="productSaved" class="success-message-120" ng-if="createProductCtrl.progressSaved" ng-cloak><p class="text-center">Product saved</p></div>
-	<div class="container">
-		<product-basic-info product="createProductCtrl.product" categories="createProductCtrl.allCategories" languages="createProductCtrl.languages"></product-basic-info>
-		<product-more-details product="createProductCtrl.product" languages="createProductCtrl.languages"></product-more-details>
-		<product-variations product="createProductCtrl.product" categories="createProductCtrl.allCategories" languages="createProductCtrl.languages" tags="createProductCtrl.tags" sizecharts="createProductCtrl.sizecharts" metric="createProductCtrl.metric" deviser="createProductCtrl.deviser" papertypes="createProductCtrl.papertypes"></product-variations>
-		<product-price-stock product="createProductCtrl.product" categories="createProductCtrl.allCategories" tags="createProductCtrl.tags" papertypes="createProductCtrl.papertypes" metric="createProductCtrl.metric"></product-price-stock>
+	<div class="create-work-wrapper">
+		<div id="productSaved" class="success-message-120" ng-if="createProductCtrl.progressSaved" ng-cloak><p class="text-center">Product saved</p></div>
+		<div class="container">
+			<product-basic-info product="createProductCtrl.product" categories="createProductCtrl.allCategories" languages="createProductCtrl.languages"></product-basic-info>
+			<product-more-details product="createProductCtrl.product" languages="createProductCtrl.languages"></product-more-details>
+			<product-variations product="createProductCtrl.product" categories="createProductCtrl.allCategories" languages="createProductCtrl.languages" tags="createProductCtrl.tags" sizecharts="createProductCtrl.sizecharts" metric="createProductCtrl.metric" deviser="createProductCtrl.deviser" papertypes="createProductCtrl.papertypes"></product-variations>
+			<product-price-stock product="createProductCtrl.product" categories="createProductCtrl.allCategories" tags="createProductCtrl.tags" papertypes="createProductCtrl.papertypes" metric="createProductCtrl.metric"></product-price-stock>
+		</div>
 	</div>
 </div>
