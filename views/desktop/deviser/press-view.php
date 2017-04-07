dbeabbcf5218cdc4e8edd7496794604b79911b9f
Mario Cordovilla <mariocordovilla@gmail.com>
2017-04-07 14:35:54 +0200

    Init carousel on press.

--- views/desktop/deviser/press-view.php
+++ views/desktop/deviser/press-view.php
@@ -40,14 +40,38 @@ $this->params['person_links_target'] = 'public_view';
 							<div><a href="<?= $person->getPressEditLink()?>" class="red-link-btn">Add / remove photos</a></div>
 						<?php } ?>
 						<div class="mesonry-row press-3">
-							<?php foreach ($press as $item) { ?>
-								<div class="menu-category list-group draggable-list">
-									<img class="grid-image draggable-img" src="<?= Utils::url_scheme() ?><?= Utils::thumborize($person->getUrlImagesLocation() . $item)->resize(355, 0) ?>">
+							<?php foreach ($press as $index=>$item) { ?>
+								<div class="menu-category list-group draggable-list" data-toggle="modal" data-target="#carouselModal">
+									<a href="#pressGallery" data-slide-to="<?= $index ?>">
+										<img class="grid-image draggable-img" src="<?= Utils::url_scheme() ?><?= Utils::thumborize($person->getUrlImagesLocation() . $item)->resize(355, 0) ?>">
+									</a>
 								</div>
 							<?php } ?>
 						</div>
 					</div>
 				<?php } ?>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="modal fade" id="carouselModal">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" title="Close"><span class="glyphicon glyphicon-remove"></span></button>
+			</div>
+			<div class="modal-body">
+				<div id="pressGallery" class="carousel slide" data-interval="false">
+						<div class="carousel-inner">
+					<?php foreach($press as $modal_index=>$item) { ?>
+							<div class="item">
+								<img src="<?= Utils::url_scheme() ?><?= Utils::thumborize($person->getUrlImagesLocation() . $item)->resize(355, 0) ?>">
+							</div>
+					<?php } ?>
+						</div>
+				</div>
+				<a href="#pressGallery" class="left carousel-control" role="button" data-slide="prev">&lt;</span></a>
+				<a href="#pressGallery" class="right carousel-control" role="button" data-slide="next">&gt;</span></a>
 			</div>
 		</div>
 	</div>
