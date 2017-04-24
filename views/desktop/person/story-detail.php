5ca3a924b861f88d52a6dd58582bdb535606e3b1
Jose Vázquez <jose.vazquez.viader@gmail.com>
2017-04-24 20:10:26 +0200

    Stories detail, first approach

--- views/desktop/person/story-detail.php
+++ views/desktop/person/story-detail.php
@@ -13,6 +13,27 @@ $this->params['person_links_target'] = 'public_view';
 
 $this->registerJs("var story = ".Json::encode($story), yii\web\View::POS_HEAD, 'story-script');
 
+$firstText = $story->getFirstTextComponent();
+$text = $firstText ? $firstText->getText() : null;
+$photoUrl = $story->mainMediaMapping->getPhotoUrl();
+
 ?>
 
-Story <?=$story->getTitle()?>
+
+<a href="<?=$story->getViewLink()?>">
+	<div>
+
+		<div class="title"><?= $story->getTitle() ?></div>
+
+		<div><?=$story->getPerson()->getName()?></div>
+
+		<div>Fashion designer</div>
+
+		<?php if ($photoUrl) { ?>
+			<div><img src="<?=$photoUrl?>" class="img-responsive" /></div>
+		<?php } ?>
+
+		<div><?=$text?></div>
+
+	</div>
+</a>
