51f94b4c3faa5ba3372b0cc20c3767127e5cddde
Gabriela Irisarri <gabriela.irisarri@hotmail.com>
2017-04-25 11:34:49 +0200

    Explore stories controller cleaning unused

--- web/js/desktop/discover/stories/exploreStoriesCtrl.js
+++ web/js/desktop/discover/stories/exploreStoriesCtrl.js
@@ -1,63 +1,59 @@
-(function () {
-	"use strict";
+(function() {
+    "use strict";
 
-	function controller(UtilService, storyDataService, $scope) {
-		var vm = this;
-		vm.search = search;
-		vm.filters = {};
-        vm.resultsFounded=false;
-		init();
+    function controller(UtilService, storyDataService, $scope) {
+        var vm = this;
+        vm.search = search;
+        vm.filters = {};
+        init();
 
-		function init() {
-			search();
-		}
+        function init() {
+            search();
+        }
 
-		function search(form) {
-			delete vm.results;
-			vm.searching = true;
-			var params = {
-				ignore_empty_stories: false
-			}
-			if(vm.key)
-				params = Object.assign(params, {q: vm.key});
-			
-			Object.keys(vm.filters).map(function(filter_type) {
-				var new_filter = []
-				Object.keys(vm.filters[filter_type]).map(function(filter) {
-					if(vm.filters[filter_type][filter])
-						new_filter.push(filter);
-				})
-				if(new_filter.length > 0)
-					params[filter_type+'[]'] = new_filter;
-			})
+        function search(form) {
+            delete vm.results;
+            vm.searching = true;
+            var params = {
+                ignore_empty_stories: false
+            }
+            if (vm.key)
+                params = Object.assign(params, { q: vm.key });
 
-			function onGetStoriesSuccess(data) {
-				vm.searching= false;
-				vm.search_key = angular.copy(vm.key);
-				vm.results = angular.copy(data);
-				if (vm.results.items.length > 0) {
-					vm.resultsFounded=true;
-				}
-			}
+            Object.keys(vm.filters).map(function(filter_type) {
+                var new_filter = []
+                Object.keys(vm.filters[filter_type]).map(function(filter) {
+                    if (vm.filters[filter_type][filter])
+                        new_filter.push(filter);
+                })
+                if (new_filter.length > 0)
+                    params[filter_type + '[]'] = new_filter;
+            })
 
-			function onGetStoriesError(err) {
-				UtilService.onError(err);
-				vm.searching = false;
-				vm.resultsFounded=false;
-			}
+            function onGetStoriesSuccess(data) {
+                vm.searching = false;
+                vm.search_key = angular.copy(vm.key);
+                vm.results = angular.copy(data);
+                if (vm.results.items.length > 0) {}
+            }
 
-			storyDataService.getStoryPub(params, onGetStoriesSuccess, onGetStoriesError);
-		}
+            function onGetStoriesError(err) {
+                UtilService.onError(err);
+                vm.searching = false;
+            }
 
-		//watches
-		$scope.$watch('exploreStoriesCtrl.filters', function(newValue, oldValue) {
-			search(vm.form)
-		}, true);
+            storyDataService.getStoryPub(params, onGetStoriesSuccess, onGetStoriesError);
+        }
 
-	}
+        //watches
+        $scope.$watch('exploreStoriesCtrl.filters', function(newValue, oldValue) {
+            search(vm.form)
+        }, true);
 
-	angular
-		.module('todevise')
-		.controller('exploreStoriesCtrl', controller);
+    }
+
+    angular
+        .module('todevise')
+        .controller('exploreStoriesCtrl', controller);
 
 }())
