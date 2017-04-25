f5c40a3ea393df8c87025501c3fae4034ddb0af0
Gabriela Irisarri <gabriela.irisarri@hotmail.com>
2017-04-25 21:02:35 +0200

    Fix: one way bind to search state on discover devisers

--- web/js/desktop/discover/person/filters/filters.js
+++ web/js/desktop/discover/person/filters/filters.js
@@ -55,7 +55,7 @@
         controller: controller,
         controllerAs: 'discoverFiltersCtrl',
         bindings: {
-            searching: '=',
+            searching: '<',
             filters: '<'
         }
     }
