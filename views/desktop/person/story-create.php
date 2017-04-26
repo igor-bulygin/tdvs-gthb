1f78dc228633fa0a7ca53e5acfbcf9d8fa9f2beb
Marcos Muñiz García <esechinako@gmail.com>
2017-04-26 19:16:00 +0200

    Fix publish btn

--- views/desktop/person/story-create.php
+++ views/desktop/person/story-create.php
@@ -28,7 +28,7 @@ $this->registerJs("var person = ".Json::encode($person), yii\web\View::POS_HEAD,
 		<story-add-component story="createStoryCtrl.story"></story-add-component>
 		<story-category-component story="createStoryCtrl.story" categories="createStoryCtrl.categories"></story-category-component>
 		<story-tag-component story="createStoryCtrl.story" languages="createStoryCtrl.languages"></story-tag-component>
-		<div class="text-center" style="display: block; width: 100%; float: left;">
+		<div class="text-center" style="display: block; width: 100%; float: left; margin:50px 0 100px;">
 			<button class="btn btn-default btn-green" ng-click="createStoryCtrl.save(createStoryCtrl.story)">Publish story</button>
 		</div>
 	</div>
