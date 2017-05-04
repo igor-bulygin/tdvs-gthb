5cba01dfe08d19dcf5e0729f0b2b4eaca647c20d
Gabriela Irisarri <gabriela.irisarri@hotmail.com>
2017-05-04 11:43:45 +0200

    Tab key navigation on editable table

--- web/js/desktop/product/xeditable-select.js
+++ web/js/desktop/product/xeditable-select.js
@@ -10,10 +10,13 @@
 		function link(scope, element, attr) {
 			element.find('.editable').on('click', function() {
 				element.find('.editable-input').select();
-			})
+			});
+			//Fix to editing when element is focused (tab key navigation for editable elements in table)
+			element.find('.editable').on('focus', function() {
+				element.find('.editable-click').click();
+			});
 		}
 	}
-
 
 	angular
 		.module('todevise')
