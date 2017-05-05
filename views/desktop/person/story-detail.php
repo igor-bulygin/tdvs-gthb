c69de6645a254f4b95e76842b18ee06c290302bb
Marcos Muñiz García <esechinako@gmail.com>
2017-05-05 11:52:06 +0200

    Styles story view

--- views/desktop/person/story-detail.php
+++ views/desktop/person/story-detail.php
@@ -18,52 +18,53 @@ $this->registerJs("var person = ".Json::encode($person), yii\web\View::POS_HEAD,
 
 ?>
 <div class="our-devisers-wrapper" ng-controller="detailStoryCtrl as detailStoryCtrl">
-	<div class="container relative">
-		<div class="pull-left">
-		<a href="<?= $person->getMainLink()?>">
-			<img class="avatar-logued-user" src="<?= $person->getAvatarImage() ?>">
-			<span><?=$person->getName()?></span>
-		</a>
-		</div>
-		<?php if ($person->isPersonEditable()) { ?>
-			<div class="pull-right">
-				<?php /*s
-				<button class="btn btn-default" type="button">
-					<i class="ion-ios-heart"></i>
-					Love Story
-				</button>
-	 			*/ ?>
-
-				<a class="btn btn-default" href="<?=$story->getEditLink()?>">
-					<i class="ion-edit"></i>
-					Edit Story
+	<div class="storie-detail-header">
+		<div class="container relative">
+			<div class="avatar-wrapper">
+				<a href="<?= $person->getMainLink()?>">
+					<img class="avatar-logued-user" src="<?= $person->getAvatarImage() ?>">
+					<span class="person-name"><?=$person->getName()?></span>
 				</a>
-
-				<button class="btn btn-default" type="button" ng-click="detailStoryCtrl.openModal()">
-					<i class="ion-delete"></i>
-					Delete Story
-				</button>
 			</div>
+			<?php if ($person->isPersonEditable()) { ?>
+				<div class="pull-right">
+					<?php /*s
+					<button class="btn btn-default" type="button">
+						<i class="ion-ios-heart"></i>
+						Love Story
+					</button>
+					*/ ?>
+				
+					<a class="btn btn-default" href="<?=$story->getEditLink()?>">
+						Edit Story
+					</a>
+
+					<button class="btn btn-default" type="button" ng-click="detailStoryCtrl.openModal()">
+						<i class="ion-delete"></i>
+						Delete Story
+					</button>
+				</div>
 
 
-			<script type="text/ng-template" id="deleteStoryModal.html">
-				<div class="modal-header">
-					<h3 class="modal-title">Are you sure you want to delete this story?</h3>
-				</div>
-				<div class="modal-body">
-					<p>You will not be able to recover it.</p>
-				</div>
-				<div class="modal-footer">
-					<button class="btn btn-default" ng-click="deleteStoryModalCtrl.ok()">Delete</button>
-					<button class="btn btn-default btn-green" ng-click="deleteStoryModalCtrl.cancel()">Cancel</button>
-				</div>
-			</script>
-		<?php } ?>
+				<script type="text/ng-template" id="deleteStoryModal.html">
+					<div class="modal-header">
+						<h3 class="modal-title">Are you sure you want to delete this story?</h3>
+					</div>
+					<div class="modal-body">
+						<p>You will not be able to recover it.</p>
+					</div>
+					<div class="modal-footer">
+						<button class="btn btn-default" ng-click="deleteStoryModalCtrl.ok()">Delete</button>
+						<button class="btn btn-default btn-green" ng-click="deleteStoryModalCtrl.cancel()">Cancel</button>
+					</div>
+				</script>
+			<?php } ?>
 
+		</div>
 	</div>
-
-	<div class="container">
-		<div><?= $story->getTitle() ?></div>
+	<div class="stories-detail-content container">
+		<div class="detail-title"><?= $story->getTitle() ?></div>
+		<div class="detail-subtitle">
 		<div>
 			<div class="loved-comments-wrapper">
 				<div class="loved-wrapper">
@@ -78,9 +79,11 @@ $this->registerJs("var person = ".Json::encode($person), yii\web\View::POS_HEAD,
 		</div>
 
 		<?php if ($story->published_at) { ?>
-			<div>Published on <?=$story->getPublishingDateFormatted()?></div>
+			<div class="published-text">Published on <?=$story->getPublishingDateFormatted()?></div>
 		<?php } ?>
-
+		</div>
+		
+		
 		<?php foreach ($story->componentsMapping as $component) { ?>
 
 			<?php if ($component->type == \app\models\StoryComponent::STORY_COMPONENT_TYPE_TEXT) { ?>
