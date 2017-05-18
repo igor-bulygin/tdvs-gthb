155f4cfea704b70cdd9572a52e254ec92393dfe4
Gabriela Irisarri <gabriela.irisarri@hotmail.com>
2017-05-18 12:31:58 +0200

    Fields requirements and city selection in general settings

--- views/desktop/settings/general.php
+++ views/desktop/settings/general.php
@@ -26,58 +26,77 @@ $this->registerJs("var person= ".Json::encode($person), yii\web\View::POS_HEAD, 
 					<div ng-hide="generalSettingsCtrl.saving">
 						<div class="form-group">
 							<label for="brand_name" class="col-md-2">Brand / Artist name</label>
-							<div class="col-md-4">
-								<input type="text" name="brand_name" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.brand_name" ng-class="{'purple': generalSettingsCtrl.dataForm.brand_name.$invalid}" required><!--only devisers -->
+							<div class="col-md-4 text-right">
+								<input type="text" name="brand_name" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.brand_name" ng-class="{'error-input': generalSettingsCtrl.dataForm.brand_name.$invalid}" required><!--only devisers -->
+								<span class="purple-text" ng-show="generalSettingsCtrl.existRequiredError('brand_name')">Please, fill out this field</span>
 							</div>
 							<label for="city" class="col-md-2">City</label>
-							<div class="col-md-4">
-								<input type="text" name="city" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.city" ng-class="{'purple': generalSettingsCtrl.dataForm.city.$invalid}" required><!-- with google API -->
+							<div class="col-md-4 text-right">
+								<input type="text" name="city" class="form-control" ng-model="generalSettingsCtrl.city" ng-class="{'error-input': generalSettingsCtrl.dataForm.city.$invalid}" ng-model-options='{ debounce: 100 }' ng-change="generalSettingsCtrl.searchPlace(generalSettingsCtrl.person.personal_info.city)" required>
+								<div ng-if="generalSettingsCtrl.showCities" ng-cloak>
+									<ul class="city-selection">
+										<li ng-repeat="city in generalSettingsCtrl.cities">
+											<span ng-click="generalSettingsCtrl.selectCity(city)" style="cursor:pointer;">{{city.city}} - {{city.country_name}}</span>
+										</li>
+										<li>
+											<img class="powered-google" src="/imgs/powered_by_google_on_white_hdpi.png">
+										</li>
+									</ul>
+								</div>
+								<span class="purple-text" ng-show="generalSettingsCtrl.existRequiredError('city')">Please, fill out this field</span>
 							</div>
 						</div>
+
 						<div class="form-group">
 							<label for="representative_name" class="col-md-2">Representative name</label>
 							<div class="col-md-2">
-								<input type="text" name="name" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.name" ng-class="{'purple': generalSettingsCtrl.dataForm.name.$invalid}">
+								<input type="text" name="name" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.name" ng-class="{'error-input': generalSettingsCtrl.dataForm.name.$invalid}">								
 							</div>
-							<div class="col-md-2">
-								<input type="text" name="last_name" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.last_name" ng-class="{'purple': generalSettingsCtrl.last.brand_name.$invalid}">
+							<div class="col-md-2 text-right">
+								<input type="text" name="last_name" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.last_name" ng-class="{'error-input': generalSettingsCtrl.last_name.$invalid}">
+								<span class="text-green">OPTIONAL</span>
 							</div>
 							<label for="street" class="col-md-2">Street</label>
-							<div class="col-md-4">
-								<input type="text" name="street" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.street" ng-class="{'purple': generalSettingsCtrl.dataForm.street.$invalid}" required>
+							<div class="col-md-4 text-right">
+								<input type="text" name="street" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.street" ng-class="{'error-input': generalSettingsCtrl.dataForm.street.$invalid}" required>
+								<span class="purple-text" ng-show="generalSettingsCtrl.existRequiredError('street')">Please, fill out this field</span>
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="phone" class="col-md-2">Phone number</label>
-							<div class="col-md-4">
-								<input type="text" name="phone" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.phone_number" ng-class="{'purple': generalSettingsCtrl.dataForm.phone.$invalid}" required>
+							<div class="col-md-4 text-right">
+								<input type="tel" name="phone" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.phone_number" ng-class="{'error-input': generalSettingsCtrl.dataForm.phone.$invalid}" required>
+								<span class="purple-text" ng-show="generalSettingsCtrl.existRequiredError('phone')">Please, fill out this field</span>
 							</div>
 							<label for="number" class="col-md-2">Number</label>
-							<div class="col-md-4">
-								<input type="text" name="number" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.number" ng-class="{'purple': generalSettingsCtrl.dataForm.number.$invalid}" required>
+							<div class="col-md-4 text-right">
+								<input type="text" name="number" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.number" ng-class="{'error-input': generalSettingsCtrl.dataForm.number.$invalid}" required>
+								<span class="purple-text" ng-show="generalSettingsCtrl.existRequiredError('number')">Please, fill out this field</span>
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="password" class="col-md-2">Password</label>
-							<div class="col-md-2">
-								<input type="password" name="password" class="form-control" ng-class="{'purple': generalSettingsCtrl.dataForm.password.$invalid}" required>
+							<div class="col-md-2 text-right">
+								<input type="password" name="password" class="form-control" ng-class="{'error-input': generalSettingsCtrl.dataForm.password.$invalid}" placeholder="*********" disabled>
 							</div>
 							<div class="col-md-2">
-								<a href="#"><span class="glyphicon glyphicon-refresh"></span> Change password</a>
+								<a href="#" class="red-text"><span class="glyphicon glyphicon-refresh"></span> Change password</a>
 							</div>
 							<label for="zip" class="col-md-2">ZIP</label>
-							<div class="col-md-4">
-								<input type="zip" name="zip" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.zip" ng-class="{'purple': generalSettingsCtrl.dataForm.zip.$invalid}" required>
+							<div class="col-md-4 text-right">
+								<input type="text" name="zip" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.zip" ng-class="{'error-input': generalSettingsCtrl.dataForm.zip.$invalid}" required>
+								<span class="purple-text" ng-show="generalSettingsCtrl.existRequiredError('zip')">Please, fill out this field</span>
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="currency" class="col-md-2">Currency</label>
-							<div class="col-md-4">
-								<ol name="currency" class="col-md-12 nya-bs-select" ng-model="generalSettingsCtrl.person.settings.currency" ng-class="{'purple': generalSettingsCtrl.dataForm.currency.$invalid}">
+							<div class="col-md-4 text-right">
+								<ol name="currency" class="col-md-12 nya-bs-select" ng-model="generalSettingsCtrl.person.settings.currency" ng-class="{'error-input': generalSettingsCtrl.notCurrencySelected}">
 									<li nya-bs-option="currency in generalSettingsCtrl.currencies" data-value="currency.value">
 										<a href="#"><span ng-bind="currency.symbol"></span>&nbsp;<span ng-bind="currency.text"></span></a>
 									</li>
 								</ol>
+								<span class="purple-text" ng-show="generalSettingsCtrl.notCurrencySelected">Please, select an option</span>
 							</div>
 							<div class="col-md-6 text-right">
 								<button class="btn btn-default btn-green" ng-click="generalSettingsCtrl.update()" ng-disabled="generalSettingsCtrl.saving">Save</button>
@@ -85,7 +104,7 @@ $this->registerJs("var person= ".Json::encode($person), yii\web\View::POS_HEAD, 
 						</div>
 						<div class="col-xs-12 text-right text-green" ng-show="generalSettingsCtrl.saved&&!generalSettingsCtrl.dataForm.$dirty">
 							<span>Updated changes</span>
-						</div>
+						</div>						
 					</div>
 				</form>
 				<div class="text-center" ng-show="generalSettingsCtrl.saving">
--- web/js/desktop/settings/general/general.js
+++ web/js/desktop/settings/general/general.js
@@ -1,13 +1,18 @@
 (function () {
 	"use strict";
 
-	function controller(personDataService,UtilService) {
+	function controller(personDataService,UtilService,locationDataService) {
 		var vm = this;
 		vm.person = {id:person.id, personal_info:angular.copy(person.personal_info), settings:angular.copy(person.settings)};
+		vm.city = vm.person.personal_info.city + ', ' + vm.person.personal_info.country;
 		init();
 		vm.update=update;
 		vm.saving=false;
 		vm.saved=false;
+		vm.existRequiredError=existRequiredError;
+		vm.searchPlace=searchPlace;
+		vm.selectCity=selectCity;
+		vm.showCities = false;
 		
 		function init() {
 			getCurrencies();
@@ -20,22 +25,70 @@
 			personDataService.getCurrencies(onGetCurrenciesSuccess, UtilService.onError);
 		}
 
-		function update() {
-			vm.saving=true;
-			function onUpdateGeneralSettingsSuccess(data) {
-				vm.saving=false;
-				vm.saved=true;
-				vm.dataForm.$dirty=false;
+		function searchPlace(place) {
+			if (place.length>2) {
+				function onGetLocationSuccess(data) {
+					if(data.items.length === 0) {
+						vm.showCities = false;
+					}
+					if(data.items.length > 0) {
+						vm.showCities = true;
+						vm.cities = angular.copy(data.items);
+					}
+				}
+				if(place)
+					locationDataService.getLocation({q: place}, onGetLocationSuccess, UtilService.onError);
+				else {
+					selectCity({
+						city: null,
+						country_code: null
+					})
+				}
 			}
+		}
 
-			function onUpdateGeneralSettingsError(data) {
-				vm.saving=false;
+		function selectCity(city) {
+			vm.person.personal_info.city = city.city;
+			vm.person.personal_info.country = city.country_code;
+			if(city.city && city.country_code)
+				vm.city = vm.person.personal_info.city + ', ' + vm.person.personal_info.country;
+			vm.showCities = false;
+		}
+
+		function update() {
+			if (!vm.dataForm.$valid) {
+				for (var i = array.length - 1; i > -1; i--) {
+					if (array[i].name === "zipCode")
+						array.splice(i, 1);
+				}
 			}
-			personDataService.updateProfile(vm.person,{personId: vm.person.id}, onUpdateGeneralSettingsSuccess, onUpdateGeneralSettingsError);
+			else {
+				vm.saving=true;
+				function onUpdateGeneralSettingsSuccess(data) {
+					vm.saving=false;
+					vm.saved=true;
+					vm.dataForm.$dirty=false;
+				}
+
+				function onUpdateGeneralSettingsError(data) {
+					vm.saving=false;
+				}
+				personDataService.updateProfile(vm.person,{personId: vm.person.id}, onUpdateGeneralSettingsSuccess, onUpdateGeneralSettingsError);
+			}
+		}
+
+		function existRequiredError(fieldName) {
+			var exists=false;
+			angular.forEach(vm.dataForm.$error.required, function(value){
+				if (!angular.isUndefined(value.$name) && value.$name==fieldName) {
+					exists=true;
+				}
+			});
+			return exists;
 		}
 	}
 
-	angular
+	angular	
 	.module('todevise')
 	.controller('generalSettingsCtrl', controller);
 
