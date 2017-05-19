09372aaa8c36e9253244d8b8a99f2ae8acd3e587
Mario Cordovilla <mariocordovilla@gmail.com>
2017-05-19 14:11:54 +0200

    Parse worldwide tree.

--- web/js/api/locationDataService.js
+++ web/js/api/locationDataService.js
@@ -4,9 +4,11 @@
 	function locationDataService($resource, apiConfig, apiMethods) {
 		var Location = $resource(apiConfig.baseUrl + 'pub/' + apiConfig.version + 'locations');
 		var Country = $resource(apiConfig.baseUrl + 'pub/' + apiConfig.version + 'countries');
+		var WorldWide = $resource(apiConfig.baseUrl + 'pub/' + apiConfig.version + 'countries/worldwide');
 
 		this.getCountry = getCountry;
 		this.getLocation = getLocation;
+		this.getWorldWide = getWorldWide;
 
 		function getCountry(params, onSuccess, onError) {
 			apiMethods.get(Country, params, onSuccess, onError);
@@ -14,6 +16,10 @@
 
 		function getLocation(params, onSuccess, onError) {
 			apiMethods.get(Location, params, onSuccess, onError);
+		}
+
+		function getWorldWide(params, onSuccess, onError) {
+			apiMethods.get(WorldWide, params, onSuccess, onError);
 		}
 	}
 
