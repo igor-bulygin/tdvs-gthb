5e08a0a2fae515a8352b9d052fce312233a81efb
Gabriela Irisarri <gabriela.irisarri@hotmail.com>
2017-05-29 17:50:57 +0200

    Weight and phone prefix for general settings

--- views/desktop/settings/general.php
+++ views/desktop/settings/general.php
@@ -26,13 +26,13 @@ $this->registerJs("var person= ".Json::encode($person), yii\web\View::POS_HEAD, 
 					<div ng-hide="generalSettingsCtrl.saving">
 						<div class="form-group">
 							<div ng-if="generalSettingsCtrl.isDeviser">
-								<label for="brand_name" class="col-md-2">Brand / Artist name</label>
+								<label for="brand_name" class="col-md-2">BRAND / ARTIST NAME</label>
 								<div class="col-md-4 text-right">
 									<input type="text" name="brand_name" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.brand_name" ng-class="{'error-input': generalSettingsCtrl.dataForm.brand_name.$invalid}" required><!--only devisers -->
 									<span class="purple-text" ng-show="generalSettingsCtrl.existRequiredError('brand_name',generalSettingsCtrl.dataForm)">Please, fill out this field</span>
 								</div>
 							</div>
-							<label for="city" class="col-md-2" ng-class="{'col-md-offset-6': !generalSettingsCtrl.isDeviser}">City</label>
+							<label for="city" class="col-md-2" ng-class="{'col-md-offset-6': !generalSettingsCtrl.isDeviser}">CITY</label>
 							<div class="col-md-4 text-right">
 								<input type="text" name="city" class="form-control" ng-model="generalSettingsCtrl.city" ng-class="{'error-input': generalSettingsCtrl.dataForm.city.$invalid}" ng-model-options='{ debounce: 100 }' ng-change="generalSettingsCtrl.searchPlace(generalSettingsCtrl.person.personal_info.city)" required>
 								<div ng-if="generalSettingsCtrl.showCities" ng-cloak>
@@ -50,7 +50,7 @@ $this->registerJs("var person= ".Json::encode($person), yii\web\View::POS_HEAD, 
 						</div>
 
 						<div class="form-group">
-							<label for="representative_name" class="col-md-2">Representative name</label>
+							<label for="representative_name" class="col-md-2">REPRESENTATIVE NAME</label>
 							<div class="col-md-2">
 								<input type="text" name="name" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.name" ng-class="{'error-input': generalSettingsCtrl.dataForm.name.$invalid}">								
 							</div>
@@ -58,26 +58,30 @@ $this->registerJs("var person= ".Json::encode($person), yii\web\View::POS_HEAD, 
 								<input type="text" name="last_name" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.last_name" ng-class="{'error-input': generalSettingsCtrl.last_name.$invalid}">
 								<span class="text-green">OPTIONAL</span>
 							</div>
-							<label for="street" class="col-md-2">Street</label>
+							<label for="street" class="col-md-2">STREET</label>
 							<div class="col-md-4 text-right">
 								<input type="text" name="street" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.street" ng-class="{'error-input': generalSettingsCtrl.dataForm.street.$invalid}" required>
 								<span class="purple-text" ng-show="generalSettingsCtrl.existRequiredError('street',generalSettingsCtrl.dataForm)">Please, fill out this field</span>
 							</div>
 						</div>
 						<div class="form-group">
-							<label for="phone" class="col-md-2">Phone number</label>
-							<div class="col-md-4 text-right">
+							<label for="phone" class="col-md-2">PHONE NUMBER</label>
+							<div class="col-md-1 text-right">
+								<input type="tel" name="phone_prefix" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.phone_number_prefix" ng-class="{'error-input': generalSettingsCtrl.dataForm.phone_prefix.$invalid}" placeholder="prefix" required>
+								<span class="purple-text" ng-show="generalSettingsCtrl.existRequiredError('phone_prefix',generalSettingsCtrl.dataForm)">Please, fill out this field</span>
+							</div>
+							<div class="col-md-3 text-right">
 								<input type="tel" name="phone" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.phone_number" ng-class="{'error-input': generalSettingsCtrl.dataForm.phone.$invalid}" required>
 								<span class="purple-text" ng-show="generalSettingsCtrl.existRequiredError('phone',generalSettingsCtrl.dataForm)">Please, fill out this field</span>
 							</div>
-							<label for="number" class="col-md-2">Number</label>
+							<label for="number" class="col-md-2">NUMBER</label>
 							<div class="col-md-4 text-right">
 								<input type="text" name="number" class="form-control" ng-model="generalSettingsCtrl.person.personal_info.number" ng-class="{'error-input': generalSettingsCtrl.dataForm.number.$invalid}" required>
 								<span class="purple-text" ng-show="generalSettingsCtrl.existRequiredError('number',generalSettingsCtrl.dataForm)">Please, fill out this field</span>
 							</div>
 						</div>
 						<div class="form-group">
-							<label for="password" class="col-md-2">Password</label>
+							<label for="password" class="col-md-2">PASSWORD</label>
 							<div class="col-md-2 text-right">
 								<input type="password" name="password" class="form-control" ng-class="{'error-input': generalSettingsCtrl.dataForm.password.$invalid}" placeholder="*********" disabled>
 							</div>
@@ -91,21 +95,21 @@ $this->registerJs("var person= ".Json::encode($person), yii\web\View::POS_HEAD, 
 							</div>
 						</div>
 						<div class="form-group">
-							<label for="currency" class="col-md-2">Currency</label>
-							<div class="col-md-4 text-right">
-								<ol name="currency" class="col-md-12 nya-bs-select" ng-model="generalSettingsCtrl.person.settings.currency" ng-class="{'error-input': generalSettingsCtrl.notCurrencySelected}" ng-change="generalSettingsCtrl.notCurrencySelected=false">
-									<li nya-bs-option="currency in generalSettingsCtrl.currencies" data-value="currency.value">
-										<a href="#"><span ng-bind="currency.symbol"></span>&nbsp;<span ng-bind="currency.text"></span></a>
+							<label for="weight_measure" class="col-md-2">WEIGHT MEASURE</label>
+							<div class="col-md-4" >
+								<ol name="weightMeasure" class="nya-bs-select" ng-model="generalSettingsCtrl.person.settings.weight_measure" ng-class="{'error-input': generalSettingsCtrl.notWeightMeasureSelected}" ng-change="generalSettingsCtrl.notWeightMeasureSelected=false" ng-show="generalSettingsCtrl.weightCharged">
+									<li nya-bs-option="weightMeasure in generalSettingsCtrl.weightMeasures">
+										<a href="#"><span ng-bind="weightMeasure"></span></a>
 									</li>
 								</ol>
-								<span class="purple-text" ng-show="generalSettingsCtrl.notCurrencySelected">Please, select an option</span>
+								<span class="purple-text" ng-show="generalSettingsCtrl.notweightMeasureSelected">Please, select an option</span>
 							</div>
 							<div class="col-md-6 text-right">
 								<button class="btn btn-default btn-green" ng-click="generalSettingsCtrl.update()" ng-disabled="generalSettingsCtrl.saving">Save</button>
 							</div>
 						</div>
-						<div class="col-xs-12 text-center text-green" ng-show="generalSettingsCtrl.saved&&!generalSettingsCtrl.dataForm.$dirty">
-							<span>Changes updated</span>
+						<div class="col-xs-12 text-center" ng-show="generalSettingsCtrl.saved&&!generalSettingsCtrl.dataForm.$dirty">
+							<span><i class="ion-checkmark text-green"></i> The changes have been saved!</span>
 						</div>						
 					</div>
 				</form>
