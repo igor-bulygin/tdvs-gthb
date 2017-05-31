0568ddb1dee030948495ea59173d12c763b9d177
Jose Vázquez <jose.vazquez.viader@gmail.com>
2017-05-31 13:51:49 +0200

    Validating dates (not implemented yet)

--- models/Preorder.php
+++ models/Preorder.php
@@ -32,9 +32,21 @@ class Preorder extends EmbedModel {
 		return [
 			['type', 'required', 'on' => Product::SCENARIO_PRODUCT_PUBLIC],
 			['type', 'in', 'range' => [self::YES, self::NO], 'on' => [Product::SCENARIO_PRODUCT_PUBLIC, Product::SCENARIO_PRODUCT_DRAFT]],
-			[['ship', 'end'], 'required', 'on' => Product::SCENARIO_PRODUCT_PUBLIC, 'when' => function($model) {
-				return $model->type == self::YES;
-			}],
+			[
+				['ship', 'end'],
+				'required',
+				'on' => Product::SCENARIO_PRODUCT_PUBLIC,
+				'when' => function($model) {
+					return $model->type == self::YES;
+				}
+			],
+			/*
+			[
+				['ship', 'end'],
+				'yii\mongodb\validators\MongoDateValidator',
+				'format' => 'Y-m-d\TH:i:s.u\Z',
+			],
+			*/
 		];
 	}
 }
