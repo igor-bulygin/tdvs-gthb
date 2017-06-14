5a25b10d5305b7bdd8da3f92cbf854210cc1c4f1
Mario Cordovilla <mariocordovilla@gmail.com>
2017-06-14 19:21:22 +0200

    Change some spaces to tabs.

--- views/desktop/person/loved-view.php
+++ views/desktop/person/loved-view.php
@@ -26,56 +26,56 @@ $this->params['person_links_target'] = 'public_view';
 				<?= PersonMenu::widget() ?>
 			</div>
 			<div class="col-md-10">
-                <?php if (empty($loveds)) { ?>
-                    <div class="empty-wrapper">
-                        <?php if ($person->isConnectedUser()) { ?>
-                            <p class="no-video-text">You haven't loved any works yet.</p>
-                            <p>Start now by clicking the <span class="glyphicon glyphicon-heart"></span> button inside a work.</p>
-                        <?php } else { ?>
-                            <p class="no-video-text"><?=$person->getName()?> hasn't loved any works yet.</p>
-                        <?php } ?>
-                        
-                        <img class="image-loved" src="/imgs/loved-image.png">
-                        
-                    </div>
-                <?php } else { ?>
-                    <div class="content-store">
-                        <div class="store-grid">
-                            <div class="title-wrapper title-loved">
-                                <span class="title">Loved by <?=$person->getName()?></span>
-                            </div>
-                            <div id="macy-container">
-                                <?php foreach ($loveds as $loved) {
-                                    $product = $loved->getProduct();
-                                    if ($product->product_state != \app\models\Product::PRODUCT_STATE_ACTIVE) {
+				<?php if (empty($loveds)) { ?>
+					<div class="empty-wrapper">
+						<?php if ($person->isConnectedUser()) { ?>
+							<p class="no-video-text">You haven't loved any works yet.</p>
+							<p>Start now by clicking the <span class="glyphicon glyphicon-heart"></span> button inside a work.</p>
+						<?php } else { ?>
+							<p class="no-video-text"><?=$person->getName()?> hasn't loved any works yet.</p>
+						<?php } ?>
+						
+						<img class="image-loved" src="/imgs/loved-image.png">
+						
+					</div>
+				<?php } else { ?>
+					<div class="content-store">
+						<div class="store-grid">
+							<div class="title-wrapper title-loved">
+								<span class="title">Loved by <?=$person->getName()?></span>
+							</div>
+							<div id="macy-container">
+								<?php foreach ($loveds as $loved) {
+									$product = $loved->getProduct();
+									if ($product->product_state != \app\models\Product::PRODUCT_STATE_ACTIVE) {
 										continue;
 									} ?>
-                                    <div class="menu-category list-group">
-                                        <div class="grid">
-                                            <figure class="effect-zoe">
-                                                <image-hover-buttons product-id="{{'<?= $product->short_id ?>'}}" is-loved="{{'<?=$product->isLovedByCurrentUser() ? 1 : 0 ?>'}}" is-mine="{{'<?= $product->isWorkFromCurrentUser() ? 1 : 0 ?>'}}">
-                                                    <a href="<?= $product->getViewLink() ?>">
-                                                        <img class="grid-image"
-                                                             src="<?= Utils::url_scheme() ?><?= Utils::thumborize($product->getMainImage())->resize(400, 0) ?>">
-                                                    </a>
-                                                </image-hover-buttons>
-                                                <a href="<?= $product->getViewLink() ?>">
-                                                    <figcaption>
-                                                        <p class="instauser">
+									<div class="menu-category list-group">
+										<div class="grid">
+											<figure class="effect-zoe">
+												<image-hover-buttons product-id="{{'<?= $product->short_id ?>'}}" is-loved="{{'<?=$product->isLovedByCurrentUser() ? 1 : 0 ?>'}}" is-mine="{{'<?= $product->isWorkFromCurrentUser() ? 1 : 0 ?>'}}">
+													<a href="<?= $product->getViewLink() ?>">
+														<img class="grid-image"
+															 src="<?= Utils::url_scheme() ?><?= Utils::thumborize($product->getMainImage())->resize(400, 0) ?>">
+													</a>
+												</image-hover-buttons>
+												<a href="<?= $product->getViewLink() ?>">
+													<figcaption>
+														<p class="instauser">
 															<?= $product->name ?>
-                                                        </p>
-                                                        <p class="price">€ <?= $product->getMinimumPrice() ?></p>
-                                                    </figcaption>
-                                                </a>
-                                            </figure>
-                                        </div>
-                                    </div>
-                                <?php } ?>
-                            </div>
-                        </div>
-                    </div>
-                <?php } ?>
-            </div>
+														</p>
+														<p class="price">€ <?= $product->getMinimumPrice() ?></p>
+													</figcaption>
+												</a>
+											</figure>
+										</div>
+									</div>
+								<?php } ?>
+							</div>
+						</div>
+					</div>
+				<?php } ?>
+			</div>
 		</div>
 	</div>
 </div>
