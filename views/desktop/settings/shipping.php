60bbbe6d0a73660b8bd70cf475e40e985738007e
Mario Cordovilla <mariocordovilla@gmail.com>
2017-06-15 14:07:51 +0200

    Include text on shipping settings.

--- views/desktop/settings/shipping.php
+++ views/desktop/settings/shipping.php
@@ -40,7 +40,9 @@ $this->registerJs('var person = ' .Json::encode($person), yii\web\View::POS_HEAD
 					<shipping-observations setting="setting" languages="shippingSettingsCtrl.languages"></shipping-observations>
 					<div class="row text-center">
 						<button class="btn btn-default btn-save btn-xl" ng-click="shippingSettingsCtrl.save()">Save</button>
-						<button class="btn btn-default btn-xl">Cancel</button>
+						<?php if($person->isPublic()) { ?>
+							<button class="btn btn-default btn-xl">Cancel</button>
+						<?php } ?>
 					</div>
 				</div>
 			</uib-accordion>
