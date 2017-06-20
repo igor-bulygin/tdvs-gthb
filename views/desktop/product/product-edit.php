dba6ff017d6bfe97d7a3a77626e1686614f3c700
Gabriela Irisarri <gabriela.irisarri@hotmail.com>
2017-06-20 18:45:30 +0200

    Add button to edit product

--- views/desktop/product/product-edit.php
+++ views/desktop/product/product-edit.php
@@ -13,21 +13,21 @@ $this->registerJs("var product = ".Json::encode($product), yii\web\View::POS_HEA
 ?>
 <div ng-controller="editProductCtrl as editProductCtrl">
 	<div id="newProductHeader" class="new-product-header ng-class:{'purple': editProductCtrl.errors}" data-spy="affix" data-offset-top="120">
-			<div class="container relative">
-				<div class="pull-left">
-					<a class="back-link" ng-href="{{editProductCtrl.link_profile}}"> &lt go back to your profile</a>
-				</div>
-				<div class="avatar">
-					<img ng-src="{{editProductCtrl.profile}}">
-				</div>
-				<div class="text-center" ng-if="!editProductCtrl.errors"><h4 class="title">Edit work</h4></div>
-				<div class="text-center" ng-if="editProductCtrl.errors" ng-cloak><p>Please complete all the required fields before publishing your work.</p></div>
-				<div class="btns-group">
-					<button class="btn btn-transparent" ng-click="editProductCtrl.save()" ng-disabled="editProductCtrl.disable_save_buttons">Save progress</button>
-					<button class="btn btn-default btn-green" ng-click="editProductCtrl.save('true')" ng-disabled="editProductCtrl.disable_save_buttons">Publish work</button>
-				</div>
+		<div class="container relative">
+			<div class="pull-left">
+				<a class="back-link" ng-href="{{editProductCtrl.link_profile}}"> &lt go back to your profile</a>
+			</div>
+			<div class="avatar">
+				<img ng-src="{{editProductCtrl.profile}}">
+			</div>
+			<div class="text-center" ng-if="!editProductCtrl.errors"><h4 class="title">Edit work</h4></div>
+			<div class="text-center" ng-if="editProductCtrl.errors" ng-cloak><p>Please complete all the required fields before publishing your work.</p></div>
+			<div class="btns-group">
+				<button class="btn btn-transparent" ng-click="editProductCtrl.save()" ng-disabled="editProductCtrl.disable_save_buttons">Save progress</button>
+				<button class="btn btn-default btn-green" ng-click="editProductCtrl.save('true')" ng-disabled="editProductCtrl.disable_save_buttons">Publish work</button>
 			</div>
 		</div>
+	</div>
 	<div class="create-work-wrapper">
 		<div id="productSaved" class="success-message-120" ng-if="editProductCtrl.progressSaved" ng-cloak><p class="text-center">Product saved</p></div>
 		<div class="container">
@@ -35,6 +35,9 @@ $this->registerJs("var product = ".Json::encode($product), yii\web\View::POS_HEA
 			<product-more-details product="editProductCtrl.product" languages="editProductCtrl.languages"></product-more-details>
 			<product-variations product="editProductCtrl.product" categories="editProductCtrl.allCategories" languages="editProductCtrl.languages" tags="editProductCtrl.tags" sizecharts="editProductCtrl.sizecharts" metric="editProductCtrl.metric" deviser="editProductCtrl.deviser" papertypes="editProductCtrl.papertypes" fromedit="editProductCtrl.from_edit"></product-variations>
 			<product-price-stock product="editProductCtrl.product" categories="editProductCtrl.allCategories" tags="editProductCtrl.tags" papertypes="editProductCtrl.papertypes" metric="editProductCtrl.metric" fromedit="editProductCtrl.from_edit"></product-price-stock>
+			<div class="text-center">
+				<button class="btn btn-default btn-green" ng-click="createProductCtrl.save('product_state_active')" ng-disabled="createProductCtrl.disable_save_buttons">Publish work</button>
+			</div>
 		</div>
 	</div>
 </div>
