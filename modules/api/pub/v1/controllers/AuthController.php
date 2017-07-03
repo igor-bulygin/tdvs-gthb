1a944a1ff040e74f999aea647fe3186584ca101b
Jose Vázquez <jose.vazquez.viader@gmail.com>
2017-07-03 13:31:19 +0200

    Set "main_link" as return url in login if there is no other return url defined

--- modules/api/pub/v1/controllers/AuthController.php
+++ modules/api/pub/v1/controllers/AuthController.php
@@ -23,11 +23,18 @@ class AuthController extends AppPublicController
 			
 			$message = sprintf("LOGIN: User %s logged succesfully via authcontroller", $person->getName());
 			Yii::info($message, __METHOD__);
+
+			$returnUrl = Yii::$app->getUser()->getReturnUrl();
+
+			// If return url is "home", we redirect the user to the main link (profile)
+			if ($returnUrl == Yii::$app->getHomeUrl()) {
+				$returnUrl = $person->getMainLink();
+			}
 			
 			Yii::$app->response->setStatusCode(200); // Created
 			return [
 				'access_token' => $person->getAccessToken(),
-				'return_url' => Yii::$app->getUser()->getReturnUrl(),
+				'return_url' => $returnUrl,
 			];
 
 		} else {
