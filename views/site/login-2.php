96fc4359d6efbcd7e49ba6a27d676b69b9142afd
Gabriela Irisarri <gabriela.irisarri@hotmail.com>
2017-07-11 10:31:50 +0200

    Spinner for login

--- views/site/login-2.php
+++ views/site/login-2.php
@@ -11,35 +11,34 @@ $this->title = 'Login';
 
 <div class="create-deviser-account-wrapper pt-0" ng-controller="loginCtrl as loginCtrl">
 	<form name="loginCtrl.form">
-	<span class="login-title">Log in to your todevise account</span>
-	<div class="create-deviser-account-container black-form">
-		<div class="row">
-			<label for="email">Email</label>
-			<input type="email" id="email" name="email" ng-model="loginCtrl.user.email" class="form-control grey-input" />
-		</div>
-
-		<div class="row">
-			<label for="password">Password</label>
-			<input type="password" id="password" name="password" ng-model="loginCtrl.user.password" class="form-control grey-input" />
-		</div>
-
-		<div class="row">
-			<div class="checkbox checkbox-circle remember-me">
-				<input id="checkbox7" name="remember" ng-model="loginCtrl.user.rememberMe" class="styled" type="checkbox" value="1">
-				<label for="checkbox7">
-					Remember me
-				</label>
+		<span class="login-title">Log in to your todevise account</span>
+		<div class="create-deviser-account-container black-form" ng-if="!loginCtrl.loading">
+			<div class="row">
+				<label for="email">Email</label>
+				<input type="email" id="email" name="email" ng-model="loginCtrl.user.email" class="form-control grey-input" />
+			</div>
+			<div class="row">
+				<label for="password">Password</label>
+				<input type="password" id="password" name="password" ng-model="loginCtrl.user.password" class="form-control grey-input" />
+			</div>
+			<div class="row">
+				<div class="checkbox checkbox-circle remember-me">
+					<input id="checkbox7" name="remember" ng-model="loginCtrl.user.rememberMe" class="styled" type="checkbox" value="1">
+					<label for="checkbox7">
+						Remember me
+					</label>
+				</div>
+			</div>		
+			<div class="alert alert-danger" ng-if="loginCtrl.errors" ng-cloak>Your email or password are not valid</div>
+			<div class="row">
+				<button type="submit" class="btn-red send-btn" ng-click="loginCtrl.login()">
+					<i class="ion-android-navigate"></i>
+				</button>
 			</div>
 		</div>
-
-		
-		<div class="alert alert-danger" ng-if="loginCtrl.errors" ng-cloak>Your email or password are not valid</div>
-
-		<div class="row">
-			<button type="submit" class="btn-red send-btn" ng-click="loginCtrl.login()">
-				<i class="ion-android-navigate"></i>
-			</button>
+		<div class="text-center" ng-if="loginCtrl.loading">
+			<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+			<span class="sr-only">Loading...</span>
 		</div>
-	</div>
 	</form>
 </div>
--- web/js/desktop/public-2/login.js
+++ web/js/desktop/public-2/login.js
@@ -4,8 +4,10 @@
 	function controller(personDataService, $window, UtilService,localStorageUtilService) {
 		var vm = this;
 		vm.login = login;
+		vm.loading=false;
 
 		function login() {
+			vm.loading=true;
 			function onLoginSuccess(data) {
 				localStorageUtilService.setLocalStorage('access_token', data.access_token);
 				$window.location.href = data.return_url;
@@ -14,6 +16,7 @@
 			function onLoginError(err) {
 				UtilService.onError(err);
 				vm.errors = true;
+				vm.loading=false;
 			}
 
 			personDataService.login(vm.user, null, onLoginSuccess, onLoginError);
