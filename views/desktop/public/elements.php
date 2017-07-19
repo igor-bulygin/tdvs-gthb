eddd24739d6f46745667a9d25ca5d287604e501a
Marcos Muñiz García <esechinako@gmail.com>
2017-07-19 18:57:45 +0200

    Refactor text&buttons

--- views/desktop/public/elements.php
+++ views/desktop/public/elements.php
@@ -8,5 +8,55 @@ $this->title = 'Todevise / Elements';
 ?>
 <div class="elements-wrapper">
 	<img class="logo-md auto-center" src="/imgs/logo.svg" data-pin-nopin="true">
-	<p class="text-center">ELEMENTS</p>		
+	<p class="text-center">ELEMENTS</p>	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Tipografía</h1>
+				<span class="title-underlined">Roboto Condensed</span>
+				<span class="text-title">Body text</span>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae semper magna. Mauris at ligula massa, sit amet tincidunt leo. Praesent a magna tellus, ac dignissim risus.</p>
+				<span class="text-title mt-20">Categorías y preguntas</span>
+				<div class="category">Sizes</div>
+				<div class="category">Material</div>
+				<div class="question">Which size is this question available?</div>
+				<span class="text-title mt-20">Precios</span>
+				<div class="big-price">600</div>
+				<span class="text-title mt-20">Otros textos</span>
+				<div class="small-text">Select an option</div>
+				<span class="text-title mt-20">Menu topbar</span>
+				<div class="topbar-item">Boxes</div>
+			</div>
+		</div>
+		<div class="row mt-40">
+			<div class="col-md-6">
+				<span class="title-underlined">UNIVERS LT 49 LIGHTULTRA CN</span>
+				<span class="title-product-name">Titulos productos/nombre</span>
+			</div>
+		</div>
+		<div class="row mt-40">
+			<div class="col-md-12">
+				<h1>Botones</h1>
+			</div>
+			<div class="col-md-4">
+				<span class="title-underlined">Botón pequeño</span>
+				<div>
+					<button class="btn btn-small btn-red">Save</button>
+				</div>
+				<div class="mt-10">
+					<button class="btn btn-small btn-red disabled">Save</button>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<span class="title-underlined">Botón compra</span>
+				<button class="btn btn-small btn-red">
+					<i class="ion-ios-cart cart-icon-btn"></i>
+					<span>Add to the cart</span>
+				</button>
+			</div>
+			<div class="col-md-4">
+				<span class="title-underlined">Botón grande</span>
+			</div>
+		</div>
+	</div>	
 </div>
