c8fe36db0a89c13d7626245bd70a486989da5e94
Gabriela Irisarri <gabriela.irisarri@hotmail.com>
2017-07-26 11:13:29 +0200

    order commission

--- web/js/desktop/settings/order/open-orders.js
+++ web/js/desktop/settings/order/open-orders.js
@@ -237,13 +237,15 @@
 				angular.forEach(vm.orders, function(order, key) {
 					order.state='aware';
 					order.totalPrice = 0;
+					order.commission=0;
 					order.totalShippingPrice=0;
 					order.order_date= new Date(order.order_date.sec*1000)
 					angular.forEach(order.packs, function(pack, keyPack) {
 						order.totalPrice = order.totalPrice + pack.pack_price;
 						order.totalShippingPrice = order.totalShippingPrice + pack.shipping_price;
+						order.commision= order.commision + pack.pack_percentage_fee;
 					});
-					order.total= order.totalPrice + order.totalShippingPrice ; //TODO + commission
+					order.total= order.totalPrice + order.totalShippingPrice + order.commission;
 				});
 			}
 			orderDataService.getDeviserOrders({pack_state:"open", personId:vm.deviserId}, onGetOrdersSuccess, UtilService.onError);
