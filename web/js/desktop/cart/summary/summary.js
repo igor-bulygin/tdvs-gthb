1816d1c5431da24c3eb56a43d91d804e3959ab85
Mario Cordovilla <mariocordovilla@gmail.com>
2017-07-27 14:27:31 +0200

    Checkout summary fixes.

--- web/js/desktop/cart/summary/summary.js
+++ web/js/desktop/cart/summary/summary.js
@@ -5,17 +5,8 @@
 		var vm = this;
 		vm.isObject = UtilService.isObject;
 		
-		function getTotalItems(cart) {
-			var total = 0;
-			vm.cart.packs.forEach(function(pack) {
-				total += pack.products.length;
-			})
-			return total;
-		}
-
 		$scope.$on(cartEvents.cartUpdated, function(event, args) {
 			vm.cart = angular.copy(args.cart);
-			vm.totalItems = getTotalItems(vm.cart);
 			cartService.parseTags(vm.cart);
 			cartService.setTotalItems(vm.cart);
 		});
