392d1fe64889b110e6e98bb698bf47dd0d2a6874
Gabriela Irisarri <gabriela.irisarri@hotmail.com>
2017-08-03 18:26:08 +0200

    Tracking modal

--- web/js/desktop/settings/order/bought-orders/bought-orders.js
+++ web/js/desktop/settings/order/bought-orders/bought-orders.js
@@ -1,10 +1,11 @@
 (function () {
 	"use strict";
 
-	function controller(UtilService, cartService) {
+	function controller(UtilService, cartService,$uibModal) {
 		var vm = this;
 		vm.parseDate=UtilService.parseDate;
 		vm.ordersTotalPrice=0;
+		vm.openTrackModal=openTrackModal;
 
 		init();
 
@@ -20,6 +21,33 @@
 					cartService.parseTags(order);
 				});
 		}
+
+		function openTrackModal(pack) {
+				var modalInstance = $uibModal.open({
+				component: 'modalInfo',
+				resolve: {
+					title: function () {
+						return 'Track package';
+					},
+					text: function () {
+						var bodyText='<span class="col-md-12" style="color: #9D9D9D;">SHIPPING DATE</span><label class="col-md-12">'+ pack.shipping_info.company +
+						 '</label><span class="col-md-12" style="color: #9D9D9D;">TRACKING NUMBER</span><label class="col-md-12">'+ pack.shipping_info.tracking_number + '</label>';
+						if (!angular.isUndefined(pack.shipping_info.tracking_link)) {
+							bodyText=bodyText + '<span class="col-md-12" style="color: #9D9D9D;">TRACKING LINK</span><a class="col-md-12 red-text" ng-href="'+pack.shipping_info.tracking_link+'" target="blank">'+pack.shipping_info.tracking_link+'</a>';
+						}
+						return bodyText;
+					},
+					showButton: function () {
+						return false;
+					}
+				}
+			});
+			modalInstance.result.then(function(data) {
+				return data;
+			}, function(err) {
+				UtilService.onError(err);
+			});
+		} 
 	}
 
 	var component = {
