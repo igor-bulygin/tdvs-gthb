59de5ebf861c6736d0afffb98e7a551c4efbbf02
Jose Vázquez <jose.vazquez.viader@gmail.com>
2017-09-11 19:40:22 +0200

    Fix if

--- models/OrderPack.php
+++ models/OrderPack.php
@@ -228,7 +228,7 @@ class OrderPack extends EmbedModel
 		if (empty($priceStock)) {
 			throw new NotFoundHttpException(sprintf("Price stock item with id %s does not exists", $orderProduct->price_stock_id));
 		}
-		if (!isset($priceStock['available']) || $priceStock['available']) {
+		if (!isset($priceStock['available']) || !$priceStock['available']) {
 			throw new NotFoundHttpException(sprintf("Price stock item with id %s is not available", $orderProduct->price_stock_id));
 		}
 
