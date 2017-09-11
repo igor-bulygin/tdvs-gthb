80b8cd7eff737c599d22866bbed7c6f3dbb37de0
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2017-09-11 17:19:02 +0200

    String truncate function.

--- web/js/util/util.js
+++ web/js/util/util.js
@@ -23,6 +23,7 @@
 		this.isDraft = isDraft;
 		this.setElementPosition = setElementPosition;
 		this.parseDate=parseDate;
+		this.truncateString = truncateString;
 		//regex from: https://gist.github.com/dperini/729294
 		//added "?" after (?:(?:https?|ftp):\/\/) for urls like www.google.es
 		this.urlRegEx = /^(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
@@ -214,6 +215,19 @@
 
 	function parseDate(date) {
 		return new Date(date);
+	}
+
+	function truncateString(string, pos, addendum) {
+		if(angular.isString(string) && angular.isString(addendum) && pos >= 0){
+			if(string.length < pos)
+				return string;
+			else {
+				var new_string = string.slice(0, pos);
+				new_string = new_string.concat(addendum);
+				return new_string;
+			}
+		}
+		return null;
 	}
 
 	angular.module('util', ['util.formMessages', 'LocalStorageModule', 'ui.bootstrap', 'infinite-scroll', 'uiCropper','pascalprecht.translate'])
