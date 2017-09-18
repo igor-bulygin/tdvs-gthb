de21cf4174a16ef1550939a83aaf5b7eac389c35
Jose Vázquez Viader <jose.vazquez.viader@gmail.com>
2017-09-19 00:23:15 +0200

    Using locally gulp

--- tools/gulp/assets.php
+++ tools/gulp/assets.php
@@ -11,11 +11,11 @@ return [
 
 	// Adjust command/callback for JavaScript files compressing:
 //    'jsCompressor' => 'java -jar tools/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js {from} --js_output_file {to}',
-	'jsCompressor' => 'gulp compress-js --gulpfile tools/gulp/gulpfile.js --src {from} --dist {to}',
+	'jsCompressor' => 'tools/gulp/node_modules/.bin/gulp compress-js --gulpfile tools/gulp/gulpfile.js --src {from} --dist {to}',
 
     // Adjust command/callback for CSS files compressing:
 //    'cssCompressor' => 'java -jar tools/yuicompressor.jar --type css {from} -o {to}',
-	'cssCompressor' => 'gulp compress-css --gulpfile tools/gulp/gulpfile.js --src {from} --dist {to}',
+	'cssCompressor' => 'tools/gulp/node_modules/.bin/gulp compress-css --gulpfile tools/gulp/gulpfile.js --src {from} --dist {to}',
 
 	// Whether to delete asset source after compression:
 	'deleteSource' => false,
