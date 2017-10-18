d4411aa78f8ff5fdea1696f8799cf5cd2ff15196
Jose Vázquez Viader <jose.vazquez.viader@gmail.com>
2017-10-18 10:17:31 +0200

    Social media links without using external library

--- views/desktop/product/detail.php
+++ views/desktop/product/detail.php
@@ -5,7 +5,6 @@ use app\helpers\Utils;
 use app\models\Country;
 use app\models\Person;
 use app\models\PersonVideo;
-use bigpaulie\social\share\Share;
 use yii\helpers\Json;
 
 GlobalAsset::register($this);
@@ -282,101 +281,107 @@ $this->registerJs("var product = ".Json::encode($product), yii\web\View::POS_HEA
 								<div class="product-data">
 									<div class="full-width mb-20">
 										<div class="btns-product-wrapper">
-											<?= Share::widget(
-												[
-													'include' => [
-														'facebook',
-														'twitter',
-														'pinterest',
-													],
-													'template' => '<li title="Share on {network}">{button}</li>',
-													'title' => $product->name,
-													'description' => $product->description,
-													'image' => $product->getImagePreview(1200, 1200),
-													'htmlOptions' => [
-														'class' => 'social-items',
-													],
-													'type' => Share::TYPE_SMALL,
-												]
-											); ?>
-											<?php /*
+											<?php
+												$sharerUrl = urlencode($product->getViewLink());
+												$sharerTitle = urlencode($product->getName());
+												$sharerImage = urlencode($product->getImagePreview(1280, 0));
+											?>
 											<ul class="social-items">
 												<li>
-													<a href="https://facebook.com/todevise">
+													<a href="https://www.facebook.com/sharer/sharer.php?u=<?=$sharerUrl?>" target="_blank">
 														<i class="facebook">
 															<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+																x="0px" y="0px"
+																viewBox="0 0 23 42" style="enable-background:new 0 0 23 42;" xml:space="preserve">
+																<g id="Page-1">
+																	<path id="Path" class="st0" d="M14.3,41V21h5.9l0.8-6.9h-6.7l0-3.4c0-1.8,0.2-2.8,3-2.8H21V1H15c-7.1,0-9.6,3.3-9.6,9v4.1H1V21h4.4
+																	v20H14.3L14.3,41L14.3,41L14.3,41z"/>
+																</g>
+															</svg>
+														</i>
+													</a>
+												</li>
+
+												<li>
+													<a class="twitter" href="https://twitter.com/home?status=<?=$sharerUrl?>" target="_blank">
+														<i class="twitter">
+															<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+																x="0px" y="0px"
+																viewBox="0 0 49 42" style="enable-background:new 0 0 49 42;" xml:space="preserve">
+																<g id="Page-1">
+																	<path id="Path" class="st0" d="M23.5,11.6l0.1,1.7l-1.7-0.2C15.8,12.3,10.5,9.6,6,5L3.8,2.7L3.2,4.4C2,8.1,2.8,12.1,5.3,14.8
+																	c1.3,1.5,1,1.7-1.3,0.8c-0.8-0.3-1.5-0.5-1.6-0.4C2.2,15.5,3,18.7,3.6,20c0.9,1.8,2.6,3.5,4.6,4.5l1.6,0.8l-1.9,0
+																	c-1.9,0-1.9,0-1.7,0.8c0.7,2.3,3.3,4.7,6.3,5.8l2.1,0.7l-1.8,1.1c-2.7,1.6-5.8,2.5-9,2.6c-1.5,0-2.7,0.2-2.7,0.3
+																	C1,37,5.1,39,7.5,39.8c7.1,2.3,15.6,1.3,22-2.6c4.5-2.8,9-8.3,11.1-13.7c1.1-2.9,2.3-8.1,2.3-10.6c0-1.6,0.1-1.8,2-3.8
+																	c1.1-1.1,2.1-2.4,2.3-2.7c0.3-0.7,0.3-0.7-1.4-0.1c-2.8,1.1-3.2,0.9-1.8-0.7c1-1.1,2.3-3.2,2.3-3.8c0-0.1-0.5,0.1-1.1,0.4
+																	c-0.6,0.4-1.9,0.9-2.9,1.2L40.4,4l-1.6-1.2c-0.9-0.6-2.2-1.3-2.8-1.6c-1.7-0.5-4.3-0.4-5.9,0.1C25.8,3.1,23.2,7.2,23.5,11.6
+																	C23.5,11.6,23.2,7.2,23.5,11.6L23.5,11.6L23.5,11.6z"/>
+																</g>
+															</svg>
+														</i>
+													</a>
+												</li>
+												<li>
+													<a class="pinterest" href="http://pinterest.com/pin/create/button/?url=<?=$sharerUrl?>&media=<?=$sharerImage?>&description=<?=$sharerTitle?>" target="_blank">
+														<i class="pinterest" aria-hidden="true">
+															<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+																 x="0px" y="0px"
+																 viewBox="0 0 32 42" style="enable-background:new 0 0 32 42;" xml:space="preserve">
+																<g id="Page-1">
+																	<path id="Path" class="st0" d="M1,15.4c0,4,1.4,7.5,4.4,8.8c0.5,0.2,0.9,0,1.1-0.6c0.1-0.4,0.3-1.4,0.4-1.8
+																		c0.1-0.6,0.1-0.8-0.3-1.3c-0.9-1.1-1.4-2.5-1.4-4.5c0-5.8,4.1-11,10.7-11c5.8,0,9,3.8,9,8.8c0,6.6-2.8,12.2-6.9,12.2
+																		c-2.3,0-4-2-3.4-4.4c0.7-2.9,1.9-6.1,1.9-8.2c0-1.9-1-3.4-2.9-3.4c-2.3,0-4.2,2.5-4.2,5.9c0,2.2,0.7,3.6,0.7,3.6S7.8,30.2,7.3,32
+																		c-0.8,3.7-0.1,8.3-0.1,8.7c0,0.3,0.4,0.3,0.5,0.1c0.2-0.3,3-3.9,3.9-7.5c0.3-1,1.5-6.3,1.5-6.3c0.8,1.5,3,2.9,5.3,2.9
+																		c7,0,11.7-6.7,11.7-15.7C30.2,7.3,24.8,1,16.5,1C6.2,1,1,8.8,1,15.4z"/>
+																</g>
+															</svg>
+														</i>
+													</a>
+												</li>
+											</ul>
+
+										<?php /*
+											<li>
+												<a class="instagram" href="https://www.instagram.com/todevise.official">
+													<i class="instagram">
+														<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
 															x="0px" y="0px"
-															viewBox="0 0 23 42" style="enable-background:new 0 0 23 42;" xml:space="preserve">
-															<g id="Page-1">
-																<path id="Path" class="st0" d="M14.3,41V21h5.9l0.8-6.9h-6.7l0-3.4c0-1.8,0.2-2.8,3-2.8H21V1H15c-7.1,0-9.6,3.3-9.6,9v4.1H1V21h4.4
-																v20H14.3L14.3,41L14.3,41L14.3,41z"/>
+															viewBox="0 0 49 42" style="enable-background:new 0 0 49 42;" xml:space="preserve">
+															<g>
+																<path id="Path" class="st0" d="M27.5,40.5h-16c-7.2,0-13-5.8-13-13v-16c0-7.2,5.8-13,13-13h16c7.2,0,13,5.8,13,13v16
+																C40.5,34.7,34.7,40.5,27.5,40.5z M11.5,0.5c-6.1,0-11,4.9-11,11v16c0,6.1,4.9,11,11,11h16c6.1,0,11-4.9,11-11v-16
+																c0-6.1-4.9-11-11-11H11.5z"/>
+															</g>
+															<g>
+																<path id="Path" class="st0" d="M19.5,29.6c-5.6,0-10.1-4.5-10.1-10.1c0-5.6,4.5-10.1,10.1-10.1c5.6,0,10.1,4.5,10.1,10.1
+																C29.6,25.1,25.1,29.6,19.5,29.6z M19.5,11.4c-4.5,0-8.1,3.6-8.1,8.1s3.6,8.1,8.1,8.1s8.1-3.6,8.1-8.1S24,11.4,19.5,11.4z"/>
+															</g>
+															<g>
+																<circle id="Path" class="st0" cx="30.5" cy="10" r="1.6"/>
 															</g>
 														</svg>
 													</i>
 												</a>
 											</li>
-
 											<li>
-												<a class="twitter" href="https://twitter.com/todevise">
-													<i class="twitter">
+												<a class="pinterest" href="#">
+													<i class="pinterest" aria-hidden="true">
 														<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
-														x="0px" y="0px"
-														viewBox="0 0 49 42" style="enable-background:new 0 0 49 42;" xml:space="preserve">
+															 x="0px" y="0px"
+															 viewBox="0 0 32 42" style="enable-background:new 0 0 32 42;" xml:space="preserve">
 														<g id="Page-1">
-															<path id="Path" class="st0" d="M23.5,11.6l0.1,1.7l-1.7-0.2C15.8,12.3,10.5,9.6,6,5L3.8,2.7L3.2,4.4C2,8.1,2.8,12.1,5.3,14.8
-															c1.3,1.5,1,1.7-1.3,0.8c-0.8-0.3-1.5-0.5-1.6-0.4C2.2,15.5,3,18.7,3.6,20c0.9,1.8,2.6,3.5,4.6,4.5l1.6,0.8l-1.9,0
-															c-1.9,0-1.9,0-1.7,0.8c0.7,2.3,3.3,4.7,6.3,5.8l2.1,0.7l-1.8,1.1c-2.7,1.6-5.8,2.5-9,2.6c-1.5,0-2.7,0.2-2.7,0.3
-															C1,37,5.1,39,7.5,39.8c7.1,2.3,15.6,1.3,22-2.6c4.5-2.8,9-8.3,11.1-13.7c1.1-2.9,2.3-8.1,2.3-10.6c0-1.6,0.1-1.8,2-3.8
-															c1.1-1.1,2.1-2.4,2.3-2.7c0.3-0.7,0.3-0.7-1.4-0.1c-2.8,1.1-3.2,0.9-1.8-0.7c1-1.1,2.3-3.2,2.3-3.8c0-0.1-0.5,0.1-1.1,0.4
-															c-0.6,0.4-1.9,0.9-2.9,1.2L40.4,4l-1.6-1.2c-0.9-0.6-2.2-1.3-2.8-1.6c-1.7-0.5-4.3-0.4-5.9,0.1C25.8,3.1,23.2,7.2,23.5,11.6
-															C23.5,11.6,23.2,7.2,23.5,11.6L23.5,11.6L23.5,11.6z"/>
+															<path id="Path" class="st0" d="M1,15.4c0,4,1.4,7.5,4.4,8.8c0.5,0.2,0.9,0,1.1-0.6c0.1-0.4,0.3-1.4,0.4-1.8
+																c0.1-0.6,0.1-0.8-0.3-1.3c-0.9-1.1-1.4-2.5-1.4-4.5c0-5.8,4.1-11,10.7-11c5.8,0,9,3.8,9,8.8c0,6.6-2.8,12.2-6.9,12.2
+																c-2.3,0-4-2-3.4-4.4c0.7-2.9,1.9-6.1,1.9-8.2c0-1.9-1-3.4-2.9-3.4c-2.3,0-4.2,2.5-4.2,5.9c0,2.2,0.7,3.6,0.7,3.6S7.8,30.2,7.3,32
+																c-0.8,3.7-0.1,8.3-0.1,8.7c0,0.3,0.4,0.3,0.5,0.1c0.2-0.3,3-3.9,3.9-7.5c0.3-1,1.5-6.3,1.5-6.3c0.8,1.5,3,2.9,5.3,2.9
+																c7,0,11.7-6.7,11.7-15.7C30.2,7.3,24.8,1,16.5,1C6.2,1,1,8.8,1,15.4z"/>
 														</g>
-													</svg>
-												</i>
-											</a>
-										</li>
-
-										<li>
-											<a class="instagram" href="https://www.instagram.com/todevise.official">
-												<i class="instagram">
-													<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
-													x="0px" y="0px"
-													viewBox="0 0 49 42" style="enable-background:new 0 0 49 42;" xml:space="preserve">
-													<g>
-														<path id="Path" class="st0" d="M27.5,40.5h-16c-7.2,0-13-5.8-13-13v-16c0-7.2,5.8-13,13-13h16c7.2,0,13,5.8,13,13v16
-														C40.5,34.7,34.7,40.5,27.5,40.5z M11.5,0.5c-6.1,0-11,4.9-11,11v16c0,6.1,4.9,11,11,11h16c6.1,0,11-4.9,11-11v-16
-														c0-6.1-4.9-11-11-11H11.5z"/>
-													</g>
-													<g>
-														<path id="Path" class="st0" d="M19.5,29.6c-5.6,0-10.1-4.5-10.1-10.1c0-5.6,4.5-10.1,10.1-10.1c5.6,0,10.1,4.5,10.1,10.1
-														C29.6,25.1,25.1,29.6,19.5,29.6z M19.5,11.4c-4.5,0-8.1,3.6-8.1,8.1s3.6,8.1,8.1,8.1s8.1-3.6,8.1-8.1S24,11.4,19.5,11.4z"/>
-													</g>
-													<g>
-														<circle id="Path" class="st0" cx="30.5" cy="10" r="1.6"/>
-													</g>
-												</svg>
-											</i>
-										</a>
-									</li>
-												<!--li>
-													<a class="pinterest" href="#">
-														<i class="pinterest" aria-hidden="true">
-															<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
-																 x="0px" y="0px"
-																 viewBox="0 0 32 42" style="enable-background:new 0 0 32 42;" xml:space="preserve">
-															<g id="Page-1">
-																<path id="Path" class="st0" d="M1,15.4c0,4,1.4,7.5,4.4,8.8c0.5,0.2,0.9,0,1.1-0.6c0.1-0.4,0.3-1.4,0.4-1.8
-																	c0.1-0.6,0.1-0.8-0.3-1.3c-0.9-1.1-1.4-2.5-1.4-4.5c0-5.8,4.1-11,10.7-11c5.8,0,9,3.8,9,8.8c0,6.6-2.8,12.2-6.9,12.2
-																	c-2.3,0-4-2-3.4-4.4c0.7-2.9,1.9-6.1,1.9-8.2c0-1.9-1-3.4-2.9-3.4c-2.3,0-4.2,2.5-4.2,5.9c0,2.2,0.7,3.6,0.7,3.6S7.8,30.2,7.3,32
-																	c-0.8,3.7-0.1,8.3-0.1,8.7c0,0.3,0.4,0.3,0.5,0.1c0.2-0.3,3-3.9,3.9-7.5c0.3-1,1.5-6.3,1.5-6.3c0.8,1.5,3,2.9,5.3,2.9
-																	c7,0,11.7-6.7,11.7-15.7C30.2,7.3,24.8,1,16.5,1C6.2,1,1,8.8,1,15.4z"/>
-															</g>
-															</svg>
-														</i>
-													</a>
-												</li-->
-											</ul>
+														</svg>
+													</i>
+												</a>
+											</li>
 											*/?>
+
 										</div>
 									</div>
 								</div>
