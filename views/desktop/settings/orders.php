b787769ba7eb1984990afebbd5d310963c4f11d6
Marcos Muñiz García <esechinako@gmail.com>
2017-10-28 00:08:33 +0200

    Smal changes on settings

--- views/desktop/settings/orders.php
+++ views/desktop/settings/orders.php
@@ -23,7 +23,7 @@ $this->registerJs('var person = ' .Json::encode($person), yii\web\View::POS_HEAD
 
 <div ng-controller="ordersCtrl as ordersCtrl">
 	<div class="col-md-12 settings-header">
-		<h4 class="col-md-7 col-md-offset-5"><span translate="settings.orders.ORDERS"></span></h4>
+		<h4 class="col-md-12"><span class="orders-subtitle" translate="settings.orders.ORDERS"></span></h4>
 		<div class="col-md-4" ng-if="ordersCtrl.isDeviser" ng-cloak>
 			<ol class="nya-bs-select btn-group bootstrap-select form-control product-select col-md-12" ng-model="ordersCtrl.typeFilter" ng-change="ordersCtrl.getOrders()" ng-if="ordersCtrl.isDeviser" ng-cloak>
 				<li nya-bs-option="type in ordersCtrl.enabledTypes">
