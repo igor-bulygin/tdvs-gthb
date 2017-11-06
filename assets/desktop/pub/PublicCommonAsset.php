d630496784c3f915b99d889569519d645a2ac223
Jose Vázquez Viader <jose.vazquez.viader@gmail.com>
2017-11-06 11:49:56 +0100

    Delete unused files

--- assets/desktop/pub/PublicCommonAsset.php
+++ assets/desktop/pub/PublicCommonAsset.php
@@ -7,7 +7,6 @@ class PublicCommonAsset extends AssetBundle {
 	public $basePath = '@webroot';
 	public $baseUrl = '@web';
 	public $css = [
-		'css/desktop/public-2/login-2.css',
 	];
 	public $js = [
 		'js/desktop/public-2/become-deviser.js',
