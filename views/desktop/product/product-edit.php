f9059139b5a1b46a68f3103b4379ccf9244e83d5
Gabriela Irisarri <gabriela.irisarri@hotmail.com>
2017-12-16 23:02:14 +0100

    Fix to show variations on edition

--- views/desktop/product/product-edit.php
+++ views/desktop/product/product-edit.php
@@ -38,8 +38,8 @@ $this->registerJs("var product = ".Json::encode($product), yii\web\View::POS_HEA
 		<div id="productSaved" class="success-message-120" ng-if="editProductCtrl.progressSaved" ng-cloak><p class="text-center"><span translate="product.creation_edition.PRODUCT_SAVED"></span></p></div>
 		<div class="container" >
 			<div ng-show="!editProductCtrl.saving" ng-if ="editProductCtrl.product" ng-cloak>
-				<product-basic-info product="editProductCtrl.product" categories="editProductCtrl.allCategories" languages="editProductCtrl.languages"></product-basic-info>
-				<product-variations product="editProductCtrl.product" categories="editProductCtrl.allCategories" languages="editProductCtrl.languages" tags="editProductCtrl.tags" sizecharts="editProductCtrl.sizecharts" metric="editProductCtrl.metric" deviser="editProductCtrl.deviser" papertypes="editProductCtrl.papertypes" fromedit="editProductCtrl.from_edit"></product-variations>
+				<product-basic-info ng-if="editProductCtrl.product.variationsLoaded" product="editProductCtrl.product" categories="editProductCtrl.allCategories" languages="editProductCtrl.languages"></product-basic-info>
+				<product-variations  product="editProductCtrl.product" categories="editProductCtrl.allCategories" languages="editProductCtrl.languages" tags="editProductCtrl.tags" sizecharts="editProductCtrl.sizecharts" metric="editProductCtrl.metric" deviser="editProductCtrl.deviser" papertypes="editProductCtrl.papertypes" fromedit="editProductCtrl.from_edit"></product-variations>
 				<product-price-stock product="editProductCtrl.product" categories="editProductCtrl.allCategories" tags="editProductCtrl.tags" papertypes="editProductCtrl.papertypes" metric="editProductCtrl.metric" fromedit="editProductCtrl.from_edit"></product-price-stock>
 				<product-more-details product="editProductCtrl.product" languages="editProductCtrl.languages"></product-more-details>
 				<div class="text-center">
