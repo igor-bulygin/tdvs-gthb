cfc0e376c2564916827dd1072b34cf56249339e6
David Bodero <dbodero@gmail.com>
2018-01-27 00:33:57 +0100

    SETTINGS - MY ORDERS  -  Aligns 'Orders' and select to order details below

--- views/desktop/settings/orders.php
+++ views/desktop/settings/orders.php
@@ -22,9 +22,9 @@ $this->registerJs('var person = ' .Json::encode($person), yii\web\View::POS_HEAD
 <?php } ?>
 
 <div ng-controller="ordersCtrl as ordersCtrl">
-	<div class="col-md-12 settings-header">
+	<div class="col-md-10 col-md-offset-1 settings-header">
 		<h4 class="col-md-12"><span class="orders-subtitle" translate="settings.orders.ORDERS"></span></h4>
-		<div class="select-settings-wrapper" ng-if="ordersCtrl.isDeviser" ng-cloak>
+		<div class="col-xs-4 select-settings-wrapper" ng-if="ordersCtrl.isDeviser" ng-cloak>
 			<ol class="nya-bs-select btn-group bootstrap-select form-control product-select col-md-12" ng-model="ordersCtrl.typeFilter" ng-change="ordersCtrl.getOrders()" ng-if="ordersCtrl.isDeviser" ng-cloak>
 				<li nya-bs-option="type in ordersCtrl.enabledTypes">
 					<a href="#"><span translate="{{type.name}}"></span></a>
