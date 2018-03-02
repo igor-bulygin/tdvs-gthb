5981415fbdd9079358477426c320102a2d17b05e
Jose Vázquez Viader <jose.vazquez.viader@gmail.com>
2018-03-02 21:00:39 +0100

    Load env files from .env and define helper constants

--- web/index.php
+++ web/index.php
@@ -1,11 +1,17 @@
 <?php
 
+require(__DIR__ . '/../vendor/autoload.php');
+
+// load .env file values
+$dotenv = new Dotenv\Dotenv(__DIR__.'/..');
+$dotenv->load();
+
 if(getenv("DEV") == "1"){
 	define('YII_DEBUG', true);
 	define('YII_ENV', 'dev');
 }
+define('ENVIRONMENT', getenv('ENVIRONMENT'));
 
-require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 /* Require all helper files */
