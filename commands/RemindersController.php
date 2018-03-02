d83424ca98a1e018ae79a29385d3e754f3e02fdb
Jose Vázquez Viader <jose.vazquez.viader@gmail.com>
2018-03-02 20:02:11 +0100

    Rename tasks controller and implement task to send notifications of unread chats

--- commands/RemindersController.php
+++ commands/RemindersController.php
@@ -7,14 +7,23 @@
 
 namespace app\commands;
 
+use app\helpers\EmailsHelper;
+use app\models\Chat;
 use app\models\Order;
 use app\models\OrderPack;
+use app\models\Person;
 use yii\console\Controller;
 
-class SmsTaskController extends Controller
+class RemindersController extends Controller
 {
-    public function actionDeviserReminders()
+    public function actionIndex()
     {
+    	$this->sendSmsDevisersReminders();
+    	$this->sendUnreadMessagesReminders();
+    }
+
+	protected function sendSmsDevisersReminders()
+	{
 		$hoursFrom = \DateInterval::createFromDateString('96 hours');
 		$hoursTo = \DateInterval::createFromDateString('72 hours');
 
@@ -24,25 +33,25 @@ class SmsTaskController extends Controller
 		$date_from_formatted = strtotime($dateFrom);
 		$date_to_formatted = strtotime($dateTo);
 
-    	$orders = Order::findSerialized(
-    		[
+		$orders = Order::findSerialized(
+			[
 				'order_date_from' => new \MongoDate($date_from_formatted),
 				'order_date_to' => new \MongoDate($date_to_formatted),
 				'order_state' => Order::ORDER_STATE_PAID,
 				'pack_state' => OrderPack::PACK_STATE_PAID,
 			]
 		);
-    	foreach ($orders as $order) {
-    		$packs = $order->getPacks();
-    		foreach ($packs as $pack) {
-    			if ($pack->pack_state == OrderPack::PACK_STATE_PAID) {
+		foreach ($orders as $order) {
+			$packs = $order->getPacks();
+			foreach ($packs as $pack) {
+				if ($pack->pack_state == OrderPack::PACK_STATE_PAID) {
 
 					if (!$pack->getDeviser()->personalInfoMapping->getPhoneNumber()) {
 						echo "Deviser ".$pack->getDeviser()->getName()." (pack ".$pack->short_id.", order ".$order->short_id.") does not have a phone number\n";
 						continue; // Deviser does not have a phone number
 					}
 
-    				if ($pack->hasSentSmsNewOrderReminder72()) {
+					if ($pack->hasSentSmsNewOrderReminder72()) {
 						echo "Message (pack ".$pack->short_id.", order ".$order->short_id.") already sent\n";
 						continue; // Already sent
 					}
@@ -56,5 +65,23 @@ class SmsTaskController extends Controller
 				}
 			}
 		}
-    }
+	}
+
+	protected function sendUnreadMessagesReminders()
+	{
+		// find unread messages for 24horas or more
+		$chats = Chat::findSerialized();
+		foreach ($chats as $chat) {
+			foreach ($chat->unread_by as $person_id) {
+				$receiver = Person::findOneSerialized($person_id);
+				$message = $chat->getLastMessage($receiver);
+				if ($message) {
+					// TODO: check if sender and receiver are buyer and seller
+
+					EmailsHelper::unreadChat($receiver, $message);
+				}
+			}
+
+		}
+	}
 }
