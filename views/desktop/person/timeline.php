e3253b954fb0b8c72ebf6d7476029894f39434d9
Jose Vázquez Viader <jose.vazquez.viader@gmail.com>
2018-07-16 00:39:29 +0200

    Restore message if empty timeline

--- views/desktop/person/timeline.php
+++ views/desktop/person/timeline.php
@@ -30,7 +30,7 @@ Yii::$app->opengraph->title = $this->title;
 			</ul></nav></div>
 			<div class="col-xs-12 col-md-8 col-lg-7">
 				<div class="timeline-wrapper">
-					<div ng-if="false && (timelineCtrl.timeline | filter:{ person: {person_type:timelineCtrl.selectedPersonType}}).length < 1" ng-cloak>
+					<div ng-if="(timelineCtrl.timeline | filter:{ person: {person_type:timelineCtrl.selectedPersonType}}).length < 1" ng-cloak>
 						<span translate="person.timeline.EMPTY_TIMELINE"></span>
 					</div>
 					<div ng-if="(timelineCtrl.timeline | filter:{ person: {person_type:timelineCtrl.selectedPersonType}}).length > 0" infinite-scroll="timelineCtrl.addMoreItems()" infinite-scroll-distance="timelineCtrl.show_items-1">
