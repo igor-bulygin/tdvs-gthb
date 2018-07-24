62a7453b88da3bcae89bdab8eb8853f89449869d
Gabriela Irisarri <gabriela.irisarri@hotmail.com>
2018-07-24 20:35:10 +0200

    redirect when unlogged person clicks on follow in home and headers

--- web/js/desktop/person/person-component.js
+++ web/js/desktop/person/person-component.js
@@ -1,50 +1,60 @@
-(function () {
-	"use strict";
+(function() {
+    "use strict";
 
-	function controller(personDataService, UtilService) {
-		var vm = this;
-		vm.init = init;
-		vm.follow = follow;
-		vm.unFollow = unFollow;
+    function controller(personDataService, UtilService, $window) {
+        var vm = this;
+        vm.init = init;
+        vm.follow = follow;
+        vm.unFollow = unFollow;
 
-		function init(isFollowed) {
-			vm.isFollowed = isFollowed;
-		}
+        function init(isFollowed) {
+            vm.isFollowed = isFollowed;
+        }
 
-		function unFollow(personId) {
-			function onSetFollowSuccess(data) {
-				vm.isFollowed = false;
-			}
+        function unFollow(personId) {
+            var connectedUser = UtilService.getConnectedUser();
+            if (!connectedUser) {
+                $window.location.href = '/timeline';
+            } else {
+                function onSetFollowSuccess(data) {
+                    vm.isFollowed = false;
+                }
 
-			function onSetFollowError(err) {
-				UtilService.onError(err);
-			}
+                function onSetFollowError(err) {
+                    UtilService.onError(err);
+                }
 
-			var params = {
-				personId: personId
-			}
-			personDataService.unFollowPerson(params, params, onSetFollowSuccess, onSetFollowError);
-		}
+                var params = {
+                    personId: personId
+                }
+                personDataService.unFollowPerson(params, params, onSetFollowSuccess, onSetFollowError);
+            }
+        }
 
-		function follow(personId) {
-			function onSetFollowSuccess(data) {
-				vm.isFollowed = true;
-			}
+        function follow(personId) {
+            var connectedUser = UtilService.getConnectedUser();
+            if (!connectedUser) {
+                $window.location.href = '/timeline';
+            } else {
+                function onSetFollowSuccess(data) {
+                    vm.isFollowed = true;
+                }
 
-			function onSetFollowError(err) {
-				UtilService.onError(err);
-			}
+                function onSetFollowError(err) {
+                    UtilService.onError(err);
+                }
 
-			var params = {
-				personId: personId
-			}
-			personDataService.followPerson(params, params, onSetFollowSuccess, onSetFollowError);
-		}
+                var params = {
+                    personId: personId
+                }
+                personDataService.followPerson(params, params, onSetFollowSuccess, onSetFollowError);
+            }
+        }
 
-	}
+    }
 
-	angular
-		.module('person')
-		.controller('personComponentCtrl', controller)
+    angular
+        .module('person')
+        .controller('personComponentCtrl', controller)
 
 }());
