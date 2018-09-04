98b1a46c20b2ff512cc0a2357c1793dc3349b361
Gabriela Irisarri <gabriela.irisarri@hotmail.com>
2018-09-04 18:01:44 +0200

    get connected user

--- web/js/desktop/todevise.module.js
+++ web/js/desktop/todevise.module.js
@@ -18,8 +18,8 @@
     angular
         .module('todevise', ['header', 'footer', 'api', 'util', 'toastr', 'box', 'person', 'product', 'discover', 'settings', 'cart', 'chat', 'pascalprecht.translate', 'ngCookies'])
         .config(config)
-        .run(function(UtilService, $cookieStore, localStorageUtilService) {
-            if (!$cookieStore.get('sesion_id') && localStorageUtilService.getLocalStorage('access_token')) {
+        .run(function(UtilService, $cookies, localStorageUtilService) {
+            if (!$cookies.get("sesion_id") && localStorageUtilService.getLocalStorage('access_token')) {
                 UtilService.logout();
             }
         });
