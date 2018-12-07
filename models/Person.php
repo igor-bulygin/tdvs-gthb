42d8d048fdb9270fa081d27499f30131a325df5f
Eduard Dalmau <eduard.dr@coditramuntana.com>
2018-12-07 09:12:24 +0100

    [FIX] affiliate page error on dev

--- models/Person.php
+++ models/Person.php
@@ -2647,9 +2647,9 @@ class Person extends CActiveRecord implements IdentityInterface
       if(isset($this->earnings_by_user[$affiliate->short_id])) {
         foreach($this->earnings_by_user[$affiliate->short_id]['earnings_by_order'] as $earningOrder) {
           if(isset($affiliatesAux[$affiliate->short_id]['totalEarning']))
-            $affiliatesAux[$affiliate->short_id]['totalEarning'] = (float)$affiliatesAux[$affiliate->short_id]['totalEarning'] + (float)$earningOrder;
+            $affiliatesAux[$affiliate->short_id]['totalEarning'] = (float)$affiliatesAux[$affiliate->short_id]['totalEarning'] + (float)$earningOrder->amount;
           else
-            $affiliatesAux[$affiliate->short_id]['totalEarning'] = (float)$earningOrder;
+            $affiliatesAux[$affiliate->short_id]['totalEarning'] = (float)$earningOrder->amount;
         }
       }
       else {
