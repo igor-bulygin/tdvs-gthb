ebd1b4c88f121fb0a98eb89613d54ffbbc97fe9c
eduard-dr <38754625+eduard-dr@users.noreply.github.com>
2019-01-17 17:36:30 +0100

    Revert "[FIX] Fixed text, loved and size in social feed"

--- views/desktop/person/social-view.php
+++ views/desktop/person/social-view.php
@@ -45,11 +45,26 @@ $this->params['person_links_target'] = 'public_view';
 					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mb-20 text-center" ng-if="socialManagerCtrl.viewingConnectedUser()" ng-cloak>
 						<button class="btn btn-red btn-add-box" ng-click="socialManagerCtrl.showNewPost()"><span translate="person.posts.ADD_POST"></span></button>
 					</div>
-					<div id="content-posts" class="col-xs-4" ng-repeat="post in socialManagerCtrl.posts" ng-cloak>
+					<div id="content-posts" class="col-xs-12 col-sm-6 col-md-4 col-lg-4" ng-repeat="post in socialManagerCtrl.posts" ng-cloak>
 						<figure class="showcase" ng-click="socialManagerCtrl.openPostDetailsModal(post)">
 							<div class="images-box">
-								<img class="col-xs-12 grid-image" style="width: 100%; height: 100%; padding: 0; object-fit: cover;" ng-src="{{post.photo_url}}">
+								<img class="col-xs-12 grid-image" ng-src="{{post.photo_url}}">
 							</div>
+							<figcaption>
+								<div class="row no-mar">
+									<div class="col-xs-2 col-sm-2 col-md-3 col-xs-offset-10 col-sm-offset-10 col-md-offset-9 no-padding">
+										<span ng-bind="post.loveds" class="heart-num"></span>
+										<span ng-if="post.isLoved" class="icons-hover heart-icon heart-red-icon" ng-click="socialManagerCtrl.unLovePost(post)" ng-cloak></span>
+										<span ng-if="!post.isLoved" class="icons-hover heart-icon heart-black-icon" ng-click="socialManagerCtrl.lovePost(post)" ng-cloak></span>
+									</div>
+									<span class="no-padding col-xs-12 posts-text align-left" ng-bind-html="socialManagerCtrl.sanitizeMe(socialManagerCtrl.truncateString(post.text, socialManagerCtrl.maxCharacters, '...'))"></span>
+									<a ng-if="post.text.length > socialManagerCtrl.maxCharacters && !socialManagerCtrl.viewingConnectedUser()" class="col-xs-4 col-xs-push-8 no-padding"><span translate="person.posts.SEE_MORE"></span></a>
+									<div class="col-xs-4 col-xs-offset-8 col-sm-4 col-md-4 no-padding posts-edit" ng-if="socialManagerCtrl.viewingConnectedUser()">
+										<a ng-click="socialManagerCtrl.editPost(post.id)"><span class="edit-post-icon"><i class="ion-edit"></i></span></a> 
+										<a ng-click="socialManagerCtrl.deletePost(post.id)"><span class="close-post-icon"></span></a>
+									</div>
+								</div>
+							</figcaption>
 						</figure>
 					</div>
 				</div>
@@ -68,50 +83,55 @@ $this->params['person_links_target'] = 'public_view';
 			</h3>
 		</div>
 		<div class="modal-body">
-			<div class="stories-wrapper">
-				<div class="story-component-wrapper">
-					<div class="col-xs-12">
-						<form novalidate>
-							<div class="row no-margin">
-								<div class="text-center" ng-if="!socialManagerCtrl.isEdition">
-									<div class="photo-loader loader-post" ng-if="!socialManagerCtrl.newPost.photo">
-										<div class="new-add-wrapper">
-										</div>
-									</div>
-									<div class="col-xs-12 mb-20" ng-if="!socialManagerCtrl.newPost.photo">
-											<button id="btn_create_box_popup" class="btn btn-red" name="file" ngf-select="socialManagerCtrl.uploadPhoto($files, $invalidFiles)"  ngf-accept="'image/*'"  ngf-drop-available="socialManagerCtrl.isDropAvailable" ngf-multiple="false">
-											<span>upload <span class="ion-camera"></span></span>
-										</button>
-									</div>
-									<div class="col-xs-12 mb-20 text-center" ng-if="socialManagerCtrl.newPost.photo" ng-cloak>
-										<img class="grid-image img-responsive" ng-src="{{socialManagerCtrl.newImage}}">
-									</div>
+					<div class="stories-wrapper">
+						<div class="story-component-wrapper">
+								<div class="col-xs-12">
+										<form novalidate>
+												<div class="row no-margin">
+													<div class="text-center" ng-if="!socialManagerCtrl.isEdition">
+														<div class="photo-loader loader-post" ng-if="!socialManagerCtrl.newPost.photo">
+															<div class="new-add-wrapper">
+															</div>
+														</div>
+														<div class="col-xs-12 mb-20" ng-if="!socialManagerCtrl.newPost.photo">
+																<button id="btn_create_box_popup" class="btn btn-red" name="file" ngf-select="socialManagerCtrl.uploadPhoto($files, $invalidFiles)"  ngf-accept="'image/*'"  ngf-drop-available="socialManagerCtrl.isDropAvailable" ngf-multiple="false">
+																<span>upload <span class="ion-camera"></span></span>
+															</button>
+														</div>
+														<div class="col-xs-12 mb-20 text-center" ng-if="socialManagerCtrl.newPost.photo" ng-cloak>
+															<img class="grid-image img-responsive" ng-src="{{socialManagerCtrl.newImage}}">
+														</div>
+													</div>
+												</div>
+												<!-
+												<div class="col-xs-4 col-xs-push-8 no-margin">
+													<ol class="nya-bs-select form-control" ng-model="socialManagerCtrl.newPost.selected_language" ng-change="socialManagerCtrl.parseText(socialManagerCtrl.newPost)" ng-init="socialManagerCtrl.newPost.selected_language = socialManagerCtrl.selected_language">
+														<li nya-bs-option="language in socialManagerCtrl.languages" class="ng-class:{'lang-selected': socialManagerCtrl.isLanguageOk(language.code, socialManagerCtrl.newPost)}" data-value="language.code" deep-watch="true">
+															<a href="" tabindex="0">
+																<span ng-bind="language.name"></span>
+																<span class="glyphicon glyphicon-ok ok-white-icon pull-right" ng-if="socialManagerCtrl.isLanguageOk(language.code,post)"></span>
+															</a>
+														</li>
+													</ol>
+												</div>	
+												-->												
+												<div class="col-xs-8 no-margin no-padding">
+														<div text-angular ng-model="socialManagerCtrl.newPost.text[socialManagerCtrl.newPost.selected_language]" ta-toolbar="[]" translate-attr="{placeholder: 'person.posts.DESCRIPTION'}" ta-paste="socialManagerCtrl.stripHTMLTags($html)" placeholder="Description"></div>
+												</div>	
+																				
+										</form>
 								</div>
 							</div>
-							<div class="col-xs-4 col-xs-push-8 no-margin">
-								<ol class="nya-bs-select form-control" ng-model="socialManagerCtrl.newPost.selected_language" ng-change="socialManagerCtrl.parseText(socialManagerCtrl.newPost)" ng-init="socialManagerCtrl.newPost.selected_language = socialManagerCtrl.selected_language">
-									<li nya-bs-option="language in socialManagerCtrl.languages" class="ng-class:{'lang-selected': socialManagerCtrl.isLanguageOk(language.code, socialManagerCtrl.newPost)}" data-value="language.code" deep-watch="true">
-										<a href="" tabindex="0">
-											<span ng-bind="language.name"></span>
-											<span class="glyphicon glyphicon-ok ok-white-icon pull-right" ng-if="socialManagerCtrl.isLanguageOk(language.code,post)"></span>
-										</a>
-									</li>
-								</ol>
+						<div class="text-center" style="display: block; width: 100%; float: left; margin:20px 0 15px;">
+							<div class="col-md-10 text-right error-text" ng-if="socialManagerCtrl.newPost.required_text">
+								<span translate="person.faq.FIELD_LANGS_MANDATORY" translate-values='{ languageList: socialManagerCtrl.mandatory_langs_names}'></span>
 							</div>
-							<div class="col-xs-8 no-margin no-padding">
-									<div text-angular ng-model="socialManagerCtrl.newPost.text[socialManagerCtrl.newPost.selected_language]" ta-toolbar="[]" translate-attr="{placeholder: 'person.posts.DESCRIPTION'}" ta-paste="socialManagerCtrl.stripHTMLTags($html)" placeholder="Description"></div>
-							</div>
-						</form>
+							<button class="btn btn-default btn-auto btn-red" ng-click="socialManagerCtrl.createPost()"><span translate="person.posts.PUBLISH"></span></button>
+						</div>
 					</div>
-				</div>
-				<div class="text-center" style="display: block; width: 100%; float: left; margin:20px 0 15px;">
-					<div class="col-md-10 text-right error-text" ng-if="socialManagerCtrl.newPost.required_text">
-						<span translate="person.faq.FIELD_LANGS_MANDATORY" translate-values='{ languageList: socialManagerCtrl.mandatory_langs_names}'></span>
-					</div>
-					<button class="btn btn-default btn-auto btn-red" ng-click="socialManagerCtrl.createPost()"><span translate="person.posts.PUBLISH"></span></button>
-				</div>
-			</div>
 		</div>
 	</div>
 </script>
 </div>
+
+
