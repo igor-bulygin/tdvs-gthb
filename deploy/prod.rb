16a696f597e6970fee02928ebb9610e5ec72b4be
Eduard Dalmau <eduard.dr@coditramuntana.com>
2019-02-21 09:21:18 +0100

    [TEST] images.

--- deploy/prod.rb
+++ deploy/prod.rb
@@ -1,45 +1,25 @@
 task :prod do
         set :branch, 'master'
-        set :user, "todeviseapp"
-        server "todevise.com", :app, :web, :primary => true
-        set :deploy_to, "/var/www/todevise/web"
-        after "deploy", "linkprod","composerprod", "npmprod", "assetsprod", "symlinkprod","restartphpprod"
+        set :user, "coditramuntana"
+        server "tessaractic-todevise-pro-web01.ilimit.es", :app, :web, :primary => true
+        set :deploy_to, "/home/coditramuntana/todevise/web"
+        after "deploy", "composerprod"
 end
 task :composerprod do
     transaction do
-      run "ln -nfs #{shared_path}/system/vendor #{releases_path}/#{release_name}/vendor"
-      run "cd #{releases_path}/#{release_name} ; composer -q global require \"fxp/composer-asset-plugin:~1.1.1\""
-      run "cd #{releases_path}/#{release_name} ; composer -q install"
-      run "cd #{releases_path}/#{release_name} ; ./yii mongodb-migrate --interactive=0"
-    end
-end
-task :npmprod do
-    transaction do
-      run "ln -nfs #{shared_path}/system/node_modules #{releases_path}/#{release_name}/node_modules"
-      run "cd #{releases_path}/#{release_name} ; npm install"
-    end
-end
-task :assetsprod do
-    transaction do
-      # run "cd #{releases_path}/#{release_name}/tools/gulp ; npm install"
-      # run "cd #{releases_path}/#{release_name} ; ./yii asset tools/gulp/assets.php config/assets_compressed.php"
-    end
-end
-task :linkprod do
-    transaction do
-      run "ln -nfs #{shared_path}/public/images #{releases_path}/#{release_name}/web/uploads"
       run "ln -nfs #{shared_path}/.env #{releases_path}/#{release_name}/.env"
-    end
-end
-task :symlinkprod do
-  transaction do
-    run "ln -nfs #{current_release} #{deploy_to}/#{current_dir}"
-  end
-end
 
-task :restartphpprod do
-    transaction do
-        run "sudo /usr/sbin/service php5-fpm restart"
-        run "chmod 775 #{deploy_to}/#{current_dir}/web/assets"
+      run "ln -nfs #{shared_path}/system/vendor #{releases_path}/#{release_name}/vendor"
+      run "ln -nfs #{shared_path}/system/runtime #{releases_path}/#{release_name}/runtime"
+      run "ln -nfs #{shared_path}/system/web/assets #{releases_path}/#{release_name}/web/assets"
+      run "ln -nfs #{shared_path}/public/images/uploads #{releases_path}/#{release_name}/web/uploads"
+
+      run "ln -nfs #{current_release} #{deploy_to}/#{current_dir}"
+      run "cd /home/coditramuntana/todevise/web ; docker-compose restart"
+
+      run "docker exec -i web_nginx_1 chown -R 1101:999 /var/www/html/vendor"
+      run "docker exec -i web_nginx_1 chown -R 1101:999 /var/www/html/web"
+      run "docker exec -i web_nginx_1 chown -R 1101:999 /var/www/html/runtime"
+      run "docker exec -i web_nginx_1 chown -R 1101:999 /var/www/html/.env"
     end
 end
